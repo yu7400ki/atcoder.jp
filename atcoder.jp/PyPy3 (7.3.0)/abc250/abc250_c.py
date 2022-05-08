d8c99257a271c2aa5c52a00b512da670a3c02992
yu7400ki <yu7400ki@gmail.com>
2022-05-09 08:29:35 +0900

    [AC] abc250/abc250_c PyPy3 (7.3.0) at 2022-05-09 08:29:35
    {
        "id": 31557638,
        "epoch_second": 1652052575,
        "problem_id": "abc250_c",
        "contest_id": "abc250",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 300.0,
        "length": 301,
        "result": "AC",
        "execution_time": 290
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc250/abc250_c.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc250/abc250_c.py
@@ -1,12 +1,13 @@
 n,q = map(int,input().split())
 ball = list(range(1,n+1))
-index = list(range(n))
-for i in range(q):
+index = [-1] + list(range(n))
+
+for _ in range(q):
 	x = int(input())
-	p = index[x-1]
-	if p == n-1:
-		p -= 1
-	index[ball[p]-1], index[ball[p+1]-1] = index[ball[p+1]-1], index[ball[p]-1]
-	ball[p], ball[p + 1] = ball[p + 1], ball[p]
+	i = index[x]
+	if i == n-1:
+		i -= 1
+	index[ball[i]], index[ball[i+1]] = index[ball[i+1]], index[ball[i]]
+	ball[i], ball[i + 1] = ball[i + 1], ball[i]
 
 print(*ball)
