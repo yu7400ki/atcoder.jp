6cc7299c1c406ad5b403ac737783615f31ac8051
yu7400ki <yu7400ki@gmail.com>
2022-05-10 19:22:59 +0900

    [AC] abc250/abc250_d PyPy3 (7.3.0) at 2022-05-10 19:22:59
    {
        "id": 31586929,
        "epoch_second": 1652178179,
        "problem_id": "abc250_d",
        "contest_id": "abc250",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 400.0,
        "length": 361,
        "result": "AC",
        "execution_time": 361
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc250/abc250_d.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc250/abc250_d.py
@@ -1,8 +1,7 @@
 n = int(input())
 
-num = 5
 primes = [2, 3]
-for i in range(5,10**6,2):
+for i in range(5, int(pow(n,1/3))+1, 2):
 	for j in primes:
 		if j**2 > i:
 			primes.append(i)
@@ -14,8 +13,8 @@ for i in range(5,10**6,2):
 
 cnt = 0
 for i in range(len(primes)):
-	for j in range(i+1,len(primes)):
-		if primes[j] ** 3 * primes[i] <= n:
+	for j in range(i + 1, len(primes)):
+		if primes[j]**3 * primes[i] <= n:
 			cnt += 1
 		else:
 			break
