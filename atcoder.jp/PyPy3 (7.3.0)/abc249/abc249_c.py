be559478fb4786d16726640efdf8f437b7cb5b88
yu7400ki <yu7400ki@gmail.com>
2022-05-11 10:39:10 +0900

    [AC] abc249/abc249_c PyPy3 (7.3.0) at 2022-05-11 10:39:10
    {
        "id": 31595679,
        "epoch_second": 1652233150,
        "problem_id": "abc249_c",
        "contest_id": "abc249",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 300.0,
        "length": 537,
        "result": "AC",
        "execution_time": 241
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc249/abc249_c.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc249/abc249_c.py
@@ -1,22 +1,25 @@
 from string import ascii_lowercase
 
 n,k = map(int,input().split())
-S = [set(input()) for _ in range(n)]
+S = [input() for _ in range(n)]
 
-res = []
-for i in range(1,2**n):
+res = 0
+for i in range(2**n):
     select = []
+    kind = 0
     for j in range(n):
         if (i >> j) & 1:
             select.append(S[j])
-    memo = []
     for a in ascii_lowercase:
         cnt = 0
         for s in select:
             if a in s:
                 cnt += 1
-        memo.append(cnt)
-    memo = list(filter(lambda x:x == k,memo))
-    res.append(len(memo))
+                if cnt > k:
+                    break
+        else:
+            if cnt == k:
+                kind += 1
+    res = max(res, kind)
 
-print(max(res))
+print(res)
