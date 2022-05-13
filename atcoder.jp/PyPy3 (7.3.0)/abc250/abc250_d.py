b8146c70a858ec0c5f292ed41810e6787987604f
yu7400ki <yu7400ki@gmail.com>
2022-05-14 00:54:32 +0900

    [AC] abc250/abc250_d PyPy3 (7.3.0) at 2022-05-14 00:54:32
    {
        "id": 31641391,
        "epoch_second": 1652457272,
        "problem_id": "abc250_d",
        "contest_id": "abc250",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 400.0,
        "length": 545,
        "result": "AC",
        "execution_time": 133
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc250/abc250_d.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc250/abc250_d.py
@@ -1,15 +1,13 @@
 n = int(input())
 
-primes = [2, 3]
-for i in range(5, int(pow(n,1/3))+1, 2):
-	for j in primes:
-		if j**2 > i:
-			primes.append(i)
-			break
-		if i % j == 0:
-			break
-	else:
-		primes.append(i)
+x = int(pow(n,1/3))+1
+is_prime = [True] * (x+1)
+is_prime[0] = is_prime[1] = False
+for p in range(2, int(x ** 0.5) + 1):
+    if is_prime[p]:
+        for i in range(p*p, x+1, p):
+            is_prime[i] = False
+primes =  [p for p in range(x + 1) if is_prime[p]]
 
 primes = [0] + primes + [0]
 
