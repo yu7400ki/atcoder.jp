82e0ea3719e67a952663f08fe338dfd0c8784918
yu7400ki <yu7400ki@gmail.com>
2022-05-19 15:04:48 +0900

    [AC] abc245/abc245_c PyPy3 (7.3.0) at 2022-05-19 15:04:48
    {
        "id": 31794193,
        "epoch_second": 1652940288,
        "problem_id": "abc245_c",
        "contest_id": "abc245",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 300.0,
        "length": 543,
        "result": "AC",
        "execution_time": 190
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc245/abc245_c.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc245/abc245_c.py
@@ -2,25 +2,23 @@ N, K = map(int,input().split())
 A = list(map(int,input().split()))
 B = list(map(int,input().split()))
 
-dp = [None] * (N+1)
-ep = [None] * (N+1)
-dp[1] = ep[1] = True
+dp = [False] * (N)
+ep = [False] * (N)
+dp[0] = ep[0] = True
 
-for i in range(2,N+1):
-    if abs(A[i-2] - A[i-1]) <= K:
-        dp[i] = True
-    elif abs(B[i-2] - A[i-1]) <= K:
-        dp[i] = True
-    else:
-        dp[i] = False
-    if abs(A[i-2] - B[i-1]) <= K:
-        ep[i] = True
-    elif abs(B[i-2] - A[i-1]) <= K:
-        ep[i] = True
-    else:
-        ep[i] = False
-    if not dp[i] and not ep[i]:
-        print('No')
-        exit()
+for i in range(1,N):
+    if dp[i-1]:
+        if abs(A[i-1] - A[i]) <= K:
+            dp[i] = True
+        if abs(A[i-1] - B[i]) <= K:
+            ep[i] = True
+    if ep[i-1]:
+        if abs(B[i-1] - A[i]) <= K:
+            dp[i] = True
+        if abs(B[i-1] - B[i]) <= K:
+            ep[i] = True
 
-print('Yes')
+if dp[-1] or ep[-1]:
+    print('Yes')
+else:
+    print('No')
