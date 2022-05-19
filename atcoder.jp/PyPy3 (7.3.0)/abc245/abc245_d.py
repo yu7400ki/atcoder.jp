1a8474c466c85f603da4cadb6eb40cab8ce411eb
yu7400ki <yu7400ki@gmail.com>
2022-05-19 15:40:56 +0900

    [RE] abc245/abc245_d PyPy3 (7.3.0) at 2022-05-19 15:40:56
    {
        "id": 31794829,
        "epoch_second": 1652942456,
        "problem_id": "abc245_d",
        "contest_id": "abc245",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 259,
        "result": "RE",
        "execution_time": 83
    }

---
+++ atcoder.jp/PyPy3 (7.3.0)/abc245/abc245_d.py
@@ -0,0 +1,12 @@
+N, M = map(int,input().split())
+A = list(map(int,input().split()))[::-1]
+C = list(map(int,input().split()))[::-1]
+
+B = []
+for i in range(N+M):
+    b = C[i] // A[0]
+    B.append(b)
+    for j in range(N+1):
+        C[j+i] -= A[j] * b
+
+print(*B[::-1])
