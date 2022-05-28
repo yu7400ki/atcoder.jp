e450e42b2b364c677c6e4a0fc62c1f1995f8eb2c
yu7400ki <yu7400ki@gmail.com>
2022-05-28 22:33:53 +0900

    [AC] abc253/abc253_d PyPy3 (7.3.0) at 2022-05-28 22:33:53
    {
        "id": 32052566,
        "epoch_second": 1653744833,
        "problem_id": "abc253_d",
        "contest_id": "abc253",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 400.0,
        "length": 273,
        "result": "AC",
        "execution_time": 71
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc253/abc253_d.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc253/abc253_d.py
@@ -6,13 +6,10 @@ lcd = A*B // gcd(A,B)
 ans = (N * (1+N) // 2)
 
 if A <= N:
-	a = list(range(A,N+1,A))
-	ans -= len(a) * (A+a[-1]) // 2
+	ans -= N//A * (A+A*(N//A)) // 2
 if B <= N:
-	b = list(range(B,N+1,B))
-	ans -= len(b) * (B+b[-1]) // 2
+	ans -= N//B * (B+B*(N//B)) // 2
 if lcd <= N:
-	c = list(range(lcd,N+1,lcd))
-	ans += len(c) * (lcd + c[-1]) // 2
+	ans += N//lcd * (lcd + lcd*(N//lcd)) // 2
 
 print(ans)
