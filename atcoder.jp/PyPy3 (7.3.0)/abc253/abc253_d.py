1ed15fa1d2f730f16c8740a175caae6b64846305
yu7400ki <yu7400ki@gmail.com>
2022-05-28 22:18:12 +0900

    [RE] abc253/abc253_d PyPy3 (7.3.0) at 2022-05-28 22:18:12
    {
        "id": 32046374,
        "epoch_second": 1653743892,
        "problem_id": "abc253_d",
        "contest_id": "abc253",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 312,
        "result": "RE",
        "execution_time": 271
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc253/abc253_d.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc253/abc253_d.py
@@ -1,8 +1,16 @@
 N, A, B = map(int,input().split())
 
-a = list(range(1,N+1,A))
-b = list(range(1,N+1,B))
-c = list(range(1,N+1,A*B))
-ans = (N * (1+N) // 2) - (len(a) * (A+a[-1]) // 2) - (len(b) * (B+b[-1]) // 2) - (len(c) * (A*B+c[-1]) // 2)
+a = list(range(A,N+1,A))
+b = list(range(B,N+1,B))
+c = list(range(A*B,N+1,A*B))
+
+ans = (N * (1+N) // 2)
+if len(a) != 0:
+	ans -= len(a) * (A+a[-1]) // 2
+if len(b) != 0:
+	ans -= len(b) * (B+b[-1]) // 2
+if len(c) != 0:
+	ans += len(c) * (A*B+c[-1]) // 2
+
 
 print(ans)
