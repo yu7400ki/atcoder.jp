1842b7348267196ec5a82f0c4bd33ce9920ca00e
yu7400ki <yu7400ki@gmail.com>
2022-05-28 21:39:36 +0900

    [RE] abc253/abc253_c PyPy3 (7.3.0) at 2022-05-28 21:39:36
    {
        "id": 32029603,
        "epoch_second": 1653741576,
        "problem_id": "abc253_c",
        "contest_id": "abc253",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 639,
        "result": "RE",
        "execution_time": 565
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc253/abc253_c.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc253/abc253_c.py
@@ -8,10 +8,19 @@ Q = int(input())
 for i in range(Q):
 	query = list(map(int,input().split()))
 	if query[0] == 1:
+		min_x = min(min_x, query[1])
+		max_x = max(max_x, query[1])
 		S_count[query[1]] += 1
 	elif query[0] == 2:
 		S_count[query[1]] -= min(query[2], S_count[query[1]])
 		if S_count[query[1]] == 0:
-			del S_count[query[1]]
+			if query[1] == min_x:
+				del S_count[query[1]]
+				min_x = min(S_count)
+			elif query[1] == max_x:
+				del S_count[query[1]]
+				max_x = max(S_count)
+			else:
+				del S_count[query[1]]
 	else:
-		print(max(S_count) - min(S_count))
+		print(max_x - min_x)
