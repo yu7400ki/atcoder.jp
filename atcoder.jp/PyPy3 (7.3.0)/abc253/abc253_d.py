a797d0a2002e67fbf9c66ef0e0ce42a4e5cd97bc
yu7400ki <yu7400ki@gmail.com>
2022-05-28 22:21:19 +0900

    [RE] abc253/abc253_d PyPy3 (7.3.0) at 2022-05-28 22:21:19
    {
        "id": 32047561,
        "epoch_second": 1653744079,
        "problem_id": "abc253_d",
        "contest_id": "abc253",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 301,
        "result": "RE",
        "execution_time": 260
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc253/abc253_d.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc253/abc253_d.py
@@ -1,16 +1,15 @@
 N, A, B = map(int,input().split())
 
-a = list(range(A,N+1,A))
-b = list(range(B,N+1,B))
-c = list(range(A*B,N+1,A*B))
-
 ans = (N * (1+N) // 2)
-if len(a) != 0:
+
+if A <= N:
+	a = list(range(A,N+1,A))
 	ans -= len(a) * (A+a[-1]) // 2
-if len(b) != 0:
+if B <= N:
+	b = list(range(B,N+1,B))
 	ans -= len(b) * (B+b[-1]) // 2
-if len(c) != 0:
+if A*B <= N:
+	c = list(range(A*B,N+1,A*B))
 	ans += len(c) * (A*B+c[-1]) // 2
-
-
+	
 print(ans)
