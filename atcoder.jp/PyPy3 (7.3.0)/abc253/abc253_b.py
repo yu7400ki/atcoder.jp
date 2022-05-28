81e25a38cb83d933c16c46a06c192d1f3606bb93
yu7400ki <yu7400ki@gmail.com>
2022-05-28 21:14:06 +0900

    [AC] abc253/abc253_b PyPy3 (7.3.0) at 2022-05-28 21:14:06
    {
        "id": 32013792,
        "epoch_second": 1653740046,
        "problem_id": "abc253_b",
        "contest_id": "abc253",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 200.0,
        "length": 336,
        "result": "AC",
        "execution_time": 80
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc253/abc253_b.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc253/abc253_b.py
@@ -6,10 +6,15 @@ b = []
 for h in range(H):
 	s = input()
 	if 'o' in s:
-		if a == []:
-			a = [s.index('o'), h]
+		if s.count('o') == 1:
+			if a == []:
+				a = [s.index('o'), h]
+			else:
+				b = [s.index('o'), h]
 		else:
-			b = [s.index('o'), h]
+			a = [s.index('o'), h]
+			b = [s.rfind('o'), h]
+			break
 
 c = abs(a[0] - b[0])
 d = abs(a[1] - b[1])
