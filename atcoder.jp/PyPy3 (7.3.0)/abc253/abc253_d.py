3eae2487e424918c6297689ee541df3e8af93962
yu7400ki <yu7400ki@gmail.com>
2022-05-29 15:08:34 +0900

    [AC] abc253/abc253_d PyPy3 (7.3.0) at 2022-05-29 15:08:34
    {
        "id": 32075109,
        "epoch_second": 1653804514,
        "problem_id": "abc253_d",
        "contest_id": "abc253",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 400.0,
        "length": 228,
        "result": "AC",
        "execution_time": 61
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc253/abc253_d.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc253/abc253_d.py
@@ -1,15 +1,11 @@
 from math import gcd
 
 N, A, B = map(int,input().split())
-
 lcd = A*B // gcd(A,B)
-ans = (N * (1+N) // 2)
 
-if A <= N:
-	ans -= N//A * (A+A*(N//A)) // 2
-if B <= N:
-	ans -= N//B * (B+B*(N//B)) // 2
-if lcd <= N:
-	ans += N//lcd * (lcd + lcd*(N//lcd)) // 2
+ans = N * (1+N) // 2
+ans -= N//A * (A+A*(N//A)) // 2
+ans -= N//B * (B+B*(N//B)) // 2
+ans += N//lcd * (lcd + lcd*(N//lcd)) // 2
 
 print(ans)
