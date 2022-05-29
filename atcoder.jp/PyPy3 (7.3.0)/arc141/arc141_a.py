c451486b734ab990beebd3db27725c65cdafa4fa
yu7400ki <yu7400ki@gmail.com>
2022-05-29 22:42:26 +0900

    [WA] arc141/arc141_a PyPy3 (7.3.0) at 2022-05-29 22:42:26
    {
        "id": 32092543,
        "epoch_second": 1653831746,
        "problem_id": "arc141_a",
        "contest_id": "arc141",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 766,
        "result": "WA",
        "execution_time": 201
    }

--- atcoder.jp/PyPy3 (7.3.0)/arc141/arc141_a.py
+++ atcoder.jp/PyPy3 (7.3.0)/arc141/arc141_a.py
@@ -1,6 +1,6 @@
 import sys
 
-sys.setrecursionlimit(10**4)
+#sys.setrecursionlimit(10**8)
 
 def f(n):
 	for i in range(1,int(n**0.5)+1):
@@ -14,23 +14,28 @@ def main(c):
 	if d != 1:
 		c_1 = int(c[:l//d])
 		c_2 = int(c[l//d:l//d*2])
-		if c_1 > c_2:
-			c_1 -= 1
+		for i in range(1,d):
+			if c_1 != int(c[(l//d)*i : l//d*(d+1)]):
+				if c_1 > int(c[(l//d)*i : l//d*(d+1)]):
+					c_1 -= 1
+				break
 		if len(str(c_1)) < (l//d):
 			c_1 = '9' * (l-1)
 			return c_1
 		ans = str(c_1) * d
 	else:
 		c_1 = int(c[0])
-		c_2 = int(c[1])
-		if c_1 > c_2:
-			c_1 -= 1
+		for i in range(1,l):
+			if c_1 != int(c[i]):
+				if c_1 > int(c[i]):
+					c_1 -= 1
+				break
 		if c_1 == 0:
 			c_1 = 9
 		ans = str(c_1) * l
+		if int(ans) > int(c):
+			ans = ans[1:]
 
-	if int(ans) > int(c):
-		ans = main(str(int(c)-1))
 	return ans
 
 T = int(input())
