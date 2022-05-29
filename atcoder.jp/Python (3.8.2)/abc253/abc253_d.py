9d03294caeaa87921318894953585b9dd7a9096e
yu7400ki <yu7400ki@gmail.com>
2022-05-29 15:10:25 +0900

    [AC] abc253/abc253_d Python (3.8.2) at 2022-05-29 15:10:25
    {
        "id": 32075147,
        "epoch_second": 1653804625,
        "problem_id": "abc253_d",
        "contest_id": "abc253",
        "user_id": "yu7400ki",
        "language": "Python (3.8.2)",
        "point": 400.0,
        "length": 230,
        "result": "AC",
        "execution_time": 28
    }

---
+++ atcoder.jp/Python (3.8.2)/abc253/abc253_d.py
@@ -0,0 +1,11 @@
+from math import gcd
+
+N, A, B = map(int,input().split())
+lcd = A*B // gcd(A,B)
+
+ans = (N * (1+N) // 2)
+ans -= N//A * (A+A*(N//A)) // 2
+ans -= N//B * (B+B*(N//B)) // 2
+ans += N//lcd * (lcd + lcd*(N//lcd)) // 2
+
+print(ans)
