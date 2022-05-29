7e5bdcc0bc7af7851a84fff510a449d95ca786dc
yu7400ki <yu7400ki@gmail.com>
2022-05-29 21:37:15 +0900

    [WA] arc141/arc141_a PyPy3 (7.3.0) at 2022-05-29 21:37:15
    {
        "id": 32086354,
        "epoch_second": 1653827835,
        "problem_id": "arc141_a",
        "contest_id": "arc141",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 514,
        "result": "WA",
        "execution_time": 171
    }

---
+++ atcoder.jp/PyPy3 (7.3.0)/arc141/arc141_a.py
@@ -0,0 +1,33 @@
+from math import ceil
+
+def f(n):
+	for i in range(1,int(n**0.5)+1):
+		if n % i == 0:
+			ans = i
+	return ans
+
+def main(c):
+	l = len(c)
+	d = f(l)
+	if d != 1:
+		c_1 = int(c[:l//d])
+		c_2 = int(c[l//d:l//d*2])
+		if c_1 > c_2:
+			c_1 -= 1
+		ans = str(c_1) * d
+	else:
+		c_1 = int(c[0])
+		c_2 = int(c[1])
+		if c_1 > c_2:
+			c_1 -= 1
+		if c_1 == 0:
+			c_1 = 9
+		ans = str(c_1) * (l//d)
+
+	if len(ans) > l:
+		ans = ans[1:]
+	return ans
+
+T = int(input())
+for _ in range(T):
+	print(main(input()))
