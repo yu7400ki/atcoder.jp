dc5b958aadcb8314e144b5b102a6c7c623a5316f
yu7400ki <yu7400ki@gmail.com>
2022-06-01 22:00:30 +0900

    [AC] abs/abc081_b PyPy3 (7.3.0) at 2022-06-01 22:00:30
    {
        "id": 32146801,
        "epoch_second": 1654088430,
        "problem_id": "abc081_b",
        "contest_id": "abs",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 200.0,
        "length": 506,
        "result": "AC",
        "execution_time": 70
    }

--- atcoder.jp/PyPy3 (7.3.0)/abs/abc081_b.py
+++ atcoder.jp/PyPy3 (7.3.0)/abs/abc081_b.py
@@ -1,6 +1,19 @@
 input()
-a = list(map(int,input().split()))
-cnt = 0
-while all(map(lambda x: x >> cnt & 1 == 0, a)):
-	cnt += 1
-print(cnt)
+
+A = list(map(int,input().split()))
+
+def dec_to_bin(x):	#10進数を2進数へ変換
+	return bin(int(x))
+
+def consecutive_zero(x):
+	return len(x) - x.rfind("1") - 1	#後ろから0がいくつ連続するかを返す
+
+a_list = []
+for a in A:
+	a_list.append(dec_to_bin(a))	#Aの要素すべてに対してdec_to_binを実行
+
+l = []
+for a in a_list:
+	l.append(consecutive_zero(a))	#a_listの要素すべてに対してconsecutive_zeroを実行
+
+print(min(l))	#lの最小値を出力
