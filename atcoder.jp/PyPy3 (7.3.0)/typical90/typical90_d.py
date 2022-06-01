8a353c677ae8f162cb7809675d180704c0eb4776
yu7400ki <yu7400ki@gmail.com>
2022-06-02 00:06:51 +0900

    [AC] typical90/typical90_d PyPy3 (7.3.0) at 2022-06-02 00:06:51
    {
        "id": 32149131,
        "epoch_second": 1654096011,
        "problem_id": "typical90_d",
        "contest_id": "typical90",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 2.0,
        "length": 288,
        "result": "AC",
        "execution_time": 749
    }

--- atcoder.jp/PyPy3 (7.3.0)/typical90/typical90_d.py
+++ atcoder.jp/PyPy3 (7.3.0)/typical90/typical90_d.py
@@ -4,7 +4,6 @@ A = [list(map(int,input().split())) for _ in range(H)]
 A_h = [sum(A[i]) for i in range(H)]
 A_w = [sum([A[i][j] for i in range(H)]) for j in range(W)]
 
-for i in range(H):
-	for j in range(W):
-		print(A_h[i] + A_w[j] - A[i][j], end=' ')
-	print()
+ans = [[A_h[i] + A_w[j] - A[i][j] for j in range(W)] for i in range(H)]
+for h in ans:
+	print(*h)
