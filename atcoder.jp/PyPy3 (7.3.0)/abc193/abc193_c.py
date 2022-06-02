8487e1cf3e8214789e852c25bc3292aedfdbeef4
yu7400ki <yu7400ki@gmail.com>
2022-06-02 22:31:46 +0900

    [AC] abc193/abc193_c PyPy3 (7.3.0) at 2022-06-02 22:31:46
    {
        "id": 32164310,
        "epoch_second": 1654176706,
        "problem_id": "abc193_c",
        "contest_id": "abc193",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 300.0,
        "length": 188,
        "result": "AC",
        "execution_time": 74
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc193/abc193_c.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc193/abc193_c.py
@@ -2,7 +2,7 @@ N = int(input())
 
 s = set()
 for i in range(2,int(N**0.5)+1):
-    for j in range(2,int(N**0.5)+1):
+    for j in range(2,N):
         a = i**j
         if a > N:
             break
