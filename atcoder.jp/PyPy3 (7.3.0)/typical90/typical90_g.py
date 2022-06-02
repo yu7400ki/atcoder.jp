33f56bd7477125fe146b054def708fb1139e0f22
yu7400ki <yu7400ki@gmail.com>
2022-06-02 13:55:56 +0900

    [AC] typical90/typical90_g PyPy3 (7.3.0) at 2022-06-02 13:55:56
    {
        "id": 32155372,
        "epoch_second": 1654145756,
        "problem_id": "typical90_g",
        "contest_id": "typical90",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 3.0,
        "length": 544,
        "result": "AC",
        "execution_time": 1219
    }

--- atcoder.jp/PyPy3 (7.3.0)/typical90/typical90_g.py
+++ atcoder.jp/PyPy3 (7.3.0)/typical90/typical90_g.py
@@ -1,5 +1,5 @@
 N = int(input())
-A = [0] + list(map(int,input().split())) + [0]
+A = list(map(int,input().split()))
 Q = int(input())
 
 A.sort()
@@ -7,7 +7,7 @@ A.sort()
 for i in range(Q):
     B = int(input())
     
-    ok = len(A) - 1
+    ok = len(A)
     ng = -1
     while ok - ng > 1:
         mid = (ok+ng) // 2
@@ -15,17 +15,13 @@ for i in range(Q):
             ok = mid
         else:
             ng = mid
-    if ok == 0:
-        ans = abs(A[1] - B)
-        print(ans)
-        continue
-    elif ok == len(A):
+    if ok == len(A):
         ok -= 1
-    if ok > 1:
-        if abs(A[ok] - B) > abs(A[ok-1] - B):
-            ans = abs(A[ok-1] - B)
-        else:
+    if ok >= 1:
+        if abs(A[ok] - B) < abs(A[ok-1] - B):
             ans = abs(A[ok] - B)
+        else:
+            ans = abs(A[ok-1] - B)
     else:
         ans = abs(A[ok] - B)
 
