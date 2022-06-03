b6959f4144986fe2b86fb1973914450e5a33d7ac
yu7400ki <yu7400ki@gmail.com>
2022-06-03 12:54:41 +0900

    [RE] tenka1-2012-qualC/tenka1_2012_9 PyPy3 (7.3.0) at 2022-06-03 12:54:41
    {
        "id": 32172385,
        "epoch_second": 1654228481,
        "problem_id": "tenka1_2012_9",
        "contest_id": "tenka1-2012-qualC",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 310,
        "result": "RE",
        "execution_time": 91
    }

--- atcoder.jp/PyPy3 (7.3.0)/tenka1-2012-qualC/tenka1_2012_9.py
+++ atcoder.jp/PyPy3 (7.3.0)/tenka1-2012-qualC/tenka1_2012_9.py
@@ -1,15 +1,12 @@
-from operator import is_
-
-
 n = int(input())
 
 def prime(n):
-    is_prime = [True] * (n+1)
+    is_prime = [True] * n
     is_prime[0] = is_prime[1] = False
     for i in range(2,int(n**0.5)+1):
         if is_prime[i]:
-            for j in range(i*i,n+1,i):
+            for j in range(i*i,n,i):
                 is_prime[j] = False
-    return [i for i in range(n+1) if is_prime[i]]
+    return [i for i in range(n) if is_prime[i]]
 
 print(len(prime(n)))
