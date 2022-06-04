49f6c9cd70e7b19603c2d0b16f0ff1b80dfd7a67
yu7400ki <yu7400ki@gmail.com>
2022-06-04 22:11:30 +0900

    [WA] abc254/abc254_c PyPy3 (7.3.0) at 2022-06-04 22:11:30
    {
        "id": 32235673,
        "epoch_second": 1654348290,
        "problem_id": "abc254_c",
        "contest_id": "abc254",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 313,
        "result": "WA",
        "execution_time": 141
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc254/abc254_c.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc254/abc254_c.py
@@ -2,9 +2,16 @@ N, K = map(int,input().split())
 a = list(map(int,input().split()))
 A = sorted(a)
 
+if K == 1:
+	print('Yes')
+	exit()
+
 for i in range(N - K):
+	flag = True
 	for j in range(N - K - i):
 		if a[j] > a[j+K]:
+			flag = False
 			a[j], a[j+K] = a[j+K], a[j]
+		if flag: break
 
 print('Yes' if a == A else 'No')
