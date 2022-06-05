4f6e8f5ad1ee2f6003ab4c4e6b56ef2c1c2de371
yu7400ki <yu7400ki@gmail.com>
2022-06-05 17:31:49 +0900

    [AC] hhkb2020/hhkb2020_c PyPy3 (7.3.0) at 2022-06-05 17:31:49
    {
        "id": 32266265,
        "epoch_second": 1654417909,
        "problem_id": "hhkb2020_c",
        "contest_id": "hhkb2020",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 300.0,
        "length": 221,
        "result": "AC",
        "execution_time": 182
    }

--- atcoder.jp/PyPy3 (7.3.0)/hhkb2020/hhkb2020_c.py
+++ atcoder.jp/PyPy3 (7.3.0)/hhkb2020/hhkb2020_c.py
@@ -2,9 +2,10 @@ N = int(input())
 p = list(map(int,input().split()))
 p_set = set()
 
-ans = [0] * N
-for i in range(N):
-	p_set.add(p[i])
+ans = [0] * (1+N)
+for i in range(1,N+1):
+	p_set.add(p[i-1])
+	ans[i] = min(ans[i-1], p[i-1])
 	while ans[i] in p_set:
 		ans[i] += 1
 	print(ans[i])
