0adee705853f2fd961a1a6dd01de4166549d1608
yu7400ki <yu7400ki@gmail.com>
2022-06-05 16:47:33 +0900

    [RE] abc183/abc183_c PyPy3 (7.3.0) at 2022-06-05 16:47:33
    {
        "id": 32265040,
        "epoch_second": 1654415253,
        "problem_id": "abc183_c",
        "contest_id": "abc183",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 323,
        "result": "RE",
        "execution_time": 75
    }

---
+++ atcoder.jp/PyPy3 (7.3.0)/abc183/abc183_c.py
@@ -0,0 +1,15 @@
+from itertools import permutations
+
+N, K = map(int,input().split())
+T = [list(map(int,input().split())) for _ in range(N)]
+
+cnt = 0
+for way in permutations(list(range(1,N))):
+	time = T[0][way[0]]
+	for i in range(N-2):
+		time += T[way[i]][way[i+1]]
+	time += T[way[i+1]][0]
+	if time == K:
+		cnt += 1
+
+print(cnt)
