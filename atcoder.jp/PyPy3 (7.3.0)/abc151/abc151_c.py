3334c747e851a113040d778cf3fd492fcd8e5063
yu7400ki <yu7400ki@gmail.com>
2022-06-05 17:03:45 +0900

    [AC] abc151/abc151_c PyPy3 (7.3.0) at 2022-06-05 17:03:45
    {
        "id": 32265525,
        "epoch_second": 1654416225,
        "problem_id": "abc151_c",
        "contest_id": "abc151",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 300.0,
        "length": 354,
        "result": "AC",
        "execution_time": 242
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc151/abc151_c.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc151/abc151_c.py
@@ -1,6 +1,9 @@
+from collections import defaultdict
+
 N, M = map(int,input().split())
 
 ac = set()
+wa = defaultdict(int)
 ac_cnt = 0
 wa_cnt = 0
 
@@ -12,6 +15,10 @@ for i in range(M):
 		ac_cnt += 1
 		ac.add(p)
 	elif s == 'WA':
-		wa_cnt += 1
+		wa[p] += 1
+
+for j in wa:
+	if j in ac:
+		wa_cnt += wa[j]
 
 print(ac_cnt, wa_cnt)
