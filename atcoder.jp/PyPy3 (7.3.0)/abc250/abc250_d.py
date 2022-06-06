d0274145a7133f1d74cedabed37bdc9d9e9752b9
yu7400ki <yu7400ki@gmail.com>
2022-06-06 15:51:18 +0900

    [TLE] abc250/abc250_d PyPy3 (7.3.0) at 2022-06-06 15:51:18
    {
        "id": 32282904,
        "epoch_second": 1654498278,
        "problem_id": "abc250_d",
        "contest_id": "abc250",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 522,
        "result": "TLE",
        "execution_time": 2207
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc250/abc250_d.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc250/abc250_d.py
@@ -1,26 +1,22 @@
-n = int(input())
+N = int(input())
 
-x = int(pow(n,1/3))+1
-is_prime = [True] * (x+1)
-is_prime[0] = is_prime[1] = False
-for p in range(2, int(x ** 0.5) + 1):
-    if is_prime[p]:
-        for i in range(p*p, x+1, p):
-            is_prime[i] = False
-primes =  [p for p in range(x + 1) if is_prime[p]]
+def prime(n):
+    is_prime = [True] * (n+1)
+    is_prime[0] = is_prime[1] = False
+    for p in range(2,int(n**0.5)+1):
+        if is_prime[p]:
+            for i in range(p*p, n+1, p):
+                is_prime[i] = False
+    return [i for i in range(n+1) if is_prime[i]]
 
-primes = [0] + primes + [0]
+limit = int(pow(N, 1/3))+1
+primes = prime(limit)
 
 cnt = 0
-for i in range(1,len(primes)-1):
-	ok = i
-	ng = len(primes) - 1
-	while ng - ok > 1:
-		mid = (ok + ng) // 2
-		if primes[mid] ** 3 * primes[i] <= n:
-			ok = mid
-		else:
-			ng = mid
-	cnt += ok - i
+length = len(primes)
+for i in range(length):
+    for j in range(i+1, length):
+        if primes[j] ** 3 * primes[i] <= N:
+            cnt += 1
 
 print(cnt)
