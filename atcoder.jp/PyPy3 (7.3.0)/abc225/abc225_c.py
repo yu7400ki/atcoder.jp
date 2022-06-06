fe6c3677c693b6461e91f6a75847b64bbc30f0a1
yu7400ki <yu7400ki@gmail.com>
2022-06-06 16:37:20 +0900

    [WA] abc225/abc225_c PyPy3 (7.3.0) at 2022-06-06 16:37:20
    {
        "id": 32284035,
        "epoch_second": 1654501040,
        "problem_id": "abc225_c",
        "contest_id": "abc225",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 516,
        "result": "WA",
        "execution_time": 105
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc225/abc225_c.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc225/abc225_c.py
@@ -1,12 +1,20 @@
 N, M = map(int,input().split())
 
+LIMIT  = 10**100
+
 for i in range(N):
     row = list(map(int,input().split()))
     if i != 0:
         if head != row[0] - 7:
             print('No')
             exit()
+    else:
+        j = row[0] % 7
+        if j == 0: j = 7
     head = row[0]
+    if (head - j) // 7 + 1 > LIMIT:
+        print('No')
+        exit()
     for i in range(M-1):
         if row[i] == row[i+1]-1:
             pass
