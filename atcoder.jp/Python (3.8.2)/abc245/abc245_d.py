2d31c84f03c07ddfd248edbf113dd33e6104cdd8
yu7400ki <yu7400ki@gmail.com>
2022-06-09 02:05:45 +0900

    [AC] abc245/abc245_d Python (3.8.2) at 2022-06-09 02:05:45
    {
        "id": 32326239,
        "epoch_second": 1654707945,
        "problem_id": "abc245_d",
        "contest_id": "abc245",
        "user_id": "yu7400ki",
        "language": "Python (3.8.2)",
        "point": 400.0,
        "length": 262,
        "result": "AC",
        "execution_time": 27
    }

--- atcoder.jp/Python (3.8.2)/abc245/abc245_d.py
+++ atcoder.jp/Python (3.8.2)/abc245/abc245_d.py
@@ -1,12 +1,12 @@
 N, M = map(int,input().split())
-A = list(map(int,input().split()))
-C = list(map(int,input().split()))
+A = list(map(int,input().split()))[::-1]
+C = list(map(int,input().split()))[::-1]
 
 ans = [0] * (M+1)
 for i in range(M+1):
 	div = C[i] // A[0]
 	ans[i] = div
 	for j in range(N+1):
-		C[j+i] -= A[j] * div
+		C[i+j] -= A[j] * div
 
-print(*ans)
+print(*ans[::-1])
