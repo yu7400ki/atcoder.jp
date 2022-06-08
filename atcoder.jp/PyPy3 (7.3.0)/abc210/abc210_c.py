334762aec7c897026c25e727513cb06d2f4a731b
yu7400ki <yu7400ki@gmail.com>
2022-06-08 21:44:02 +0900

    [WA] abc210/abc210_c PyPy3 (7.3.0) at 2022-06-08 21:44:02
    {
        "id": 32322535,
        "epoch_second": 1654692242,
        "problem_id": "abc210_c",
        "contest_id": "abc210",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 306,
        "result": "WA",
        "execution_time": 186
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc210/abc210_c.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc210/abc210_c.py
@@ -1,14 +1,17 @@
-from collections import deque
+from collections import defaultdict
 
 N, K = map(int,input().split())
 c = list(map(int,input().split()))
 
-choose = deque(c[:K])
-ans = len(set(choose))
+choose = defaultdict(int)
 
-for i in range(K, N):
-	choose.popleft()
-	choose.append(c[i])
-	ans = max(ans, len(set(choose)))
+for i in range(K):
+	choose[c[i]] += 1
+
+ans = len(choose)
+for i in range(K,N):
+	choose[c[i-K]] -= 1
+	choose[c[i]] += 1
+	ans = max(ans, len(choose))
 
 print(ans)
