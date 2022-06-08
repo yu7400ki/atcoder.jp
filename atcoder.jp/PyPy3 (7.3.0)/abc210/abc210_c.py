188c9cf09cf3ccc3738e3fa8a028227573301eb7
yu7400ki <yu7400ki@gmail.com>
2022-06-08 21:48:00 +0900

    [AC] abc210/abc210_c PyPy3 (7.3.0) at 2022-06-08 21:48:00
    {
        "id": 32322620,
        "epoch_second": 1654692480,
        "problem_id": "abc210_c",
        "contest_id": "abc210",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 300.0,
        "length": 395,
        "result": "AC",
        "execution_time": 195
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc210/abc210_c.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc210/abc210_c.py
@@ -9,9 +9,14 @@ for i in range(K):
 	choose[c[i]] += 1
 
 ans = len(choose)
+length = ans
 for i in range(K,N):
 	choose[c[i-K]] -= 1
+	if choose[c[i-K]] == 0:
+		length -= 1
 	choose[c[i]] += 1
-	ans = max(ans, len(choose))
+	if choose[c[i]] == 1:
+		length += 1
+	ans = max(ans, length)
 
 print(ans)
