d917f0c4b6756eaa0a8c9e404fd38f96240117e4
yu7400ki <yu7400ki@gmail.com>
2022-06-09 01:47:36 +0900

    [AC] abc245/abc245_c Python (3.8.2) at 2022-06-09 01:47:36
    {
        "id": 32326116,
        "epoch_second": 1654706856,
        "problem_id": "abc245_c",
        "contest_id": "abc245",
        "user_id": "yu7400ki",
        "language": "Python (3.8.2)",
        "point": 300.0,
        "length": 459,
        "result": "AC",
        "execution_time": 308
    }

---
+++ atcoder.jp/Python (3.8.2)/abc245/abc245_c.py
@@ -0,0 +1,22 @@
+N, K = map(int,input().split())
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+
+dp = [False] * N
+ep = [False] * N
+dp[0] = True
+ep[0] = True
+
+for i in range(1,N):
+	if dp[i-1]:
+		if abs(A[i-1] - A[i]) <= K:
+			dp[i] = True
+		if abs(A[i-1] - B[i]) <= K:
+			ep[i] = True
+	if ep[i-1]:
+		if abs(B[i-1] - A[i]) <= K:
+			dp[i] = True
+		if abs(B[i-1] - B[i]) <= K:
+			ep[i] = True
+
+print('Yes' if dp[-1] or ep[-1] else 'No')
