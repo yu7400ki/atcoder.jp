e1351a29ba84dcdafa1bd8334cd1f9f6b0e5954d
yu7400ki <yu7400ki@gmail.com>
2022-06-11 22:25:40 +0900

    [WA] abc255/abc255_b PyPy3 (7.3.0) at 2022-06-11 22:25:40
    {
        "id": 32409385,
        "epoch_second": 1654953940,
        "problem_id": "abc255_b",
        "contest_id": "abc255",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 475,
        "result": "WA",
        "execution_time": 375
    }

---
+++ atcoder.jp/PyPy3 (7.3.0)/abc255/abc255_b.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+N, K = map(int,input().split())
+A = list(map(int,input().split()))
+XY = [tuple(map(int,input().split())) for _ in range(N)]
+
+dist = defaultdict(lambda : float('inf'))
+for i in A:
+	i -= 1
+	xy1 = XY[i]
+	for j in range(N):
+		if j+1 in A:
+			continue
+		xy2 = XY[j]
+		d = (xy1[0] - xy2[0]) ** 2 + (xy1[1] - xy2[1]) ** 2
+		dist[XY[j]] = min(dist[XY[j]], d)
+
+ans = 0
+for d in dist.values():
+	ans = max(ans, d)
+
+print(d**0.5)
