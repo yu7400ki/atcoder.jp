a36b049617c61e5b3521892bcbaa202d6420796e
yu7400ki <yu7400ki@gmail.com>
2022-06-13 00:11:03 +0900

    [AC] abc198/abc198_c PyPy3 (7.3.0) at 2022-06-13 00:11:03
    {
        "id": 32442778,
        "epoch_second": 1655046663,
        "problem_id": "abc198_c",
        "contest_id": "abc198",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 300.0,
        "length": 162,
        "result": "AC",
        "execution_time": 65
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc198/abc198_c.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc198/abc198_c.py
@@ -2,7 +2,9 @@ R, X, Y = map(int,input().split())
 
 dist = (X ** 2 + Y ** 2) ** 0.5
 
-if dist > R:
-	print(int(-(dist // -R)))
-else:
+if dist == R:
+	print(1)
+elif dist <= 2*R:
 	print(2)
+else:
+	print(int(-(dist // -R)))
