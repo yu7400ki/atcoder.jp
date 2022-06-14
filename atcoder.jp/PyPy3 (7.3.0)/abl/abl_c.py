9fd28f0570a27623195b45b3512bc1a097dd3dc9
yu7400ki <yu7400ki@gmail.com>
2022-06-15 00:10:38 +0900

    [TLE] abl/abl_c PyPy3 (7.3.0) at 2022-06-15 00:10:38
    {
        "id": 32475869,
        "epoch_second": 1655219438,
        "problem_id": "abl_c",
        "contest_id": "abl",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 351,
        "result": "TLE",
        "execution_time": 2208
    }

--- atcoder.jp/PyPy3 (7.3.0)/abl/abl_c.py
+++ atcoder.jp/PyPy3 (7.3.0)/abl/abl_c.py
@@ -1,8 +1,18 @@
 N, M = map(int,input().split())
-ct = set(range(1,N+1))
-for i in range(M):
-    A, B = map(int,input().split())
-    ct.discard(A)
-    ct.discard(B)
 
-print(len(ct))
+group = []
+not_seen = set(range(1,N+1))
+for _ in range(M):
+	A, B = map(int,input().split())
+	not_seen.discard(A)
+	not_seen.discard(B)
+	for g in group:
+		if A in g or B in g:
+			g.add(A)
+			g.add(B)
+	else:
+		group.append(set())
+		group[-1].add(A)
+		group[-1].add(B)
+
+print(len(group)-1 + len(not_seen))
