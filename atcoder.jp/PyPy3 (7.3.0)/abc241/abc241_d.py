27f510068a71e363e245fa572a40b666ce4ded8c
yu7400ki <yu7400ki@gmail.com>
2022-06-23 02:11:03 +0900

    [WA] abc241/abc241_d PyPy3 (7.3.0) at 2022-06-23 02:11:03
    {
        "id": 32663821,
        "epoch_second": 1655917863,
        "problem_id": "abc241_d",
        "contest_id": "abc241",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 510,
        "result": "WA",
        "execution_time": 2208
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc241/abc241_d.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc241/abc241_d.py
@@ -1,15 +1,6 @@
-from bisect import insort_left
+from bisect import bisect_right, insort_left
 
 Q = int(input())
-
-def binary_search(ok,ng,func):
-	while abs(ng - ok) > 1:
-		mid = (ok+ng) // 2
-		if func(mid):
-			ok = mid
-		else:
-			ng = mid
-	return ok
 
 A = []
 for _ in range(Q):
@@ -23,14 +14,15 @@ for _ in range(Q):
 	k -= 1
 
 	if query[0] == 2:
-		ok = binary_search(-1, len(A), lambda i: A[i] <= x)
+		ok = bisect_right(A, x)
+		ok -= 1
 		if ok == -1 or ok - k < 0:
 			print(-1)
 		else:
 			print(A[ok-k])
 
 	elif query[0] == 3:
-		ok = binary_search(len(A), -1, lambda i: A[i] >= x)
+		ok = bisect_right(A, x)
 		if ok == -1 or ok + k >= len(A):
 			print(-1)
 		else:
