b3192619070bc906a3ca9c3e00e893014e485ec6
yu7400ki <yu7400ki@gmail.com>
2022-06-23 02:18:09 +0900

    [TLE] abc241/abc241_d PyPy3 (7.3.0) at 2022-06-23 02:18:09
    {
        "id": 32663895,
        "epoch_second": 1655918289,
        "problem_id": "abc241_d",
        "contest_id": "abc241",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 740,
        "result": "TLE",
        "execution_time": 2208
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc241/abc241_d.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc241/abc241_d.py
@@ -1,29 +1,39 @@
-from bisect import bisect_right, insort_left
+from bisect import insort_left
 
 Q = int(input())
+half = 10**18 // 2
+
+def binary_search(ok,ng,func):
+	while abs(ng - ok) > 1:
+		mid = (ok+ng) // 2
+		if func(mid):
+			ok = mid
+		else:
+			ng = mid
+	return ok
 
 A = []
 for _ in range(Q):
 	query = list(map(int, input().split()))
 	if query[0] == 1:
-		x = int(query[1])
+		x = int(query[1]) - half
 		insort_left(A, x)
 		continue
 
 	x, k = query[1], query[2]
+	x -= half
 	k -= 1
 
 	if query[0] == 2:
-		ok = bisect_right(A, x)
-		ok -= 1
+		ok = binary_search(-1, len(A), lambda i: A[i] <= x)
 		if ok == -1 or ok - k < 0:
 			print(-1)
 		else:
-			print(A[ok-k])
+			print(A[ok-k]+half)
 
 	elif query[0] == 3:
-		ok = bisect_right(A, x)
-		if ok == -1 or ok + k >= len(A):
+		ok = binary_search(len(A), -1, lambda i: A[i] >= x)
+		if ok == len(A) or ok + k >= len(A):
 			print(-1)
 		else:
-			print(A[ok+k])
+			print(A[ok+k]+half)
