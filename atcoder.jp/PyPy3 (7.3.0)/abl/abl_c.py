6a1b88e89a3e6924aa264654229f07876bba76f6
yu7400ki <yu7400ki@gmail.com>
2022-06-25 13:51:34 +0900

    [WA] abl/abl_c PyPy3 (7.3.0) at 2022-06-25 13:51:34
    {
        "id": 32699775,
        "epoch_second": 1656132694,
        "problem_id": "abl_c",
        "contest_id": "abl",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 836,
        "result": "WA",
        "execution_time": 313
    }

--- atcoder.jp/PyPy3 (7.3.0)/abl/abl_c.py
+++ atcoder.jp/PyPy3 (7.3.0)/abl/abl_c.py
@@ -1,18 +1,35 @@
+class UnionFind:
+	def __init__(self, n):
+		self.n = n
+		self.parent = [i for i in range(n)]
+		self.size = [1 for _ in range(n)]
+	def find(self, x):
+		if self.parent[x] == x:
+			return x
+		else:
+			self.parent[x] = self.find(self.parent[x])
+			return self.parent[x]
+	def union(self, x, y):
+		x = self.find(x)
+		y = self.find(y)
+		if x == y: return
+		if self.size[x] > self.size[y]:
+			self.parent[y] = x
+			self.size[x] += self.size[y]
+		else:
+			self.parent[x] = y
+			self.size[y] += self.size[x]
+	def same(self, x, y):
+		return self.find(x) == self.find(y)
+	def count(self):
+		return len(set(self.parent))
+	def size(self, x):
+		return self.size[self.find(x)]
+
 N, M = map(int,input().split())
-
-par = list(range(N+1))
-
-def merge(n):
-	if par[n] == n:
-		return n
-	else:
-		par[n] = merge(par[n])
-		return par[n]
-
+uf = UnionFind(N)
 for _ in range(M):
 	A, B = map(int,input().split())
-	par[B] = par[A]
-	merge(A)
-	merge(B)
+	uf.union(A-1,B-1)
 
-print(len(set(par))-2)
+print(uf.count()-1)
