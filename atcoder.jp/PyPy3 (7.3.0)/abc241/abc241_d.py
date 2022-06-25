7044533ea2d8b06bd9f8ac8925278698450b2aca
yu7400ki <yu7400ki@gmail.com>
2022-06-26 02:28:37 +0900

    [TLE] abc241/abc241_d PyPy3 (7.3.0) at 2022-06-26 02:28:37
    {
        "id": 32757809,
        "epoch_second": 1656178117,
        "problem_id": "abc241_d",
        "contest_id": "abc241",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 868,
        "result": "TLE",
        "execution_time": 2208
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc241/abc241_d.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc241/abc241_d.py
@@ -1,39 +1,39 @@
 from bisect import insort_left
+from sys import stdin
+
+def input():
+	return stdin.readline().strip()
+
+def binary_search(ok, ng, func):
+    while abs(ng - ok) > 1:
+        mid = (ok + ng) // 2
+        if func(mid):
+            ok = mid
+        else:
+            ng = mid
+    return ok
+
 
 Q = int(input())
-half = 10**18 // 2
-
-def binary_search(ok,ng,func):
-	while abs(ng - ok) > 1:
-		mid = (ok+ng) // 2
-		if func(mid):
-			ok = mid
-		else:
-			ng = mid
-	return ok
-
 A = []
 for _ in range(Q):
-	query = list(map(int, input().split()))
-	if query[0] == 1:
-		x = int(query[1]) - half
-		insort_left(A, x)
-		continue
+    query = list(map(int, input().split()))
+    x = query[1]
+    if query[0] == 1:
+        insort_left(A, x)
+        continue
 
-	x, k = query[1], query[2]
-	x -= half
-	k -= 1
+    k = query[2] - 1
+    if query[0] == 2:
+        idx = binary_search(-1, len(A), lambda i: A[i] <= x)
+        if idx - k < 0:
+            print(-1)
+        else:
+            print(A[idx - k])
 
-	if query[0] == 2:
-		ok = binary_search(-1, len(A), lambda i: A[i] <= x)
-		if ok == -1 or ok - k < 0:
-			print(-1)
-		else:
-			print(A[ok-k]+half)
-
-	elif query[0] == 3:
-		ok = binary_search(len(A), -1, lambda i: A[i] >= x)
-		if ok == len(A) or ok + k >= len(A):
-			print(-1)
-		else:
-			print(A[ok+k]+half)
+    elif query[0] == 3:
+        idx = binary_search(len(A), -1, lambda i: A[i] >= x)
+        if idx + k >= len(A):
+            print(-1)
+        else:
+            print(A[idx + k])
