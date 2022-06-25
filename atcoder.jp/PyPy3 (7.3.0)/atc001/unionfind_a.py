0333b302483e95a50b5b27f3b08f46b4492807af
yu7400ki <yu7400ki@gmail.com>
2022-06-25 13:47:21 +0900

    [AC] atc001/unionfind_a PyPy3 (7.3.0) at 2022-06-25 13:47:21
    {
        "id": 32699717,
        "epoch_second": 1656132441,
        "problem_id": "unionfind_a",
        "contest_id": "atc001",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 100.0,
        "length": 909,
        "result": "AC",
        "execution_time": 736
    }

--- atcoder.jp/PyPy3 (7.3.0)/atc001/unionfind_a.py
+++ atcoder.jp/PyPy3 (7.3.0)/atc001/unionfind_a.py
@@ -1,40 +1,37 @@
 class UnionFind:
 	def __init__(self, n):
 		self.n = n
-		self.par = [i for i in range(n)]
-		self.rank = [0 for i in range(n)]
+		self.parent = [i for i in range(n)]
+		self.size = [1 for _ in range(n)]
 	def find(self, x):
-		if self.par[x] == x:
+		if self.parent[x] == x:
 			return x
 		else:
-			self.par[x] = self.find(self.par[x])
-			return self.par[x]
-	def unite(self, x, y):
+			self.parent[x] = self.find(self.parent[x])
+			return self.parent[x]
+	def union(self, x, y):
 		x = self.find(x)
 		y = self.find(y)
 		if x == y: return
-		if self.rank[x] < self.rank[y]:
-			self.par[x] = y
+		if self.size[x] > self.size[y]:
+			self.parent[y] = x
+			self.size[x] += self.size[y]
 		else:
-			self.par[y] = x
-			if self.rank[x] == self.rank[y]:
-				self.rank[x] += 1
+			self.parent[x] = y
+			self.size[y] += self.size[x]
 	def same(self, x, y):
 		return self.find(x) == self.find(y)
-	def size(self, x):
-		return len(self.same(x))
 	def count(self):
-		return len(self.par)
-	def show(self):
-		print(self.par)
-		print(self.rank)
+		return len(set(self.parent))
+	def size(self, x):
+		return self.size[self.find(x)]
 
 N, Q = map(int,input().split())
 tree = UnionFind(N)
 for _ in range(Q):
 	P, A, B = map(int,input().split())
 	if P == 0:
-		tree.unite(A,B)
+		tree.union(A,B)
 	elif P == 1:
 		if tree.same(A,B):
 			print('Yes')
