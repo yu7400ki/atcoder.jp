c0c5acf7f85d0c5ec3bdcf1da8be23cda3c888e2
yu7400ki <yu7400ki@gmail.com>
2022-07-02 21:21:17 +0900

    [WA] abc258/abc258_b PyPy3 (7.3.0) at 2022-07-02 21:21:17
    {
        "id": 32900748,
        "epoch_second": 1656764477,
        "problem_id": "abc258_b",
        "contest_id": "abc258",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 673,
        "result": "WA",
        "execution_time": 67
    }

---
+++ atcoder.jp/PyPy3 (7.3.0)/abc258/abc258_b.py
@@ -0,0 +1,23 @@
+N = int(input())
+A = [list(input()) for _ in range(N)]
+
+ans = 0
+for i in range(N):
+    for j in range(N):
+        res = []
+        for k in range(N):
+            res.append(A[i][(j+k)%N])
+        ans = max(ans, int(''.join(res)))
+        ans = max(ans, int(''.join(reversed(res))))
+        res = []
+        for k in range(N):
+            res.append(A[(i+k)%N][j])
+        ans = max(ans, int(''.join(res)))
+        ans = max(ans, int(''.join(reversed(res))))
+        res = []
+        for k in range(N):
+            res.append(A[(i+k)%N][(j+k)%N])
+        ans = max(ans, int(''.join(res)))
+        ans = max(ans, int(''.join(reversed(res))))
+
+print(ans)
