b5eb07b0331065fb7eb81215f6540e93c65cdb2f
yu7400ki <yu7400ki@gmail.com>
2022-07-03 21:30:55 +0900

    [WA] abc258/abc258_b PyPy3 (7.3.0) at 2022-07-03 21:30:55
    {
        "id": 32966198,
        "epoch_second": 1656851455,
        "problem_id": "abc258_b",
        "contest_id": "abc258",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 504,
        "result": "WA",
        "execution_time": 62
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc258/abc258_b.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc258/abc258_b.py
@@ -4,25 +4,15 @@ A = [list(input()) for _ in range(N)]
 ans = 0
 for i in range(N):
     for j in range(N):
-        res = []
+        res1 = []
+        res2 = []
+        res3 = []
+        res4 = []
         for k in range(N):
-            res.append(A[i][(j+k)%N])
-        ans = max(ans, int(''.join(res)))
-        ans = max(ans, int(''.join(reversed(res))))
-        res = []
-        for k in range(N):
-            res.append(A[(i+k)%N][j])
-        ans = max(ans, int(''.join(res)))
-        ans = max(ans, int(''.join(reversed(res))))
-        res = []
-        for k in range(N):
-            res.append(A[(i+k)%N][(j+k)%N])
-        ans = max(ans, int(''.join(res)))
-        ans = max(ans, int(''.join(reversed(res))))
-        res = []
-        for k in range(N):
-            res.append(A[(i-k)%N][(j+k)%N])
-        ans = max(ans, int(''.join(res)))
-        ans = max(ans, int(''.join(reversed(res))))
+            res1.append(A[i][(j+k)%N])
+            res2.append(A[(i+k)%N][j])
+            res3.append(A[(i+k)%N][(j+k)%N])
+            res4.append(A[(i-k)%N][(j+k)%N])
+        ans = max(ans, int(''.join(res1)), int(''.join(res2)), int(''.join(res3)), int(''.join(res4)))
 
 print(ans)
