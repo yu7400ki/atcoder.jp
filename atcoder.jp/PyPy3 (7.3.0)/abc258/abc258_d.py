c0aff9ab1d39d05f3ffb1768c4cf8490bf96df74
yu7400ki <yu7400ki@gmail.com>
2022-07-03 17:46:04 +0900

    [AC] abc258/abc258_d PyPy3 (7.3.0) at 2022-07-03 17:46:04
    {
        "id": 32959256,
        "epoch_second": 1656837964,
        "problem_id": "abc258_d",
        "contest_id": "abc258",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 400.0,
        "length": 398,
        "result": "AC",
        "execution_time": 224
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc258/abc258_d.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc258/abc258_d.py
@@ -1,12 +1,16 @@
-N, X = map(int,input().split())
-AB = [list(map(int,input().split())) for _ in range(N)]
+from sys import stdin
+input = lambda : stdin.readline().rstrip()
 
-accum = [0] * (N + 1)
+N, X = map(int,input().split())
+Y = min(N, X)
+AB = [list(map(int,input().split())) for _ in range(Y)]
+
+accum = [0] * (Y + 1)
 for i, n in enumerate(AB):
     accum[i+1] = accum[i] + n[0] + n[1]
 
 ans = float('inf')
-for i in range(min(N, X)):
+for i in range(Y):
     i += 1
     a = accum[i]
     ans = min(a + (X - i) * AB[i-1][1], ans)
