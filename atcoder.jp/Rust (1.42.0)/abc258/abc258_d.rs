3295f063d22056bc29f59f38fd238774e0b7b836
yu7400ki <yu7400ki@gmail.com>
2022-07-06 22:34:21 +0900

    [CE] abc258/abc258_d Rust (1.42.0) at 2022-07-06 22:34:21
    {
        "id": 33022597,
        "epoch_second": 1657114461,
        "problem_id": "abc258_d",
        "contest_id": "abc258",
        "user_id": "yu7400ki",
        "language": "Rust (1.42.0)",
        "point": 0.0,
        "length": 372,
        "result": "CE",
        "execution_time": null
    }

---
+++ atcoder.jp/Rust (1.42.0)/abc258/abc258_d.rs
@@ -0,0 +1,19 @@
+use proconio::{input, fastout};
+use std::cmp::min;
+
+#[fastout]
+fn main() {
+    let mut now = 0;
+    let mut ans = usize::MAX;
+    input! {
+        n: usize,
+        x: usize,
+        t: [(usize, usize); n],
+    };
+    for i in 0..(min(n,x)) {
+        let (a, b) = t[i];
+        now += a + b;
+        ans = min(ans, now + (x - i - 1) * b);
+    }
+    println!("{}", ans);
+}
