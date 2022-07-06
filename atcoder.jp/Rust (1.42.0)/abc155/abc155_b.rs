803aa97384f00885da850b10a673bf860220e135
yu7400ki <yu7400ki@gmail.com>
2022-07-07 00:13:03 +0900

    [AC] abc155/abc155_b Rust (1.42.0) at 2022-07-07 00:13:03
    {
        "id": 33024714,
        "epoch_second": 1657120383,
        "problem_id": "abc155_b",
        "contest_id": "abc155",
        "user_id": "yu7400ki",
        "language": "Rust (1.42.0)",
        "point": 200.0,
        "length": 353,
        "result": "AC",
        "execution_time": 6
    }

--- atcoder.jp/Rust (1.42.0)/abc155/abc155_b.rs
+++ atcoder.jp/Rust (1.42.0)/abc155/abc155_b.rs
@@ -2,7 +2,6 @@ use proconio::{input, fastout};
 
 #[fastout]
 fn main() {
-    let mut flag = true;
     input! {
         n: usize,
         a: [usize; n],
@@ -10,13 +9,10 @@ fn main() {
     for num in a.iter() {
         if num % 2 == 0 {
             if !(num % 3 == 0 || num % 5 == 0) {
-                flag = false
+                println!("DENIED");
+                return;
             }
         }
     };
-    if flag {
-        println!("APPROVED");
-    } else {
-        println!("DENIED")
-    }
+    println!("APPROVED");
 }
