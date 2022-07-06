0fcd5a864ca145027c4aabd6d5f01e06a2daf741
yu7400ki <yu7400ki@gmail.com>
2022-07-06 22:18:46 +0900

    [AC] abc258/abc258_c Rust (1.42.0) at 2022-07-06 22:18:46
    {
        "id": 33022285,
        "epoch_second": 1657113526,
        "problem_id": "abc258_c",
        "contest_id": "abc258",
        "user_id": "yu7400ki",
        "language": "Rust (1.42.0)",
        "point": 300.0,
        "length": 609,
        "result": "AC",
        "execution_time": 58
    }

--- atcoder.jp/Rust (1.42.0)/abc258/abc258_c.rs
+++ atcoder.jp/Rust (1.42.0)/abc258/abc258_c.rs
@@ -1,22 +1,30 @@
-use proconio::{input};
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     let mut idx = 0;
     input! {
         n: usize,
         q: usize,
         s: String,
+        queries: [(usize, usize); q],
     };
     let s = s.chars().collect::<Vec<char>>();
-    for _ in 0..q {
-        input! {
-            m: usize,
-            x: usize,
-        };
-        if m == 1 {
-            idx = (n - x + idx) % n;
-        } else if m == 2 {
-            println!("{}", s[(idx + x - 1) % n]);
+    let mut ans: Vec<char> = vec![];
+    for (m, x) in &queries {
+        match m {
+            1 => {
+                idx = (idx + n - x) % n
+            }
+            2 => {
+                ans.push(s[(idx + x - 1) % n]);
+            }
+            _ => {
+                unreachable!();
+            }
         }
-    };
+    }
+    for a in ans {
+        println!("{}", a);
+    }
 }
