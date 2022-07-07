eabbe1070138b8437f2eab78201c01704dacc69f
yu7400ki <yu7400ki@gmail.com>
2022-07-07 15:58:27 +0900

    [CE] abc250/abc250_d Rust (1.42.0) at 2022-07-07 15:58:27
    {
        "id": 33035083,
        "epoch_second": 1657177107,
        "problem_id": "abc250_d",
        "contest_id": "abc250",
        "user_id": "yu7400ki",
        "language": "Rust (1.42.0)",
        "point": 0.0,
        "length": 1029,
        "result": "CE",
        "execution_time": null
    }

---
+++ atcoder.jp/Rust (1.42.0)/abc250/abc250_d.rs
@@ -0,0 +1,50 @@
+use proconio::{fastout, input};
+
+fn prime(n: usize) -> Vec<usize> {
+    let mut prime = vec![true; n + 1];
+    prime[0] = false;
+    prime[1] = false;
+    for i in 2..(n + 1) {
+        if prime[i] {
+            for j in (i * i..(n + 1)).step_by(i) {
+                prime[j] = false;
+            }
+        }
+    }
+    prime
+        .iter()
+        .enumerate()
+        .filter(|&(_, &p)| p)
+        .map(|(i, _)| i)
+        .collect()
+}
+
+const LIMIT: usize = 10_usize.pow(6);
+
+#[fastout]
+fn main() {
+    let primes = prime(LIMIT);
+    let mut ans = 0;
+    input! {
+        n: usize,
+    };
+
+    for i in 1..primes.len() {
+        let a = primes[i].pow(3);
+        if a * 2 > n {
+            break;
+        }
+        let mut ok: usize = 0;
+        let mut ng: usize = i;
+        while (ng - ok) > 1 {
+            let mid = (ok + ng) / 2;
+            if a * primes[mid] <= n {
+                ok = mid;
+            } else {
+                ng = mid;
+            }
+        };
+        ans += ok + 1;
+    }
+    println!("{}", ans);
+}
