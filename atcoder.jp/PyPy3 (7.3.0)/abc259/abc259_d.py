73e7494da787e5e97abe90549d1377c85dc77e2e
yu7400ki <yu7400ki@gmail.com>
2022-07-09 23:09:13 +0900

    [AC] abc259/abc259_d PyPy3 (7.3.0) at 2022-07-09 23:09:13
    {
        "id": 33126956,
        "epoch_second": 1657375753,
        "problem_id": "abc259_d",
        "contest_id": "abc259",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 400.0,
        "length": 1215,
        "result": "AC",
        "execution_time": 678
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc259/abc259_d.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc259/abc259_d.py
@@ -20,15 +20,15 @@ def bfs(n):
     return dic
 
 def is_collision(x1, y1, r1, x2, y2, r2):
-    return not ((x1-x2)**2 + (y1-y2)**2 < (r1-r2)**2 or (x1-x2)**2 + (y1-y2)**2 > (r1+r2)**2)
+    return ((x1-x2)**2 + (y1-y2)**2 >= (r1-r2)**2) and ((x1-x2)**2 + (y1-y2)**2 <= (r1+r2)**2)
 
 start = -1
 end = -1
 
 for i in range(N):
+    if (sx - xyr[i][0])**2 + (sy - xyr[i][1])**2 == xyr[i][2]**2: start = i
+    if (tx - xyr[i][0])**2 + (ty - xyr[i][1])**2 == xyr[i][2]**2: end = i
     for j in range(i+1, N):
-        if (sx - xyr[i][0])**2 + (sy - xyr[i][1])**2 == xyr[i][2]**2: start = i
-        if (tx - xyr[i][0])**2 + (ty - xyr[i][1])**2 == xyr[i][2]**2: end = i
         if is_collision(xyr[i][0], xyr[i][1], xyr[i][2], xyr[j][0], xyr[j][1], xyr[j][2]):
             graph[i].append(j)
             graph[j].append(i)
