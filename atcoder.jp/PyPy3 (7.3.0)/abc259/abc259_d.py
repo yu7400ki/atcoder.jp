674aa3cdf44e8780fc1a97c07e29c5063c151c19
yu7400ki <yu7400ki@gmail.com>
2022-07-09 22:42:19 +0900

    [WA] abc259/abc259_d PyPy3 (7.3.0) at 2022-07-09 22:42:19
    {
        "id": 33123145,
        "epoch_second": 1657374139,
        "problem_id": "abc259_d",
        "contest_id": "abc259",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 1223,
        "result": "WA",
        "execution_time": 985
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc259/abc259_d.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc259/abc259_d.py
@@ -20,7 +20,7 @@ def bfs(n):
     return dic
 
 def is_collision(x1, y1, r1, x2, y2, r2):
-    return (x1-x2)**2 + (y1-y2)**2 >= (r1-r2)**2 and (x1-x2)**2 + (y1-y2)**2 <= (r1+r2)**2
+    return not ((x1-x2)**2 + (y1-y2)**2 < (r1-r2)**2 and (x1-x2)**2 + (y1-y2)**2 > (r1+r2)**2)
 
 start = -1
 end = -1
