54a85309bbc1541179e864d8f808e0e58adf8b7e
yu7400ki <yu7400ki@gmail.com>
2022-08-01 22:21:55 +0900

    [AC] code-festival-2016-qualb/codefestival_2016_qualB_b Python (3.8.2) at 2022-08-01 22:21:55
    {
        "id": 33713748,
        "epoch_second": 1659360115,
        "problem_id": "codefestival_2016_qualB_b",
        "contest_id": "code-festival-2016-qualb",
        "user_id": "yu7400ki",
        "language": "Python (3.8.2)",
        "point": 200.0,
        "length": 534,
        "result": "AC",
        "execution_time": 59
    }

--- atcoder.jp/Python (3.8.2)/code-festival-2016-qualb/codefestival_2016_qualB_b.py
+++ atcoder.jp/Python (3.8.2)/code-festival-2016-qualb/codefestival_2016_qualB_b.py
@@ -1,22 +1,23 @@
-N, A, B = map(int,input().split())
-S = input()
+def main():
+    _, A, B = map(int,input().split())
+    S = input()
 
-limit = A + B
-foreigner = 0
-passed = 0
-for s in S:
-    if s == 'a':
-        if passed < limit:
-            print('Yes')
-            passed += 1
-        else:
-            print('No')
-    elif s == 'b':
-        if passed < limit and foreigner < B:
-            print('Yes')
-            passed += 1
-            foreigner += 1
-        else:
-            print('No')
-    else:
+    limit = A + B
+    foreigner = 0
+    passed = 0
+    for s in S:
+        if s == 'a':
+            if passed < limit:
+                print('Yes')
+                passed += 1
+                continue
+        elif s == 'b':
+            if passed < limit and foreigner < B:
+                print('Yes')
+                passed += 1
+                foreigner += 1
+                continue
         print('No')
+
+if __name__ == '__main__':
+    main()
