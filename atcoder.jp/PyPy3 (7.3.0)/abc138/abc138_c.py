af2e1bd676fcd8c8481928f6ce58ba7059a28ee6
yu7400ki <yu7400ki@gmail.com>
2022-08-07 23:03:25 +0900

    [AC] abc138/abc138_c PyPy3 (7.3.0) at 2022-08-07 23:03:25
    {
        "id": 33864728,
        "epoch_second": 1659881005,
        "problem_id": "abc138_c",
        "contest_id": "abc138",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 300.0,
        "length": 191,
        "result": "AC",
        "execution_time": 67
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc138/abc138_c.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc138/abc138_c.py
@@ -1,17 +1,11 @@
 N = int(input())
 v = list(map(int, input().split()))
 
+from collections import deque
+v = deque(sorted(v))
+
 while len(v) > 1:
-    mi = float('inf')
-    a = 0
-    b = 0
-    for i in range(len(v)):
-        for j in range(i+1, len(v)):
-            if abs(v[i] - v[j]) < mi:
-                mi = abs(v[i] - v[j])
-                a = i
-                b = j
-    v[a] = (v[a] + v[b]) / 2
-    del v[b]
+    v[1] = (v[0] + v[1]) / 2
+    v.popleft()
 
 print(v[0])
