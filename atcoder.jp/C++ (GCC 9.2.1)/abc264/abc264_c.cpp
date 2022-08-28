a737d7333be22290611cbd5eace819ac03a3b8b3
yu7400ki <yu7400ki@gmail.com>
2022-08-29 00:39:35 +0900

    [AC] abc264/abc264_c C++ (GCC 9.2.1) at 2022-08-29 00:39:35
    {
        "id": 34430848,
        "epoch_second": 1661701175,
        "problem_id": "abc264_c",
        "contest_id": "abc264",
        "user_id": "yu7400ki",
        "language": "C++ (GCC 9.2.1)",
        "point": 300.0,
        "length": 1273,
        "result": "AC",
        "execution_time": 42
    }

--- atcoder.jp/C++ (GCC 9.2.1)/abc264/abc264_c.cpp
+++ atcoder.jp/C++ (GCC 9.2.1)/abc264/abc264_c.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
 #include <bitset>
 using namespace std;
 
@@ -14,22 +13,24 @@ int main()
     vector<vector<int>> B(H2, vector<int>(W2));
     for (vector<int> &b : B) for (int &x : b) cin >> x;
 
-    for (int i = 1; i < pow(2, H1); i++) {
+    for (int i = 1; i < 1 << H1; i++) {
         bitset<10> I(i);
         if (I.count() != H2) continue;
-        for (int j = 1; j < pow(2, W1); j++) {
+        for (int j = 1; j < 1 << W1; j++) {
             bitset<10> J(j);
             if (J.count() != W2) continue;
-            vector<vector<int>> temp;
+            vector<vector<int>> temp(H2, vector<int>(W2));
+            int y = 0;
             for (int k = 0; k < H1; k++) {
+                int x = 0;
                 if (I.test(k)) {
-                    vector<int> temp_h;
                     for (int l = 0; l < W1; l++) {
                         if (J.test(l)) {
-                            temp_h.push_back(A.at(k).at(l));
+                            temp.at(y).at(x) = A.at(k).at(l);
+                            x++;
                         }
                     }
-                    temp.push_back(temp_h);
+                    y++;
                 }
             }
             if (temp == B) {
@@ -38,7 +39,6 @@ int main()
             }
         }
     }
-
     cout << "No" << endl;
 
     return 0;
