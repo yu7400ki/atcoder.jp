db8df511cc5dda03eeee03aee55007f389fd7f3a
yu7400ki <yu7400ki@gmail.com>
2022-08-29 01:51:22 +0900

    [WA] abc263/abc263_c C++ (GCC 9.2.1) at 2022-08-29 01:51:22
    {
        "id": 34431680,
        "epoch_second": 1661705482,
        "problem_id": "abc263_c",
        "contest_id": "abc263",
        "user_id": "yu7400ki",
        "language": "C++ (GCC 9.2.1)",
        "point": 0.0,
        "length": 506,
        "result": "WA",
        "execution_time": 8
    }

---
+++ atcoder.jp/C++ (GCC 9.2.1)/abc263/abc263_c.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void bfs(vector<int> &A, int &N, int &M) {
+    if (A.size() == M - N + 1) {
+        for (int &a : A) cout << a << " ";
+        cout << endl;
+        return;
+    }
+
+    int s = A.size() == 0 ? 1 : A.back() + 1;
+    for (int i = s; i <= M; i++) {
+        A.push_back(i);
+        bfs(A, N, M);
+        A.pop_back();
+    }
+}
+
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+    vector<int> A;
+    bfs(A, N, M);
+    return 0;
+}
