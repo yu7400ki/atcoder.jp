aa18dde702e34087bc5a2aa7da527303f7ddc9ae
yu7400ki <yu7400ki@gmail.com>
2022-08-29 01:13:07 +0900

    [RE] abc263/abc263_a C++ (GCC 9.2.1) at 2022-08-29 01:13:07
    {
        "id": 34431284,
        "epoch_second": 1661703187,
        "problem_id": "abc263_a",
        "contest_id": "abc263",
        "user_id": "yu7400ki",
        "language": "C++ (GCC 9.2.1)",
        "point": 0.0,
        "length": 404,
        "result": "RE",
        "execution_time": 116
    }

---
+++ atcoder.jp/C++ (GCC 9.2.1)/abc263/abc263_a.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    vector<int> cnt(13, 0);
+    for (int i = 0; i < 5; i++) {
+        int x;
+        cin >> x;
+        cnt.at(x)++;
+    }
+
+    bool flag = find(cnt.begin(), cnt.end(), 3) != cnt.end() && find(cnt.begin(), cnt.end(), 2) != cnt.end();
+
+    cout << (flag ? "Yes" : "No") << endl;
+    return 0;
+}
