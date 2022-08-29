77cfafcb844fd4ac35d400c5dbd0b6e3cf5a7d76
yu7400ki <yu7400ki@gmail.com>
2022-08-29 11:38:49 +0900

    [CE] abc262/abc262_b C++ (GCC 9.2.1) at 2022-08-29 11:38:49
    {
        "id": 34435085,
        "epoch_second": 1661740729,
        "problem_id": "abc262_b",
        "contest_id": "abc262",
        "user_id": "yu7400ki",
        "language": "C++ (GCC 9.2.1)",
        "point": 0.0,
        "length": 985,
        "result": "CE",
        "execution_time": null
    }

--- atcoder.jp/C++ (GCC 9.2.1)/abc262/abc262_b.cpp
+++ atcoder.jp/C++ (GCC 9.2.1)/abc262/abc262_b.cpp
@@ -12,22 +12,23 @@ int main()
     {
         int U, V;
         cin >> U >> V;
-        if (graph.count(U) > 0) graph.at(U).emplace(V); else graph.emplace(U, unordered_set<int>{V});
-        if (graph.count(V) > 0) graph.at(V).emplace(U); else graph.emplace(V, unordered_set<int>{U});
+        if (graph.contains(U)) graph.at(U).emplace(V); else graph.emplace(U, unordered_set<int>{V});
+        if (graph.contains(V)) graph.at(V).emplace(U); else graph.emplace(V, unordered_set<int>{U});
     }
 
     int cnt = 0;
     for (int i = 1; i <= N; i++) {
         for (int j = i+1; j <= N; j++) {
             for (int k = j+1; k <= N; k++) {
-                if (graph.count(i) > 0 && graph.count(j) > 0 && graph.count(k) > 0) {
-                    if (graph.at(i).count(j) > 0 && graph.at(j).count(k) > 0 && graph.at(k).count(i) > 0) {
+                if (graph.contains(i) && graph.contains(j) && graph.contains(k)) {
+                    if (graph.at(i).contains(j) && graph.at(j).contains(k) && graph.at(k).contains(i)) {
                         cnt++;
                     }
                 }
             }
         }
     }
+    
     cout << cnt << endl;
 
     return 0;
