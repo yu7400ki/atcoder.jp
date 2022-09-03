21b0218a91c24eb5a6e80c47970d2a02c8fdb1a7
yu7400ki <yu7400ki@gmail.com>
2022-09-03 21:14:42 +0900

    [AC] abc267/abc267_b Python (3.8.2) at 2022-09-03 21:14:42
    {
        "id": 34541569,
        "epoch_second": 1662207282,
        "problem_id": "abc267_b",
        "contest_id": "abc267",
        "user_id": "yu7400ki",
        "language": "Python (3.8.2)",
        "point": 200.0,
        "length": 881,
        "result": "AC",
        "execution_time": 27
    }

--- atcoder.jp/Python (3.8.2)/abc267/abc267_b.py
+++ atcoder.jp/Python (3.8.2)/abc267/abc267_b.py
@@ -10,21 +10,25 @@ for i in range(7):
     for j in range(i+1, 7):
         stand_flag1 = False
         stand_flag2 = False
+        
         for k in c[i]:
             if S[k-1] == "1":
                 stand_flag1 = True
         for k in c[j]:
             if S[k-1] == "1":
                 stand_flag2 = True
+                
         if stand_flag1 and stand_flag2:
             split_flag = False
-            for k in range(i+1, k):
+            
+            for k in range(i+1, j):
                 cnt = 0
                 for l in c[k]:
                     if S[l-1] == "0":
                         cnt += 1
                 if cnt == len(c[k]):
                     split_flag = True
+                    
             if split_flag:
                 print("Yes")
                 exit()
