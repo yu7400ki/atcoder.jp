cf925595d95d05d6bce4abb5a672a8c809e3d52b
yu7400ki <yu7400ki@gmail.com>
2022-09-05 01:01:32 +0900

    [AC] abc267/abc267_d PyPy3 (7.3.0) at 2022-09-05 01:01:32
    {
        "id": 34623601,
        "epoch_second": 1662307292,
        "problem_id": "abc267_d",
        "contest_id": "abc267",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 400.0,
        "length": 287,
        "result": "AC",
        "execution_time": 192
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc267/abc267_d.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc267/abc267_d.py
@@ -5,7 +5,7 @@ dp = [[-float('inf')] * (N+1) for _ in range(M+1)]
 dp[0] = [0] * (N+1)
 
 for i in range(1, M+1):
-    for j in range(1, N-M+1+i):
+    for j in range(i, N-M+1+i):
         dp[i][j] = max(dp[i][j-1], A[j-1] * i + dp[i-1][j-1])
 
 print(max(dp[-1]))
