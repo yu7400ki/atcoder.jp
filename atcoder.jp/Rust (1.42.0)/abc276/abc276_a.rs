eabb3aacd3e910491b35c5b5735d5c1f5b9ab5aa
yu7400ki <yu7400ki@gmail.com>
2022-11-08 21:25:38 +0900

    [AC] abc276/abc276_a Rust (1.42.0) at 2022-11-08 21:25:38
    {
        "id": 36326103,
        "epoch_second": 1667910338,
        "problem_id": "abc276_a",
        "contest_id": "abc276",
        "user_id": "yu7400ki",
        "language": "Rust (1.42.0)",
        "point": 100.0,
        "length": 269,
        "result": "AC",
        "execution_time": 6
    }

---
+++ atcoder.jp/Rust (1.42.0)/abc276/abc276_a.rs
@@ -0,0 +1,17 @@
+use proconio::{fastout, input};
+
+#[fastout]
+fn main() {
+    input! {
+        s: String,
+    }
+    
+    for (i, c) in s.chars().rev().enumerate() {
+        if c == 'a' {
+            println!("{}", s.len() - i);
+            return;
+        }
+    }
+
+    println!("-1");
+}
