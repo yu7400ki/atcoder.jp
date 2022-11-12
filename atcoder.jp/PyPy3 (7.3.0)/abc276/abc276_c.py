013de8e3ce5fa1b82c738f799455950ca0fa26d6
yu7400ki <yu7400ki@gmail.com>
2022-11-13 05:07:38 +0900

    [AC] abc276/abc276_c PyPy3 (7.3.0) at 2022-11-13 05:07:38
    {
        "id": 36459684,
        "epoch_second": 1668283658,
        "problem_id": "abc276_c",
        "contest_id": "abc276",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 300.0,
        "length": 301,
        "result": "AC",
        "execution_time": 60
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc276/abc276_c.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc276/abc276_c.py
@@ -6,7 +6,9 @@ while P[idx1] > P[idx1 - 1]:
     idx1 -= 1
 idx1 -= 1
 
-idx2 = P.index(P[idx1] - 1)
+idx2 = idx1
+while idx2 < N-1 and P[idx2 + 1] < P[idx1]:
+    idx2 += 1
 
 P[idx1], P[idx2] = P[idx2], P[idx1]
 
