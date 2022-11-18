62814e6a4bf0e9e5aad4af87522643b155cedcfa
yu7400ki <yu7400ki@gmail.com>
2022-11-19 01:32:55 +0900

    [WA] abc093/abc093_b Rust (1.42.0) at 2022-11-19 01:32:55
    {
        "id": 36578490,
        "epoch_second": 1668789175,
        "problem_id": "abc093_b",
        "contest_id": "abc093",
        "user_id": "yu7400ki",
        "language": "Rust (1.42.0)",
        "point": 0.0,
        "length": 381,
        "result": "WA",
        "execution_time": 6
    }

---
+++ atcoder.jp/Rust (1.42.0)/abc093/abc093_b.rs
@@ -0,0 +1,23 @@
+use proconio::{fastout, input};
+
+#[fastout]
+fn main() {
+    input! {
+        a: i64,
+        b: i64,
+        k: i64,
+    }
+
+    if a + k >= b - k {
+        for i in a..=b {
+            println!("{}", i);
+        }
+    } else {
+        for i in a..a + k {
+            println!("{}", i);
+        }
+        for i in b - k + 1..=b {
+            println!("{}", i);
+        }
+    }
+}   
