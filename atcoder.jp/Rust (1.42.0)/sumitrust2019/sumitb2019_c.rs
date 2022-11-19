efa3becce65ef4a6936c5da36b3a458e16ce1854
yu7400ki <yu7400ki@gmail.com>
2022-11-20 03:58:01 +0900

    [AC] sumitrust2019/sumitb2019_c Rust (1.42.0) at 2022-11-20 03:58:01
    {
        "id": 36653484,
        "epoch_second": 1668884281,
        "problem_id": "sumitb2019_c",
        "contest_id": "sumitrust2019",
        "user_id": "yu7400ki",
        "language": "Rust (1.42.0)",
        "point": 300.0,
        "length": 472,
        "result": "AC",
        "execution_time": 215
    }

--- atcoder.jp/Rust (1.42.0)/sumitrust2019/sumitb2019_c.rs
+++ atcoder.jp/Rust (1.42.0)/sumitrust2019/sumitb2019_c.rs
@@ -3,8 +3,24 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        x: i64,
+        x: usize,
     }
 
-    println!("{}", if x % 615 == 0 { "1" } else { "0" });
+    let mut a = vec![false; x + 1];
+    a[0] = true;
+    let prices = [100, 101, 102, 103, 104, 105];
+
+    for p in prices.iter() {
+        for i in 0..x {
+            if !a[i] {
+                continue;
+            }
+
+            for j in 1..(x - i) / p + 1 {
+                a[i + p * j] = true;
+            }
+        }
+    }
+
+    println!("{}", if a[x] { 1 } else { 0 });
 }
