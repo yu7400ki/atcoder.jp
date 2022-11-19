8e6f42f2a8b70115915ae48b3e52ec76c2431d67
yu7400ki <yu7400ki@gmail.com>
2022-11-20 03:14:21 +0900

    [WA] sumitrust2019/sumitb2019_c Rust (1.42.0) at 2022-11-20 03:14:21
    {
        "id": 36653093,
        "epoch_second": 1668881661,
        "problem_id": "sumitb2019_c",
        "contest_id": "sumitrust2019",
        "user_id": "yu7400ki",
        "language": "Rust (1.42.0)",
        "point": 0.0,
        "length": 152,
        "result": "WA",
        "execution_time": 7
    }

--- atcoder.jp/Rust (1.42.0)/sumitrust2019/sumitb2019_c.rs
+++ atcoder.jp/Rust (1.42.0)/sumitrust2019/sumitb2019_c.rs
@@ -6,20 +6,5 @@ fn main() {
         x: i64,
     }
 
-    let price = vec![100, 101, 102, 103, 104, 105];
-
-    for i in 1..2usize.pow(6) {
-        let mut sum = 0;
-        for j in 0..6usize {
-            if i >> j & 1 == 1 {
-                sum += price[j];
-            }
-        }
-        if x % sum == 0 {
-            println!("1");
-            return;
-        }
-    }
-
-    println!("0");
+    println!("{}", if x % 615 == 0 { "1" } else { "0" });
 }
