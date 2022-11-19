56ba575f5e6411985ccb5fae847cb513c1864611
yu7400ki <yu7400ki@gmail.com>
2022-11-20 00:57:02 +0900

    [WA] abc278/abc278_d Rust (1.42.0) at 2022-11-20 00:57:02
    {
        "id": 36651177,
        "epoch_second": 1668873422,
        "problem_id": "abc278_d",
        "contest_id": "abc278",
        "user_id": "yu7400ki",
        "language": "Rust (1.42.0)",
        "point": 0.0,
        "length": 1117,
        "result": "WA",
        "execution_time": 280
    }

---
+++ atcoder.jp/Rust (1.42.0)/abc278/abc278_d.rs
@@ -0,0 +1,51 @@
+use proconio::{fastout, input};
+
+#[fastout]
+fn main() {
+    input! {
+        n: usize,
+        a: [i64; n],
+        q: usize,
+    }
+
+    let mut b: Option<i64> = None;
+    let mut last_reset = 0;
+    let mut add = vec![(last_reset, 0i64); n];
+
+    for iteration in 0..q {
+        input! {
+            query: usize,
+        }
+
+        match query {
+            1 => {
+                input! {
+                    x: i64,
+                }
+                b = Some(x);
+                last_reset = iteration;
+            }
+            2 => {
+                input! {
+                    i: usize,
+                    x: i64,
+                }
+                if add[i - 1].0 < last_reset {
+                    add[i - 1] = (last_reset, x);
+                } else {
+                    add[i - 1].1 += x;
+                }
+            }
+            _ => {
+                input! {
+                    i: usize,
+                }
+                let add = add[i - 1];
+                println!(
+                    "{}",
+                    add.1 + b.unwrap_or(a[i - 1])
+                );
+            }
+        }
+    }
+}
