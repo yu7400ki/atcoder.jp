984204130b0b87cee633b33963de1ab12d2c8711
yu7400ki <yu7400ki@gmail.com>
2022-11-20 18:08:08 +0900

    [WA] abc106/abc106_c Rust (1.42.0) at 2022-11-20 18:08:08
    {
        "id": 36665801,
        "epoch_second": 1668935288,
        "problem_id": "abc106_c",
        "contest_id": "abc106",
        "user_id": "yu7400ki",
        "language": "Rust (1.42.0)",
        "point": 0.0,
        "length": 300,
        "result": "WA",
        "execution_time": 6
    }

---
+++ atcoder.jp/Rust (1.42.0)/abc106/abc106_c.rs
@@ -0,0 +1,15 @@
+use proconio::{fastout, input};
+
+#[fastout]
+fn main() {
+    input! {
+        s: String,
+        k: usize,
+    }
+
+    let first_not_one = s.chars().enumerate().find(|(_, c)| *c != '1');
+    match first_not_one {
+        Some((i, c)) if i <= k => println!("{}", c),
+        _ => println!("1"),
+    }
+}
