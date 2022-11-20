dcd4cea7861060a2bfa18191f50673dfb4a08a09
yu7400ki <yu7400ki@gmail.com>
2022-11-20 18:08:32 +0900

    [AC] abc106/abc106_c Rust (1.42.0) at 2022-11-20 18:08:32
    {
        "id": 36665811,
        "epoch_second": 1668935312,
        "problem_id": "abc106_c",
        "contest_id": "abc106",
        "user_id": "yu7400ki",
        "language": "Rust (1.42.0)",
        "point": 300.0,
        "length": 299,
        "result": "AC",
        "execution_time": 7
    }

--- atcoder.jp/Rust (1.42.0)/abc106/abc106_c.rs
+++ atcoder.jp/Rust (1.42.0)/abc106/abc106_c.rs
@@ -9,7 +9,7 @@ fn main() {
 
     let first_not_one = s.chars().enumerate().find(|(_, c)| *c != '1');
     match first_not_one {
-        Some((i, c)) if i <= k => println!("{}", c),
+        Some((i, c)) if i < k => println!("{}", c),
         _ => println!("1"),
     }
 }
