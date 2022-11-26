2dcd004b23fc53c07bd846f91b4bd484b6dde206
yu7400ki <yu7400ki@gmail.com>
2022-11-26 22:27:19 +0900

    [AC] abc279/abc279_c PyPy3 (7.3.0) at 2022-11-26 22:27:19
    {
        "id": 36824199,
        "epoch_second": 1669469239,
        "problem_id": "abc279_c",
        "contest_id": "abc279",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 300.0,
        "length": 286,
        "result": "AC",
        "execution_time": 1165
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc279/abc279_c.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc279/abc279_c.py
@@ -1,32 +1,17 @@
 H, W = map(int, input().split())
-S = [input() for _ in range(H)]
-T = [input() for _ in range(H)]
+S = [list(input()) for _ in range(H)]
+T = [list(input()) for _ in range(H)]
 
-Sh_cnt = [0] * H
-Th_cnt = [0] * H
-Sv_cnt = [0] * W
-Tv_cnt = [0] * W
+def transpose(A):
+    return list(map(list, zip(*A)))
 
-for i in range(H):
-    for j in range(W):
-        if S[i][j] == "#":
-            Sh_cnt[i] += 1
-        if T[i][j] == "#":
-            Th_cnt[i] += 1
+S = transpose(S)
+T = transpose(T)
 
-for i in range(W):
-    for j in range(H):
-        if S[j][i] == "#":
-            Sv_cnt[i] += 1
-        if T[j][i] == "#":
-            Tv_cnt[i] += 1
+S.sort()
+T.sort()
 
-Sh_cnt.sort()
-Th_cnt.sort()
-Sv_cnt.sort()
-Sv_cnt.sort()
-
-if Sh_cnt == Th_cnt and Sv_cnt == Tv_cnt:
+if S == T:
     print("Yes")
 else:
     print("No")
