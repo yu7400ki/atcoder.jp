71d5af8553f90314bbf2627f46dd7abccccd46dd
yu7400ki <yu7400ki@gmail.com>
2022-12-03 17:47:46 +0900

    [WA] agc034/agc034_a Rust (1.42.0) at 2022-12-03 17:47:46
    {
        "id": 36943573,
        "epoch_second": 1670057266,
        "problem_id": "agc034_a",
        "contest_id": "agc034",
        "user_id": "yu7400ki",
        "language": "Rust (1.42.0)",
        "point": 0.0,
        "length": 756,
        "result": "WA",
        "execution_time": 11
    }

--- atcoder.jp/Rust (1.42.0)/agc034/agc034_a.rs
+++ atcoder.jp/Rust (1.42.0)/agc034/agc034_a.rs
@@ -19,7 +19,7 @@ fn main() {
             return;
         }
     } else {
-        let v = s[a - 1..d].iter().collect::<String>();
+        let v = s[b - 1..d].iter().collect::<String>();
         if v.contains("...") {
             let v = s[a - 1..c].iter().collect::<String>();
             if v.contains("##") {
