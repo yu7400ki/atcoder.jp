40cbed30b1486600eb85682b11ab76c656835de7
yu7400ki <yu7400ki@gmail.com>
2022-12-03 17:50:21 +0900

    [WA] agc034/agc034_a Rust (1.42.0) at 2022-12-03 17:50:21
    {
        "id": 36943636,
        "epoch_second": 1670057421,
        "problem_id": "agc034_a",
        "contest_id": "agc034",
        "user_id": "yu7400ki",
        "language": "Rust (1.42.0)",
        "point": 0.0,
        "length": 744,
        "result": "WA",
        "execution_time": 8
    }

--- atcoder.jp/Rust (1.42.0)/agc034/agc034_a.rs
+++ atcoder.jp/Rust (1.42.0)/agc034/agc034_a.rs
@@ -13,15 +13,15 @@ fn main() {
     let s = s.chars().collect::<Vec<_>>();
 
     if c < d {
-        let v = s[a - 1..d].iter().collect::<String>();
+        let v = s[a..d].iter().collect::<String>();
         if v.contains("##") {
             println!("No");
             return;
         }
     } else {
-        let v = s[b - 1..d].iter().collect::<String>();
+        let v = s[b..d].iter().collect::<String>();
         if v.contains("...") {
-            let v = s[a - 1..c].iter().collect::<String>();
+            let v = s[a..c].iter().collect::<String>();
             if v.contains("##") {
                 println!("No");
                 return;
