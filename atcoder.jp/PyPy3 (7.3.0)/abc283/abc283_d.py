3f0c8a569c6ae8c33ff943bff1f07187e19ddbae
yu7400ki <yu7400ki@gmail.com>
2022-12-24 22:19:42 +0900

    [WA] abc283/abc283_d PyPy3 (7.3.0) at 2022-12-24 22:19:42
    {
        "id": 37517048,
        "epoch_second": 1671887982,
        "problem_id": "abc283_d",
        "contest_id": "abc283",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 848,
        "result": "WA",
        "execution_time": 123
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc283/abc283_d.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc283/abc283_d.py
@@ -1,31 +1,19 @@
 S = input()
 N = len(S)
 
-def binary_search(lst, target, ok = -1, ng = None, key = None):
-    ng = len(lst) if ng is None else ng
-    key = (lambda x: lst[x] <= target) if key is None else key
 
-    while abs(ok - ng) > 1:
-        mid = (ok + ng) // 2
-        if key(mid):
-            ok = mid
-        else:
-            ng = mid
-
-    return ok
+pair = {}
+temp = []
+for i in range(N):
+    if S[i] == "(":
+        temp.append(i)
+    elif S[i] == ")":
+        pair[temp.pop()] = i
+pair = {v: k for k, v in pair.items()}
 
 
 box = set()
 box_accum = [set()]
-
-open_accum = [0]
-for i in range(N):
-    if S[i] == "(":
-        open_accum.append(open_accum[-1] + 1)
-    elif S[i] == ")":
-        open_accum.append(open_accum[-1])
-    else:
-        open_accum.append(open_accum[-1])
 
 close_cnt = 0
 
@@ -34,13 +22,11 @@ for i in range(N):
         box_copy = box_accum[-1].copy()
         box_accum.append(box_copy)
     elif S[i] == ")":
-        close_cnt += 1
+        idx = pair[i]
         box_copy = box_accum[-1].copy()
         box_accum.append(box_copy)
 
-        ok = binary_search(open_accum, open_accum[i + 1] - close_cnt)
-
-        delete_box = box_accum[i] - box_accum[ok]
+        delete_box = box_accum[i] - box_accum[idx + 1]
         box -= delete_box
     else:
         if S[i] in box:
