7d3ba660ec1b44fbe9897cd2d51ba94c6323dfaa
yu7400ki <yu7400ki@gmail.com>
2022-12-24 22:23:34 +0900

    [TLE] abc283/abc283_d PyPy3 (7.3.0) at 2022-12-24 22:23:34
    {
        "id": 37517814,
        "epoch_second": 1671888214,
        "problem_id": "abc283_d",
        "contest_id": "abc283",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 506,
        "result": "TLE",
        "execution_time": 2211
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc283/abc283_d.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc283/abc283_d.py
@@ -1,4 +1,4 @@
-S = input()
+S = list(input())
 N = len(S)
 
 
@@ -13,29 +13,19 @@ pair = {v: k for k, v in pair.items()}
 
 
 box = set()
-box_accum = [set()]
-
-close_cnt = 0
 
 for i in range(N):
     if S[i] == "(":
-        box_copy = box_accum[-1].copy()
-        box_accum.append(box_copy)
+        pass
     elif S[i] == ")":
         idx = pair[i]
-        box_copy = box_accum[-1].copy()
-        box_accum.append(box_copy)
 
-        delete_box = box_accum[i] - box_accum[idx + 1]
-        box -= delete_box
+        box -= set(S[idx:i])
     else:
         if S[i] in box:
             print("No")
             exit()
         else:
             box.add(S[i])
-            box_copy = box_accum[-1].copy()
-            box_copy.add(S[i])
-            box_accum.append(box_copy)
 
 print("Yes")
