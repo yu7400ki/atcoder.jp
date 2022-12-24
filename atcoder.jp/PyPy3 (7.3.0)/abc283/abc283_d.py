71b7db92df8e64dae9a5591038abf6017102b5de
yu7400ki <yu7400ki@gmail.com>
2022-12-24 22:43:20 +0900

    [AC] abc283/abc283_d PyPy3 (7.3.0) at 2022-12-24 22:43:20
    {
        "id": 37522144,
        "epoch_second": 1671889400,
        "problem_id": "abc283_d",
        "contest_id": "abc283",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 400.0,
        "length": 286,
        "result": "AC",
        "execution_time": 112
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc283/abc283_d.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc283/abc283_d.py
@@ -1,41 +1,13 @@
 S = list(input())
 N = len(S)
 
-
-pair = {}
-temp = []
-for i in range(N):
-    if S[i] == "(":
-        temp.append(i)
-    elif S[i] == ")":
-        pair[temp.pop()] = i
-pair = {v: k for k, v in pair.items()}
-
-
-box_accum = [set()]
-for i in range(N):
-    if S[i] == "(":
-        copy = box_accum[-1].copy()
-        box_accum.append(copy)
-    elif S[i] == ")":
-        copy = box_accum[-1].copy()
-        box_accum.append(copy)
-    else:
-        copy = box_accum[-1].copy()
-        copy.add(S[i])
-        box_accum.append(copy)
-
-
 box = set()
 
 for i in range(N):
     if S[i] == "(":
         pass
     elif S[i] == ")":
-        idx = pair[i]
-
-        delete = box_accum[i] - box_accum[idx + 1]
-        box -= delete
+        box.clear()
     else:
         if S[i] in box:
             print("No")
