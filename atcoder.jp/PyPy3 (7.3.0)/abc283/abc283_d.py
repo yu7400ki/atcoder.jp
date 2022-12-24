fd5777feaf98aef60783c0e0ab1c9110c1e75232
yu7400ki <yu7400ki@gmail.com>
2022-12-24 21:38:36 +0900

    [RE] abc283/abc283_d PyPy3 (7.3.0) at 2022-12-24 21:38:36
    {
        "id": 37506852,
        "epoch_second": 1671885516,
        "problem_id": "abc283_d",
        "contest_id": "abc283",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 1319,
        "result": "RE",
        "execution_time": 119
    }

---
+++ atcoder.jp/PyPy3 (7.3.0)/abc283/abc283_d.py
@@ -0,0 +1,57 @@
+S = input()
+N = len(S)
+
+def binary_search(lst, target, ok = -1, ng = None, key = None):
+    ng = len(lst) if ng is None else ng
+    key = (lambda x: lst[x] <= target) if key is None else key
+
+    while abs(ok - ng) > 1:
+        mid = (ok + ng) // 2
+        if key(mid):
+            ok = mid
+        else:
+            ng = mid
+
+    return ok
+
+
+box = set()
+box_accum = [set()]
+
+open_accum = [0]
+for i in range(N-1, -1, -1):
+    if S[i] == "(":
+        open_accum.append(open_accum[-1] + 1)
+    elif S[i] == ")":
+        open_accum.append(open_accum[-1])
+    else:
+        open_accum.append(open_accum[-1])
+
+close_cnt = 0
+
+for i in range(N):
+    if S[i] == "(":
+        box_copy = box_accum[-1].copy()
+        box_accum.append(box_copy)
+        pass
+    elif S[i] == ")":
+        close_cnt += 1
+        box_copy = box_accum[-1].copy()
+        box_accum.append(box_copy)
+
+        ok = binary_search(open_accum, close_cnt)
+        idx = N - ok
+
+        delete_box = box_accum[i] - box_accum[idx]
+        box -= delete_box
+    else:
+        if S[i] in box:
+            print("No")
+            exit()
+        else:
+            box.add(S[i])
+            box_copy = box_accum[-1].copy()
+            box_copy.add(S[i])
+            box_accum.append(box_copy)
+
+print("Yes")
