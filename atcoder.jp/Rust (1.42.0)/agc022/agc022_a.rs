fa16958a9b3a672abe5629ddbb71744ffad445f7
yu7400ki <yu7400ki@gmail.com>
2022-12-25 23:49:23 +0900

    [AC] agc022/agc022_a Rust (1.42.0) at 2022-12-25 23:49:23
    {
        "id": 37548957,
        "epoch_second": 1671979763,
        "problem_id": "agc022_a",
        "contest_id": "agc022",
        "user_id": "yu7400ki",
        "language": "Rust (1.42.0)",
        "point": 300.0,
        "length": 1180,
        "result": "AC",
        "execution_time": 3
    }

--- atcoder.jp/Rust (1.42.0)/agc022/agc022_a.rs
+++ atcoder.jp/Rust (1.42.0)/agc022/agc022_a.rs
@@ -13,25 +13,33 @@ fn main() {
     }
 
     let set = s.chars().collect::<BTreeSet<char>>();
-    let alp = "abcdefghijklmnopqrstuvwxyz".chars().collect::<BTreeSet<char>>();
+    let alp = "abcdefghijklmnopqrstuvwxyz"
+        .chars()
+        .collect::<BTreeSet<char>>();
     let rest = alp.difference(&set).collect::<Vec<&char>>();
 
     if rest.is_empty() {
         for i in (0..s.len()).rev() {
-            let set = s[0..i].chars().collect::<BTreeSet<char>>();
-            let c = s.chars().nth(i).unwrap();
-            if c == 'z' {
+            if s.chars().nth(i - 1).unwrap() > s.chars().nth(i).unwrap() {
                 continue;
             }
-            let next = (c as u8 + 1) as char;
-            if set.contains(&next) {
-                continue;
-            }
-            let ans = s[0..i].to_string() + &next.to_string();
+
+            let set = s[i..].chars().collect::<BTreeSet<char>>();
+            let mi = {
+                let mut res = ' ';
+                for c in set.into_iter() {
+                    if c > s.chars().nth(i - 1).unwrap() {
+                        res = c;
+                        break;
+                    }
+                }
+                res
+            };
+
+            let ans = s[..i - 1].to_string() + &mi.to_string();
             println!("{}", ans);
             return;
         }
-
     } else {
         s.push(*rest[0]);
         println!("{}", s);
