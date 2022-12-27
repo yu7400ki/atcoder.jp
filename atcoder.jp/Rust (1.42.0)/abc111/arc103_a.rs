253533d91d26476a51b5b053a2c3bc63e156586e
yu7400ki <yu7400ki@gmail.com>
2022-12-27 22:58:28 +0900

    [WA] abc111/arc103_a Rust (1.42.0) at 2022-12-27 22:58:28
    {
        "id": 37585132,
        "epoch_second": 1672149508,
        "problem_id": "arc103_a",
        "contest_id": "abc111",
        "user_id": "yu7400ki",
        "language": "Rust (1.42.0)",
        "point": 0.0,
        "length": 1707,
        "result": "WA",
        "execution_time": 47
    }

--- atcoder.jp/Rust (1.42.0)/abc111/arc103_a.rs
+++ atcoder.jp/Rust (1.42.0)/abc111/arc103_a.rs
@@ -1,11 +1,11 @@
 use proconio::{fastout, input};
 
-fn counter<T>(v: &[T]) -> std::collections::HashMap<T, usize>
+fn counter<T>(v: &[T]) -> std::collections::BTreeMap<T, usize>
 where
-    T: std::cmp::Eq + std::hash::Hash + Copy,
+    T: std::cmp::Eq + std::hash::Hash + Copy + Ord,
 {
     v.iter()
-        .fold(std::collections::HashMap::new(), |mut map, &x| {
+        .fold(std::collections::BTreeMap::new(), |mut map, &x| {
             *map.entry(x).or_insert(0) += 1;
             map
         })
@@ -28,13 +28,34 @@ fn main() {
     let a: Vec<_> = a.into_iter().map(|(_, v)| v).collect();
     let b: Vec<_> = b.into_iter().map(|(_, v)| v).collect();
 
-    let a_cnt = counter(&a);
-    let b_cnt = counter(&b);
+    let mut a_cnt = counter(&a).into_iter().collect::<Vec<_>>();
+    let mut b_cnt = counter(&b).into_iter().collect::<Vec<_>>();
 
     let ans = {
-        let a_ma = a_cnt.iter().max_by_key(|(_, v)| *v).unwrap();
-        let b_ma = b_cnt.iter().max_by_key(|(_, v)| *v).unwrap();
-        n / 2 - a_ma.1 + n / 2 - b_ma.1
+        let a_ma = a_cnt.pop().unwrap();
+        let b_ma = b_cnt.pop().unwrap();
+
+        if a_ma.0 == b_ma.0 {
+            let next_a = a_cnt.pop();
+            let next_b = b_cnt.pop();
+
+            match (next_a, next_b) {
+                (Some(next_a), Some(next_b)) => {
+                    let a = n / 2 - a_ma.1 + n / 2 - next_b.1;
+                    let b = n / 2 - b_ma.1 + n / 2 - next_a.1;
+                    std::cmp::min(a, b)
+                }
+                (Some(next_a), None) => {
+                    n / 2 - b_ma.1 + n / 2 - next_a.1
+                }
+                (None, Some(next_b)) => {
+                    n / 2 - a_ma.1 + n / 2 - next_b.1
+                }
+                _ => unreachable!(),
+            }
+        } else {
+            n / 2 - a_ma.1 + n / 2 - b_ma.1
+        }
     };
 
     println!("{}", ans);
