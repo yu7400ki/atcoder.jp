df05571e298519a2d6913f3ac9ff35c115d0466c
yu7400ki <yu7400ki@gmail.com>
2023-01-07 22:09:44 +0900

    [RE] abc284/abc284_e Python (3.8.2) at 2023-01-07 22:09:44
    {
        "id": 37836561,
        "epoch_second": 1673096984,
        "problem_id": "abc284_e",
        "contest_id": "abc284",
        "user_id": "yu7400ki",
        "language": "Python (3.8.2)",
        "point": 0.0,
        "length": 523,
        "result": "RE",
        "execution_time": 857
    }

---
+++ atcoder.jp/Python (3.8.2)/abc284/abc284_e.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+N, M = map(int, input().split())
+
+graph = defaultdict(set)
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u].add(v)
+    graph[v].add(u)
+
+ans = 1
+limit = 10 ** 6
+
+def dfs(graph, n, visited):
+    global ans
+    visited.add(n)
+    for v in graph[n]:
+        if v not in visited:
+            dfs(graph, v, visited)
+            ans += 1
+            if ans >= limit:
+                return
+    visited.remove(n)
+
+dfs(graph, 1, set())
+
+print(ans)
