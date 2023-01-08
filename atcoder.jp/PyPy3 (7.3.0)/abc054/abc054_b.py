48ab912914117f6d0e6afd4f5d5c51e437355e6c
yu7400ki <yu7400ki@gmail.com>
2023-01-08 17:07:18 +0900

    [AC] abc054/abc054_b PyPy3 (7.3.0) at 2023-01-08 17:07:18
    {
        "id": 37873544,
        "epoch_second": 1673165238,
        "problem_id": "abc054_b",
        "contest_id": "abc054",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 200.0,
        "length": 416,
        "result": "AC",
        "execution_time": 73
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc054/abc054_b.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc054/abc054_b.py
@@ -4,12 +4,13 @@ B = [list(input()) for _ in range(M)]
 
 for i in range(N - M + 1):
     for j in range(N - M + 1):
+        flag = True
         for y in range(M):
             for x in range(M):
                 if A[i + y][j + x] != B[y][x]:
-                    break
-            else:
-                print('Yes')
-                exit()
+                    flag = False
+        if flag:
+            print('Yes')
+            exit()
 
 print('No')
