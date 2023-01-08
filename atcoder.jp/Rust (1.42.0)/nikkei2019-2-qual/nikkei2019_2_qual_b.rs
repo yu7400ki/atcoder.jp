df0e6d5d5ccea667c95bf1f167235a845190e15b
yu7400ki <yu7400ki@gmail.com>
2023-01-09 03:14:02 +0900

    [WA] nikkei2019-2-qual/nikkei2019_2_qual_b Rust (1.42.0) at 2023-01-09 03:14:02
    {
        "id": 37890011,
        "epoch_second": 1673201642,
        "problem_id": "nikkei2019_2_qual_b",
        "contest_id": "nikkei2019-2-qual",
        "user_id": "yu7400ki",
        "language": "Rust (1.42.0)",
        "point": 0.0,
        "length": 962,
        "result": "WA",
        "execution_time": 32
    }

---
+++ atcoder.jp/Rust (1.42.0)/nikkei2019-2-qual/nikkei2019_2_qual_b.rs
@@ -0,0 +1,50 @@
+use proconio::{fastout, input};
+
+const MOD: usize = 1_000_000_007;
+
+#[fastout]
+fn main() {
+    input! {
+        n: usize,
+        d: [usize; n],
+    }
+
+    let d_count = counter(&d);
+
+    if d[0] != 0 {
+        println!("0");
+        return;
+    }
+
+    if d_count.get(&0).unwrap() != &1 {
+        println!("0");
+        return;
+    }
+
+    let max_height = d_count.len() - 1;
+    if *d.iter().max().unwrap() != max_height {
+        println!("0");
+        return;
+    }
+
+    let ans = (0..max_height).fold(1, |acc, i| {
+        acc * d_count
+            .get(&i)
+            .unwrap()
+            .pow(*d_count.get(&(i + 1)).unwrap() as u32)
+            % MOD
+    });
+
+    println!("{}", ans);
+}
+
+fn counter<T>(v: &[T]) -> std::collections::HashMap<T, usize>
+where
+    T: std::cmp::Eq + std::hash::Hash + Copy,
+{
+    v.iter()
+        .fold(std::collections::HashMap::new(), |mut map, &x| {
+            *map.entry(x).or_insert(0) += 1;
+            map
+        })
+}
