3d3397aa977c4724c5590fef20a3c1fb95985a01
yu7400ki <yu7400ki@gmail.com>
2023-01-10 14:14:37 +0900

    [AC] abc110/abc110_c PyPy3 (7.3.0) at 2023-01-10 14:14:37
    {
        "id": 37918140,
        "epoch_second": 1673327677,
        "problem_id": "abc110_c",
        "contest_id": "abc110",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 300.0,
        "length": 351,
        "result": "AC",
        "execution_time": 100
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc110/abc110_c.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc110/abc110_c.py
@@ -3,11 +3,13 @@ from collections import defaultdict
 S = list(input())
 T = list(input())
 
-graph = defaultdict(set)
+graph1 = defaultdict(set)
+graph2 = defaultdict(set)
 for i in range(len(S)):
-    graph[T[i]].add(S[i])
+    graph1[T[i]].add(S[i])
+    graph2[S[i]].add(T[i])
 
-if all(len(v) <= 1 for v in graph.values()):
+if all(len(v) <= 1 for v in graph1.values()) and all(len(v) <= 1 for v in graph2.values()):
     print("Yes")
 else:
     print("No")
