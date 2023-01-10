91912663b566bb9b59a3a3c0176e1cdbe82a32b7
yu7400ki <yu7400ki@gmail.com>
2023-01-10 14:10:32 +0900

    [WA] abc110/abc110_c PyPy3 (7.3.0) at 2023-01-10 14:10:32
    {
        "id": 37918076,
        "epoch_second": 1673327432,
        "problem_id": "abc110_c",
        "contest_id": "abc110",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 247,
        "result": "WA",
        "execution_time": 87
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc110/abc110_c.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc110/abc110_c.py
@@ -7,7 +7,7 @@ graph = defaultdict(set)
 for i in range(len(S)):
     graph[T[i]].add(S[i])
 
-if all(len(v) == 1 for v in graph.values()):
+if all(len(v) <= 1 for v in graph.values()):
     print("Yes")
 else:
     print("No")
