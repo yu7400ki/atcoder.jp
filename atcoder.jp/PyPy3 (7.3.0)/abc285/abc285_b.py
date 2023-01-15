0d42eba727b173fd33533394605f29f505f77187
yu7400ki <yu7400ki@gmail.com>
2023-01-15 23:04:19 +0900

    [AC] abc285/abc285_b PyPy3 (7.3.0) at 2023-01-15 23:04:19
    {
        "id": 38078430,
        "epoch_second": 1673791459,
        "problem_id": "abc285_b",
        "contest_id": "abc285",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 200.0,
        "length": 160,
        "result": "AC",
        "execution_time": 121
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc285/abc285_b.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc285/abc285_b.py
@@ -3,9 +3,6 @@ S = list(input())
 
 for i in range(1, N):
     ans = 0
-    for l in range(1, N - i + 1):
-        k = 0
-        while k < l and S[k] != S[k + i]:
-            k += 1
-        ans = max(ans, k)
+    while ans < (N - i) and S[ans] != S[ans + i]:
+        ans += 1
     print(ans)
