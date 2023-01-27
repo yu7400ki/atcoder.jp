522f9fcac36c286dd2f1d414fe285fca71ea0d03
yu7400ki <yu7400ki@gmail.com>
2023-01-27 20:45:49 +0900

    [AC] abc134/abc134_d PyPy3 (7.3.0) at 2023-01-27 20:45:49
    {
        "id": 38355142,
        "epoch_second": 1674819949,
        "problem_id": "abc134_d",
        "contest_id": "abc134",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 400.0,
        "length": 209,
        "result": "AC",
        "execution_time": 141
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc134/abc134_d.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc134/abc134_d.py
@@ -4,7 +4,7 @@ A = list(map(int, input().split()))
 b = [0] * N
 
 for i in range(N - 1, -1 , -1):
-    b[i] = sum(A[i::i+1]) % 2
+    b[i] = (sum(b[i::i+1]) + A[i]) % 2
 
 print(b.count(1))
 print(*[i + 1 for i in range(N) if b[i]])
