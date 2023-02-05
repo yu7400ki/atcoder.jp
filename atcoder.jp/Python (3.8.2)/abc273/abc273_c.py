a930924a8f860d9215156b3f73f9bcac5b0c358a
yu7400ki <yu7400ki@gmail.com>
2023-02-06 00:33:29 +0900

    [AC] abc273/abc273_c Python (3.8.2) at 2023-02-06 00:33:29
    {
        "id": 38652750,
        "epoch_second": 1675611209,
        "problem_id": "abc273_c",
        "contest_id": "abc273",
        "user_id": "yu7400ki",
        "language": "Python (3.8.2)",
        "point": 300.0,
        "length": 545,
        "result": "AC",
        "execution_time": 333
    }

--- atcoder.jp/Python (3.8.2)/abc273/abc273_c.py
+++ atcoder.jp/Python (3.8.2)/abc273/abc273_c.py
@@ -1,3 +1,11 @@
+import io, os
+buf = io.BytesIO()
+def print(*args, sep=" ", end="\n"):
+    for arg in args:
+        buf.write(str(arg).encode())
+        buf.write(sep.encode())
+    buf.write(end.encode())
+
 def solve():
     from collections import Counter
 
@@ -6,9 +14,10 @@ def solve():
 
     count = Counter(A)
     for i in sorted(list(set(A)), reverse=True):
-        print(count[i], flush=True)
+        print(count[i])
     for _ in range(N - len(count)):
-        print(0, flush=True)
+        print(0)
 
 if __name__ == "__main__":
     solve()
+    os.write(1, buf.getvalue())
