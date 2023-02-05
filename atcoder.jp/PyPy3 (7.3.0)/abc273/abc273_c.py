76d7aed7d468bf4bda1681efe600001ab30c87b4
yu7400ki <yu7400ki@gmail.com>
2023-02-06 00:35:52 +0900

    [AC] abc273/abc273_c PyPy3 (7.3.0) at 2023-02-06 00:35:52
    {
        "id": 38652794,
        "epoch_second": 1675611352,
        "problem_id": "abc273_c",
        "contest_id": "abc273",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 300.0,
        "length": 315,
        "result": "AC",
        "execution_time": 329
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc273/abc273_c.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc273/abc273_c.py
@@ -1,10 +1,14 @@
-from collections import Counter
+def solve():
+    from collections import Counter
 
-N = int(input())
-A = list(map(int, input().split()))
+    N = int(input())
+    A = list(map(int, input().split()))
 
-count = Counter(A)
-for i in sorted(list(set(A)), reverse=True):
-    print(count[i])
-for _ in range(N - len(count)):
-    print(0)
+    count = Counter(A)
+    for i in sorted(list(set(A)), reverse=True):
+        print(count[i])
+    for _ in range(N - len(count)):
+        print(0)
+
+if __name__ == "__main__":
+    solve()
