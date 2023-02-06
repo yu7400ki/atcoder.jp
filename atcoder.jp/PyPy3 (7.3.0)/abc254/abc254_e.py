52022c2d0a4534704e1d31984c4949c800d29bad
yu7400ki <yu7400ki@gmail.com>
2023-02-06 18:04:30 +0900

    [AC] abc254/abc254_e PyPy3 (7.3.0) at 2023-02-06 18:04:30
    {
        "id": 38667294,
        "epoch_second": 1675674270,
        "problem_id": "abc254_e",
        "contest_id": "abc254",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 500.0,
        "length": 898,
        "result": "AC",
        "execution_time": 1618
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc254/abc254_e.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc254/abc254_e.py
@@ -1,32 +1,37 @@
 from collections import defaultdict, deque
 
-N, M = map(int,input().split())
-graph = defaultdict(list)
+def bfs(graph, n, limit):
+    dic = defaultdict(lambda : -1)
+    queue = deque()
+    queue.append(n)
+    dic[n] = 0
+    ans = n
+    while len(queue) != 0:
+        pos = queue.popleft()
+        for i in graph[pos]:
+            if dic[i] == -1:
+                dic[i] = dic[pos] + 1
+                if dic[i] <= limit:
+                    ans += i
+                    if dic[i] < limit:
+                        queue.append(i)
+    return ans
 
-for _ in range(M):
-	a,b = map(int,input().split())
-	graph[a].append(b)
-	graph[b].append(a)
+def solve():
+    N, M = map(int, input().split())
+    graph = defaultdict(list)
 
-def bfs(n,limit):
-	dic = defaultdict(lambda : -1)
-	queue = deque()
-	queue.append(n)
-	dic[n] = 0
-	ans = n
-	while len(queue) != 0:
-		pos = queue.popleft()
-		for i in graph[pos]:
-			if dic[i] == -1:
-				dic[i] = dic[pos] + 1
-				if dic[i] <= limit:
-					ans += i
-					if dic[i] < limit:
-						queue.append(i)
-	return ans
+    for _ in range(M):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
 
-Q = int(input())
-for _ in range(Q):
-	x,k = map(int,input().split())
-	ans = bfs(x,k)
-	print(ans)
+    Q = int(input())
+
+    for _ in range(Q):
+        x, k = map(int, input().split())
+        ans = bfs(graph, x, k)
+        print(ans)
+
+if __name__ == "__main__":
+    solve()
