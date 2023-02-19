80a6a06f51855382a8df05bcef6b51b066bd6259
yu7400ki <yu7400ki@gmail.com>
2023-02-19 17:39:01 +0900

    [RE] diverta2019-2/diverta2019_2_b Python (3.8.2) at 2023-02-19 17:39:01
    {
        "id": 39004254,
        "epoch_second": 1676795941,
        "problem_id": "diverta2019_2_b",
        "contest_id": "diverta2019-2",
        "user_id": "yu7400ki",
        "language": "Python (3.8.2)",
        "point": 0.0,
        "length": 346,
        "result": "RE",
        "execution_time": 26
    }

--- atcoder.jp/Python (3.8.2)/diverta2019-2/diverta2019_2_b.py
+++ atcoder.jp/Python (3.8.2)/diverta2019-2/diverta2019_2_b.py
@@ -1,29 +1,17 @@
+from collections import Counter
 from itertools import permutations
 
 N = int(input())
 XY = [tuple(map(int, input().split())) for _ in range(N)]
 
-dists = set()
+dists = []
 for p in permutations(XY, 2):
     x1, y1 = p[0]
     x2, y2 = p[1]
     d = x2 - x1, y2 - y1
-    dists.add(d)
+    dists.append(d)
 
-ans = 1 << 60
-for dx, dy in dists:
-    tmp = 1
-    current = XY[0]
-    rest = set(XY[1:])
-    while rest:
-        x1, y1 = current
-        x2, y2 = x1 + dx, y1 + dy
-        if (x2, y2) in rest:
-            rest.remove((x2, y2))
-            current = x2, y2
-        else:
-            tmp += 1
-            current = rest.pop()
-    ans = min(ans, tmp)
+c = Counter(dists)
+ma = c.most_common(1)[0][1]
 
-print(ans)
+print(N - ma)
