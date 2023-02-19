6f3ad25965567ffbac2f5d28c271c20c6d4dcbf4
yu7400ki <yu7400ki@gmail.com>
2023-02-19 21:38:34 +0900

    [RE] abc290/abc290_c Python (3.8.2) at 2023-02-19 21:38:34
    {
        "id": 39031570,
        "epoch_second": 1676810314,
        "problem_id": "abc290_c",
        "contest_id": "abc290",
        "user_id": "yu7400ki",
        "language": "Python (3.8.2)",
        "point": 0.0,
        "length": 725,
        "result": "RE",
        "execution_time": 339
    }

--- atcoder.jp/Python (3.8.2)/abc290/abc290_c.py
+++ atcoder.jp/Python (3.8.2)/abc290/abc290_c.py
@@ -1,10 +1,40 @@
+from collections import deque
+
 N, K = map(int, input().split())
-A = set(map(int, input().split()))
+A = list(map(int, input().split()))
 
-A = sorted(A)
+lst = deque(sorted(A))
 
-if len(A) < K or A[K - 1] != K - 1:
+if lst[0] != 0:
     print(0)
     exit()
+
+res = [lst.popleft()]
+rest = []
+
+while True:
+    a = lst.popleft()
+    if a == res[-1]:
+        rest.append(a)
+    elif a == res[-1] + 1:
+        res.append(a)
+    else:
+        res.append(a)
+        while len(res) != K:
+            if rest:
+                res.append(rest.pop())
+            else:
+                res.append(lst.pop())
+    if len(res) == K:
+        break
+
+for i in range(K - 1):
+    if res[i] + 1 != res[i + 1]:
+        break
 else:
-    print(K)
+    i += 1
+
+if i + 1 not in res:
+    print(i + 1)
+else:
+    print(0)
