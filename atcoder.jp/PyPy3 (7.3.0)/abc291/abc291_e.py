5c74746fdcc4029f26ac39ddec398d05825fd0c1
yu7400ki <yu7400ki@gmail.com>
2023-02-26 22:13:44 +0900

    [AC] abc291/abc291_e PyPy3 (7.3.0) at 2023-02-26 22:13:44
    {
        "id": 39260144,
        "epoch_second": 1677417224,
        "problem_id": "abc291_e",
        "contest_id": "abc291",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 500.0,
        "length": 1126,
        "result": "AC",
        "execution_time": 717
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc291/abc291_e.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc291/abc291_e.py
@@ -4,46 +4,46 @@ N, M = map(int, input().split())
 
 graph = defaultdict(set)
 indegree = defaultdict(int)
-history = set()
 
 for _ in range(M):
     X, Y = map(int, input().split())
     X -= 1
     Y -= 1
     graph[X].add(Y)
-    if (X, Y) in history:
-        continue
-    indegree[Y] += 1
-    if X not in indegree:
-        indegree[X] = 0
-    history.add((X, Y))
 
-res = [0] * N
-i = 1
-stack = []
+def topological_sort(graph, res):
+    indegree = defaultdict(int)
+    for k, v in graph.items():
+        for w in v:
+            indegree[w] += 1
+        if k not in indegree:
+            indegree[k] = 0
+    stack = []
+    for k, v in indegree.items():
+        if v == 0:
+            stack.append(k)
+    if not stack or len(stack) > 1:
+        return False
+    while stack:
+        v = stack.pop()
+        res.append(v)
+        for w in graph[v]:
+            indegree[w] -= 1
+            if indegree[w] == 0:
+                stack.append(w)
+        if len(stack) > 1:
+            return False
+    return True
 
-for k, v in indegree.items():
-    if v == 0:
-        stack.append(k)
-
-if len(stack) == 0 or len(stack) > 1:
+res = []
+is_ok = topological_sort(graph, res)
+if not is_ok:
     print("No")
-    exit()
-
-while stack:
-    v = stack.pop()
-    res[v] = i
-    i += 1
-    for w in graph[v]:
-        indegree[w] -= 1
-        if indegree[w] == 0:
-            stack.append(w)
-    if len(stack) > 1:
-        print("No")
-        exit()
-
-if 0 in res:
+elif len(res) != N:
     print("No")
 else:
+    ans = [0] * N
+    for i, v in enumerate(res):
+        ans[v] = i + 1
     print("Yes")
-    print(" ".join(map(str, res)))
+    print(" ".join(map(str, ans)))
