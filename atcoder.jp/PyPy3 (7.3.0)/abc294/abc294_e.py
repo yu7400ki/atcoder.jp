f2b7b34f9bfce62a89b8068ce025f16839f55f2b
yu7400ki <yu7400ki@gmail.com>
2023-03-19 21:44:49 +0900

    [AC] abc294/abc294_e PyPy3 (7.3.0) at 2023-03-19 21:44:49
    {
        "id": 39866758,
        "epoch_second": 1679229889,
        "problem_id": "abc294_e",
        "contest_id": "abc294",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 500.0,
        "length": 677,
        "result": "AC",
        "execution_time": 546
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc294/abc294_e.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc294/abc294_e.py
@@ -6,42 +6,31 @@ V_1 = deque([None] * N_1)
 V_2 = deque([None] * N_2)
 
 for i in range(N_1):
-    V_1[i] = tuple(map(int, input().split()))
+    V_1[i] = list(map(int, input().split()))
 
 for i in range(N_2):
-    V_2[i] = tuple(map(int, input().split()))
+    V_2[i] = list(map(int, input().split()))
 
 ans = 0
 
-while True:
+while V_1 or V_2:
     a = V_1[0]
     b = V_2[0]
 
     if a[0] == b[0]:
         if a[1] > b[1]:
             ans += b[1]
-            V_1[0] = (a[0], a[1] - b[1])
-            V_2.popleft()
-        elif a[1] < b[1]:
-            ans += a[1]
-            V_1.popleft()
-            V_2[0] = (b[0], b[1] - a[1])
         else:
             ans += a[1]
-            V_1.popleft()
-            V_2.popleft()
-    else:
-        if a[1] > b[1]:
-            V_1[0] = (a[0], a[1] - b[1])
-            V_2.popleft()
-        elif a[1] < b[1]:
-            V_1.popleft()
-            V_2[0] = (b[0], b[1] - a[1])
-        else:
-            V_1.popleft()
-            V_2.popleft()
 
-    if not V_1 or not V_2:
-        break
+    if a[1] > b[1]:
+        V_1[0][1] = a[1] - b[1]
+        V_2.popleft()
+    elif a[1] < b[1]:
+        V_1.popleft()
+        V_2[0][1] = b[1] - a[1]
+    else:
+        V_1.popleft()
+        V_2.popleft()
 
 print(ans)
