abf4cd7f26ee31434dec21a28175047144ac1eca
yu7400ki <yu7400ki@gmail.com>
2023-04-09 21:09:36 +0900

    [AC] abc297/abc297_c Python (3.8.2) at 2023-04-09 21:09:36
    {
        "id": 40460495,
        "epoch_second": 1681042176,
        "problem_id": "abc297_c",
        "contest_id": "abc297",
        "user_id": "yu7400ki",
        "language": "Python (3.8.2)",
        "point": 300.0,
        "length": 280,
        "result": "AC",
        "execution_time": 24
    }

---
+++ atcoder.jp/Python (3.8.2)/abc297/abc297_c.py
@@ -0,0 +1,11 @@
+H, W = map(int, input().split())
+S = [list(input()) for _ in range(H)]
+
+for i in range(H):
+    for j in range(W - 1):
+        if S[i][j] == "T" and S[i][j + 1] == "T":
+            S[i][j] = "P"
+            S[i][j + 1] = "C"
+
+for i in range(H):
+    print("".join(S[i]))
