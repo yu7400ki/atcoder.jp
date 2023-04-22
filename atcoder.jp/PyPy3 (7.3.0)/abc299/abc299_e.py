3853e60d48b1c5c6f65ae79e200ae98d23894768
yu7400ki <yu7400ki@gmail.com>
2023-04-22 22:29:35 +0900

    [WA] abc299/abc299_e PyPy3 (7.3.0) at 2023-04-22 22:29:35
    {
        "id": 40875694,
        "epoch_second": 1682170175,
        "problem_id": "abc299_e",
        "contest_id": "abc299",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 1467,
        "result": "WA",
        "execution_time": 280
    }

---
+++ atcoder.jp/PyPy3 (7.3.0)/abc299/abc299_e.py
@@ -0,0 +1,76 @@
+from collections import defaultdict, deque
+
+def bfs(graph, n, d):
+    depth = defaultdict(lambda : -1)
+    depth[n] = 0
+
+    if d == 0:
+        return depth
+
+    queue = deque()
+    queue.append(n)
+
+    while queue:
+        u = queue.popleft()
+        for v in graph[u]:
+            if depth[v] == -1:
+                depth[v] = depth[u] + 1
+                if depth[v] != d:
+                    queue.append(v)
+
+    return depth
+
+N, M = map(int, input().split())
+
+if N == 1:
+    print("Yes")
+    print(1)
+    exit()
+
+graph = defaultdict(set)
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u].add(v)
+    graph[v].add(u)
+
+K = int(input())
+
+if K == 0:
+    print("Yes")
+    print("".join(["0"] * N))
+    exit()
+
+depths = {}
+can_paint = set()
+not_paint = set()
+for _ in range(K):
+    p, d = map(int, input().split())
+    depth = bfs(graph, p, d)
+    depths[p] = depth
+    for node, dep in depth.items():
+        if dep == d:
+            can_paint.add(node)
+        else:
+            not_paint.add(node)
+
+can_paint = can_paint - not_paint
+if not can_paint:
+    print("No")
+    exit()
+
+for depth in depths.values():
+    flag = False
+    for node in can_paint:
+        if node in depth:
+            flag = True
+            break
+    if not flag:
+        print("No")
+        exit()
+
+ans = ["0"] * N
+for node in can_paint:
+    ans[node - 1] = "1"
+
+print("Yes")
+print("".join(ans))
