59de17a03d391139dff32e31aaaf1f4e9dde3ada
yu7400ki <yu7400ki@gmail.com>
2023-04-22 21:22:52 +0900

    [WA] abc299/abc299_c Python (3.8.2) at 2023-04-22 21:22:52
    {
        "id": 40847047,
        "epoch_second": 1682166172,
        "problem_id": "abc299_c",
        "contest_id": "abc299",
        "user_id": "yu7400ki",
        "language": "Python (3.8.2)",
        "point": 0.0,
        "length": 167,
        "result": "WA",
        "execution_time": 48
    }

--- atcoder.jp/Python (3.8.2)/abc299/abc299_c.py
+++ atcoder.jp/Python (3.8.2)/abc299/abc299_c.py
@@ -1,23 +1,14 @@
+import re
+
 N = int(input())
 S = input()
 
-def solve(s):
-    res = 0
-    tmp = 0
-    if "o" not in s:
-        return -1
-    first = s.find("-")
-    if first == -1:
-        return 0
-    for i in range(first + 1, N):
-        if s[i] == "o":
-            tmp += 1
-        else:
-            res = max(res, tmp)
-            tmp = 0
-    res = max(res, tmp)
-    return res
+S = re.sub(r"-+", "-", S)
 
-ans = max(solve(S), solve(S[::-1]))
+if S == "-":
+    print(-1)
+    exit()
 
-print(ans)
+S = S.split("-")
+
+print(len(max(S, key=len)))
