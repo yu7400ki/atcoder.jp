994b7ed18f4ee6948e7a768b42419aa02f843133
yu7400ki <yu7400ki@gmail.com>
2023-05-13 22:05:19 +0900

    [TLE] abc301/abc301_d PyPy3 (7.3.0) at 2023-05-13 22:05:19
    {
        "id": 41378294,
        "epoch_second": 1683983119,
        "problem_id": "abc301_d",
        "contest_id": "abc301",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 550,
        "result": "TLE",
        "execution_time": 2207
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc301/abc301_d.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc301/abc301_d.py
@@ -7,30 +7,21 @@ if int(min_s, 2) > N:
     print(-1)
     exit()
 
-temp = 0
-
 def dfs(s, i = 0):
-    global temp
     num = int("".join(s).replace("?", "0"), 2)
     if num > N:
-        return "0"
-    num = int("".join(s).replace("?", "1"), 2)
-    if num <= temp:
         return "0"
     if i == len(S):
         return "".join(s)
     if S[i] == "?":
-        s[i] = "0"
-        l = dfs(s, i + 1)
         s[i] = "1"
+        l = dfs(s, i + 1)
+        s[i] = "0"
         r = dfs(s, i + 1)
         s[i] = "?"
         res = max(l, r, key=lambda x: int(x, 2))
     else:
         res = dfs(s, i + 1)
-    n = int(res, 2)
-    if n > temp:
-        temp = n
     return res
 
 print(int(dfs(S), 2))
