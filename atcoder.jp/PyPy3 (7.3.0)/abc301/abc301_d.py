e3653f43c38ab722d250661261ed9d7bfb2a9316
yu7400ki <yu7400ki@gmail.com>
2023-05-13 21:54:17 +0900

    [TLE] abc301/abc301_d PyPy3 (7.3.0) at 2023-05-13 21:54:17
    {
        "id": 41374578,
        "epoch_second": 1683982457,
        "problem_id": "abc301_d",
        "contest_id": "abc301",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 536,
        "result": "TLE",
        "execution_time": 2208
    }

---
+++ atcoder.jp/PyPy3 (7.3.0)/abc301/abc301_d.py
@@ -0,0 +1,26 @@
+S = list(input())
+N = int(input())
+
+min_s = "".join(S).replace("?", "0")
+
+if int(min_s, 2) > N:
+    print(-1)
+    exit()
+
+def dfs(s, i = 0):
+    num = int("".join(s).replace("?", "0"), 2)
+    if num > N:
+        return "0"
+    if i == len(S):
+        return "".join(s)
+    if S[i] == "?":
+        s[i] = "0"
+        l = dfs(s, i + 1)
+        s[i] = "1"
+        r = dfs(s, i + 1)
+        s[i] = "?"
+        return max(l, r, key=lambda x: int(x, 2))
+    else:
+        return dfs(s, i + 1)
+
+print(int(dfs(S), 2))
