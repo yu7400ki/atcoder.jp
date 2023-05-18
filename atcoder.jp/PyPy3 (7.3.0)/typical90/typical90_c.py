9cbb5ffd2074a2ebad4363357ac088b64fa997b7
yu7400ki <yu7400ki@gmail.com>
2023-05-18 13:25:35 +0900

    [AC] typical90/typical90_c PyPy3 (7.3.0) at 2023-05-18 13:25:35
    {
        "id": 41489889,
        "epoch_second": 1684383935,
        "problem_id": "typical90_c",
        "contest_id": "typical90",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 4.0,
        "length": 641,
        "result": "AC",
        "execution_time": 379
    }

--- atcoder.jp/PyPy3 (7.3.0)/typical90/typical90_c.py
+++ atcoder.jp/PyPy3 (7.3.0)/typical90/typical90_c.py
@@ -1,29 +1,30 @@
 from collections import defaultdict, deque
 
+def bfs(graph, n):
+    depth = defaultdict(lambda : -1)
+    depth[n] = 0
+    queue = deque()
+    queue.append(n)
+
+    while queue:
+        u = queue.popleft()
+        for v in graph[u]:
+            if depth[v] == -1:
+                queue.append(v)
+                depth[v] = depth[u] + 1
+
+    return depth
+
+
 N = int(input())
-graph = defaultdict(list)
+graph = defaultdict(set)
+for _ in range(N - 1):
+    a, b = map(int, input().split())
+    graph[a].add(b)
+    graph[b].add(a)
 
-for _ in range(N-1):
-	a,b = map(int,input().split())
-	graph[a].append(b)
-	graph[b].append(a)
+depth = bfs(graph, 1)
+u = max(depth, key=lambda x: depth[x])
+depth = bfs(graph, u)
 
-def bfs(n):
-	dic = defaultdict(lambda : -1)
-	m = 0
-	queue = deque()
-	queue.append(n)
-	dic[n] = 0
-	while len(queue) != 0:
-		pos = queue.popleft()
-		for i in graph[pos]:
-			if dic[i] == -1:
-				queue.append(i)
-				dic[i] = dic[pos] + 1
-				if dic[pos] + 1 > m:
-					ans = i
-	return ans,dic
-
-m = bfs(1)[0]
-n = bfs(m)
-print(n[1][n[0]]+1)
+print(max(depth.values()) + 1)
