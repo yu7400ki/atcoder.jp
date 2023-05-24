942612015fd3ac086870065cb6b39c05dbc2f8b5
yu7400ki <yu7400ki@gmail.com>
2023-05-24 09:35:47 +0900

    [AC] typical90/typical90_i PyPy3 (7.3.0) at 2023-05-24 09:35:47
    {
        "id": 41658925,
        "epoch_second": 1684888547,
        "problem_id": "typical90_i",
        "contest_id": "typical90",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 6.0,
        "length": 884,
        "result": "AC",
        "execution_time": 1285
    }

--- atcoder.jp/PyPy3 (7.3.0)/typical90/typical90_i.py
+++ atcoder.jp/PyPy3 (7.3.0)/typical90/typical90_i.py
@@ -1,47 +1,35 @@
-from collections import namedtuple
-from math import atan2, degrees
-
-Point = namedtuple('Point', ['x', 'y'])
+import math
+from bisect import bisect_left
 
 N = int(input())
-XY = [Point(*map(int, input().split())) for _ in range(N)]
+XY = [tuple(map(int, input().split())) for _ in range(N)]
 
-
-def calc_slope(point1, point2):
-    deg =  degrees(atan2(point2.y - point1.y, point2.x - point1.x))
-    return deg if deg >= 0 else 360 + deg
-
+def calc_slope_deg(x1, y1, x2, y2):
+    deg = math.degrees(math.atan2(y2 - y1, x2 - x1))
+    if deg < 0:
+        deg += 360
+    return deg
 
 def deg_diff(deg1, deg2):
     diff = abs(deg1 - deg2)
     return min(diff, 360 - diff)
 
-
-def binary_search(lst, target, ok = -1, ng = None, key = None):
-    ng = len(lst) if ng is None else ng
-    key = (lambda x: lst[x] <= target) if key is None else key
-
-    while abs(ok - ng) > 1:
-        mid = (ok + ng) // 2
-        if key(mid):
-            ok = mid
-        else:
-            ng = mid
-
-    return ok
-
-
 ans = 0
-for p_j in XY:
-    slopes_dict = {}
-    for p_i in XY:
-        if p_i == p_j:
+for j in range(N):
+    slopes = []
+    for i in range(N):
+        if j == i:
             continue
-        slopes_dict[p_i] = calc_slope(p_j, p_i)
-    slopes = sorted(slopes_dict.values())
-    for p_i, slope in slopes_dict.items():
-        ok = min(binary_search(slopes, 360 - slope), 0)
-        or_ = max(ok + 1, len(slopes) - 1)
-        ans = max(ans, deg_diff(slopes[ok], slope), deg_diff(slopes[or_], slope))
+        x1, y1 = XY[j]
+        x2, y2 = XY[i]
+        slopes.append(calc_slope_deg(x1, y1, x2, y2))
+    slopes.sort()
+    for deg in slopes:
+        idx = bisect_left(slopes, (deg + 180) % 360)
+        idx = min(idx, len(slopes) - 1)
+        deg1 = deg_diff(deg, slopes[idx])
+        idx = max(idx - 1, 0)
+        deg2 = deg_diff(deg, slopes[idx])
+        ans = max(ans, deg1, deg2)
 
 print(ans)
