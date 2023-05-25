c7225aaae470b9e9254634100d4787890cb1c86f
yu7400ki <yu7400ki@gmail.com>
2023-05-25 11:02:10 +0900

    [AC] typical90/typical90_j PyPy3 (7.3.0) at 2023-05-25 11:02:10
    {
        "id": 41678282,
        "epoch_second": 1684980130,
        "problem_id": "typical90_j",
        "contest_id": "typical90",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 2.0,
        "length": 377,
        "result": "AC",
        "execution_time": 538
    }

--- atcoder.jp/PyPy3 (7.3.0)/typical90/typical90_j.py
+++ atcoder.jp/PyPy3 (7.3.0)/typical90/typical90_j.py
@@ -1,18 +1,17 @@
 N = int(input())
-sum_1 = [0] * (N+1)
-sum_2 = [0] * (N+1)
-for i in range(1, N+1):
-    c, p = map(int,input().split())
-    if c == 1:
-        sum_1[i] = (sum_1[i-1] + p)
-        sum_2[i] = (sum_2[i-1])
-    elif c == 2:
-        sum_1[i] = (sum_1[i-1])
-        sum_2[i] = (sum_2[i-1] + p)
+
+c_1 = [0] * (N + 1)
+c_2 = [0] * (N + 1)
+for i in range(1, N + 1):
+    C, P = map(int, input().split())
+    if C == 1:
+        c_1[i] = P
+    else:
+        c_2[i] = P
+    c_1[i] += c_1[i - 1]
+    c_2[i] += c_2[i - 1]
 
 Q = int(input())
 for _ in range(Q):
-    l, r = map(int,input().split())
-    ans_1 = sum_1[r] - sum_1[l-1]
-    ans_2 = sum_2[r] - sum_2[l-1]
-    print(ans_1, ans_2)
+    L, R = map(int, input().split())
+    print(c_1[R] - c_1[L - 1], c_2[R] - c_2[L - 1])
