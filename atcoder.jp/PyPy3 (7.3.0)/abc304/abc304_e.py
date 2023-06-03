456db7d3f676d123a3fda1779b93ec10793f0480
yu7400ki <yu7400ki@gmail.com>
2023-06-03 22:38:22 +0900

    [AC] abc304/abc304_e PyPy3 (7.3.0) at 2023-06-03 22:38:22
    {
        "id": 41973376,
        "epoch_second": 1685799502,
        "problem_id": "abc304_e",
        "contest_id": "abc304",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 475.0,
        "length": 1647,
        "result": "AC",
        "execution_time": 1590
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc304/abc304_e.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc304/abc304_e.py
@@ -1,35 +1,31 @@
 class UnionFind():
+    # 初期化
     def __init__(self, n):
-        self.n = n
-        self.parents = [-1] * n
+        self.par = [-1] * n
+        self.rank = [0] * n
+        self.siz = [1] * n
 
+    # 根を求める
     def find(self, x):
-        if self.parents[x] < 0:
-            return x
+        if self.par[x] == -1: return x # x が根の場合は x を返す
         else:
-            self.parents[x] = self.find(self.parents[x])
-            return self.parents[x]
+            self.par[x] = self.find(self.par[x]) # 経路圧縮
+            return self.par[x]
 
+    # x を含むグループと y を含むグループを併合する
     def union(self, x, y):
-        x = self.find(x)
-        y = self.find(y)
-        if x == y: return
-        if self.parents[x] > self.parents[y]:
-            x, y = y, x
-        self.parents[x] += self.parents[y]
-        self.parents[y] = x
-
-    def size(self, x):
-        return -self.parents[self.find(x)]
-
-    def same(self, x, y):
-        return self.find(x) == self.find(y)
-
-    def roots(self):
-        return [i for i, x in enumerate(self.parents) if x < 0]
-
-    def group(self):
-        return len(self.roots())
+        # x 側と y 側の根を取得する
+        rx = self.find(x)
+        ry = self.find(y)
+        if rx == ry: return False # すでに同じグループのときは何もしない
+        # union by rank
+        if self.rank[rx] < self.rank[ry]: # ry 側の rank が小さくなるようにする
+            rx, ry = ry, rx
+        self.par[ry] = rx # ry を rx の子とする
+        if self.rank[rx] == self.rank[ry]: # rx 側の rank を調整する
+            self.rank[rx] += 1
+        self.siz[rx] += self.siz[ry] # rx 側の siz を調整する
+        return True
 
 N, M = map(int, input().split())
 uf = UnionFind(N)
