d1e7fded4fad17b77ce3a8192810a075f113bfbf
yu7400ki <yu7400ki@gmail.com>
2023-07-02 01:20:32 +0900

    [AC] abc308/abc308_d PyPy3 (7.3.0) at 2023-07-02 01:20:32
    {
        "id": 43162689,
        "epoch_second": 1688228432,
        "problem_id": "abc308_d",
        "contest_id": "abc308",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 400.0,
        "length": 976,
        "result": "AC",
        "execution_time": 362
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc308/abc308_d.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc308/abc308_d.py
@@ -1,31 +1,41 @@
-import sys
-sys.setrecursionlimit(10 ** 6)
+from collections import defaultdict, deque
 
 H, W = map(int, input().split())
 S = [list(input()) for _ in range(H)]
 
 snuke = "snuke"
-def dfs(x, y, traversed = set()):
-    traversed.add((x, y))
-    if (x, y) == (H - 1, W - 1):
-        return True
-    flag = False
-    c = S[x][y]
-    if c not in snuke:
-        return False
-    for dx, dy in ((1, 0), (-1, 0), (0, 1), (0, -1)):
-        nx, ny = x + dx, y + dy
-        if not (0 <= nx < H and 0 <= ny < W):
-            continue
-        if (nx, ny) in traversed:
-            continue
-        if S[nx][ny] not in snuke:
-            continue
-        if (snuke.index(c) + 1) % 5 == snuke.index(S[nx][ny]):
-            flag |= dfs(nx, ny, traversed)
-    return flag
 
-if dfs(0, 0):
+def is_ok(u, v):
+    x, y = u
+    nx, ny = v
+    if not (0 <= nx < H and 0 <= ny < W):
+        return False
+    if S[x][y] not in snuke:
+        return False
+    if S[nx][ny] not in snuke:
+        return False
+    if (snuke.index(S[x][y]) + 1) % 5 != snuke.index(S[nx][ny]):
+        return False
+    return True
+
+def bfs(x, y):
+    depth = defaultdict(lambda : -1)
+    depth[(x, y)] = 0
+    queue = deque()
+    queue.append((x, y))
+
+    while queue:
+        u = queue.popleft()
+        for dx, dy in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+            v = (u[0] + dx, u[1] + dy)
+            if depth[v] == -1 and is_ok(u, v):
+                queue.append(v)
+                depth[v] = depth[u] + 1
+
+    return depth
+
+depth = bfs(0, 0)
+if depth[(H - 1, W - 1)] != -1:
     print("Yes")
 else:
     print("No")
