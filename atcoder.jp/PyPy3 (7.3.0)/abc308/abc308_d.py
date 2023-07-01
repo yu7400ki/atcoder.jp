8119caa282bdf403200cc270b640354d1b900f33
yu7400ki <yu7400ki@gmail.com>
2023-07-02 01:08:33 +0900

    [RE] abc308/abc308_d PyPy3 (7.3.0) at 2023-07-02 01:08:33
    {
        "id": 43162389,
        "epoch_second": 1688227713,
        "problem_id": "abc308_d",
        "contest_id": "abc308",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 746,
        "result": "RE",
        "execution_time": 268
    }

---
+++ atcoder.jp/PyPy3 (7.3.0)/abc308/abc308_d.py
@@ -0,0 +1,28 @@
+H, W = map(int, input().split())
+S = [list(input()) for _ in range(H)]
+
+snuke = "snuke"
+def dfs(x, y, traversed = set()):
+    traversed.add((x, y))
+    if (x, y) == (H - 1, W - 1):
+        return True
+    flag = False
+    c = S[x][y]
+    if c not in snuke:
+        return False
+    for dx, dy in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+        nx, ny = x + dx, y + dy
+        if not (0 <= nx < H and 0 <= ny < W):
+            continue
+        if (nx, ny) in traversed:
+            continue
+        if S[nx][ny] not in snuke:
+            continue
+        if (snuke.index(c) + 1) % 5 == snuke.index(S[nx][ny]):
+            flag |= dfs(nx, ny, traversed)
+    return flag
+
+if dfs(0, 0):
+    print("Yes")
+else:
+    print("No")
