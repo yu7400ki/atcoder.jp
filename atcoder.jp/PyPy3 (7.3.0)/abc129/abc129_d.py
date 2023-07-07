4f035c6e3e0394033ca5ae9a3113e1a0cc5fc250
yu7400ki <yu7400ki@gmail.com>
2023-07-08 02:40:10 +0900

    [WA] abc129/abc129_d PyPy3 (7.3.0) at 2023-07-08 02:40:10
    {
        "id": 43320786,
        "epoch_second": 1688751610,
        "problem_id": "abc129_d",
        "contest_id": "abc129",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 798,
        "result": "WA",
        "execution_time": 762
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc129/abc129_d.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc129/abc129_d.py
@@ -7,36 +7,28 @@ cntV = [[0] * W for _ in range(H)]
 for y in range(H):
     tmp = 0
     for x in range(W):
-        s = S[y][x]
-        if s == ".":
+        if S[y][x] == ".":
             tmp += 1
         else:
             tmp = 0
         cntH[y][x] = tmp
     for x in range(W - 1, 0, -1):
-        if cntH[y][x] == 0 or cntH[y][x - 1] == 0:
-            continue
-        else:
-            cntH[y][x - 1] = cntH[y][x]
+        cntH[y][x - 1] = max(cntH[y][x - 1], cntH[y][x])
 
 for x in range(W):
     tmp = 0
     for y in range(H):
-        s = S[y][x]
-        if s == ".":
+        if S[y][x] == ".":
             tmp += 1
         else:
             tmp = 0
         cntV[y][x] = tmp
     for y in range(H - 1, 0, -1):
-        if cntV[y][x] == 0 or cntV[y - 1][x] == 0:
-            continue
-        else:
-            cntV[y - 1][x] = cntV[y][x]
+        cntV[y - 1][x] = max(cntV[y - 1][x], cntV[y][x])
 
 ans = 0
-for y in range(H):
-    for x in range(W):
-        ans = max(ans, cntH[y][x] + cntV[y][x] - 1)
+for i in range(H * W):
+    y, x = divmod(i, W)
+    ans = max(ans, cntH[y][x] + cntV[y][x] - 1)
 
 print(ans)
