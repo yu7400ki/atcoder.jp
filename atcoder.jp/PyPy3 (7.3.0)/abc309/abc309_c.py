c05593b419d1a2c85a7056de32e7d5d6af9b8605
yu7400ki <yu7400ki@gmail.com>
2023-07-08 21:48:54 +0900

    [WA] abc309/abc309_c PyPy3 (7.3.0) at 2023-07-08 21:48:54
    {
        "id": 43370369,
        "epoch_second": 1688820534,
        "problem_id": "abc309_c",
        "contest_id": "abc309",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 497,
        "result": "WA",
        "execution_time": 667
    }

--- atcoder.jp/PyPy3 (7.3.0)/abc309/abc309_c.py
+++ atcoder.jp/PyPy3 (7.3.0)/abc309/abc309_c.py
@@ -1,11 +1,17 @@
 N, K = map(int, input().split())
-AB = [list(map(int, input().split())) for _ in range(N)]
-AB_sorted = sorted(AB, key=lambda x: x[0])
+A = set()
+B = {}
+for _ in range(N):
+    a, b = map(int, input().split())
+    A.add(a)
+    B[a] = B.get(a, 0) + b
+N = len(A)
 
+A_sorted = sorted(A)
 acc = [0] * (N + 1)
-for i, (a_i, b_i) in enumerate(AB_sorted):
-    acc[0] += b_i
-    acc[i + 1] -= b_i
+for i, a_i in enumerate(A_sorted):
+    acc[0] += B[a_i]
+    acc[i + 1] -= B[a_i]
 
 for i in range(N):
     acc[i + 1] += acc[i]
@@ -15,5 +21,5 @@ for i in range(N):
         if i == 0:
             print(1)
         else:
-            print(AB_sorted[i - 1][0] + 1)
+            print(A_sorted[i - 1] + 1)
         break
