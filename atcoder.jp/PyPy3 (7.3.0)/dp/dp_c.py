37e071bf9b336a25418f009d133a2f29e9d0fb49
yu7400ki <yu7400ki@gmail.com>
2023-08-02 17:44:47 +0900

    [AC] dp/dp_c PyPy3 (7.3.0) at 2023-08-02 17:44:47
    {
        "id": 44181391,
        "epoch_second": 1690965887,
        "problem_id": "dp_c",
        "contest_id": "dp",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 100.0,
        "length": 345,
        "result": "AC",
        "execution_time": 245
    }

--- atcoder.jp/PyPy3 (7.3.0)/dp/dp_c.py
+++ atcoder.jp/PyPy3 (7.3.0)/dp/dp_c.py
@@ -1,16 +1,12 @@
 N = int(input())
-abc = [tuple(map(int, input().split())) for _ in range(N)]
+abc = [list(map(int, input().split())) for _ in range(N)]
 
 dp = [[0] * 3 for _ in range(N)]
-dp[0][0] = abc[0][0]
-dp[0][1] = abc[0][1]
-dp[0][2] = abc[0][2]
+dp[0] = abc[0]
 
-for i in range(N-1):
-    for j in range(3):
-        for k in range(3):
-            if j == k:
-                continue
-            dp[i+1][k] = max(dp[i+1][k], dp[i][j] + abc[i+1][k])
+for i in range(1, N):
+    dp[i][0] = max(dp[i-1][1], dp[i-1][2]) + abc[i][0]
+    dp[i][1] = max(dp[i-1][0], dp[i-1][2]) + abc[i][1]
+    dp[i][2] = max(dp[i-1][0], dp[i-1][1]) + abc[i][2]
 
 print(max(dp[N-1]))
