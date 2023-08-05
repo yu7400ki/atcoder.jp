d33dcc99b9b2aa9ba1ac131cc6737c35450ff03c
yu7400ki <yu7400ki@gmail.com>
2023-08-05 23:58:35 +0900

    [WA] abc313/abc313_c PyPy3 (7.3.0) at 2023-08-05 23:58:35
    {
        "id": 44313211,
        "epoch_second": 1691247515,
        "problem_id": "abc313_c",
        "contest_id": "abc313",
        "user_id": "yu7400ki",
        "language": "PyPy3 (7.3.0)",
        "point": 0.0,
        "length": 197,
        "result": "WA",
        "execution_time": 128
    }

---
+++ atcoder.jp/PyPy3 (7.3.0)/abc313/abc313_c.py
@@ -0,0 +1,8 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+sum_a = sum(A)
+b = [sum_a // N + (sum_a % N > i) for i in range(N)][::-1]
+c = [abs(b[i] - A[i]) for i in range(N)]
+
+print(sum(c) // 2)
