2d41064a59b54da23b362c14539e1e643bb14331
yu7400ki <yu7400ki@gmail.com>
2023-08-06 17:39:41 +0900

    [AC] abc264/abc264_d Python (3.8.2) at 2023-08-06 17:39:41
    {
        "id": 44331650,
        "epoch_second": 1691311181,
        "problem_id": "abc264_d",
        "contest_id": "abc264",
        "user_id": "yu7400ki",
        "language": "Python (3.8.2)",
        "point": 400.0,
        "length": 255,
        "result": "AC",
        "execution_time": 23
    }

--- atcoder.jp/Python (3.8.2)/abc264/abc264_d.py
+++ atcoder.jp/Python (3.8.2)/abc264/abc264_d.py
@@ -1,20 +1,12 @@
 S = list(input())
+N = len(S)
 
-for i in range(len(S)):
-    if S[i] == 'a': S[i] = 0
-    if S[i] == 't': S[i] = 1
-    if S[i] == 'c': S[i] = 2
-    if S[i] == 'o': S[i] = 3
-    if S[i] == 'd': S[i] = 4
-    if S[i] == 'e': S[i] = 5
-    if S[i] == 'r': S[i] = 6
+atcoder = "atcoder"
+ans = 0
+for i in range(N):
+    for j in range(N - i - 1):
+        if atcoder.index(S[j]) > atcoder.index(S[j + 1]):
+            S[j], S[j + 1] = S[j + 1], S[j]
+            ans += 1
 
-cnt = 0
-
-for i in range(len(S)):
-    for j in range(len(S)-i-1):
-        if S[j] > S[j+1]:
-            cnt += 1
-            S[j], S[j+1] = S[j+1], S[j]
-
-print(cnt)
+print(ans)
