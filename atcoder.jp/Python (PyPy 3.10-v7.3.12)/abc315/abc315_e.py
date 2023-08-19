469afabbc6e0cab8e1633a90ec00425b43557dcd
yu7400ki <yu7400ki@gmail.com>
2023-08-19 22:36:07 +0900

    [AC] abc315/abc315_e Python (PyPy 3.10-v7.3.12) at 2023-08-19 22:36:07
    {
        "id": 44761481,
        "epoch_second": 1692452167,
        "problem_id": "abc315_e",
        "contest_id": "abc315",
        "user_id": "yu7400ki",
        "language": "Python (PyPy 3.10-v7.3.12)",
        "point": 425.0,
        "length": 1262,
        "result": "AC",
        "execution_time": 390
    }

--- atcoder.jp/Python (PyPy 3.10-v7.3.12)/abc315/abc315_e.py
+++ atcoder.jp/Python (PyPy 3.10-v7.3.12)/abc315/abc315_e.py
@@ -1,14 +1,25 @@
 from collections import defaultdict, deque
 
-N = int(input())
-
-graph = defaultdict(list)
-for i in range(1, N + 1):
-    l = list(map(int, input().split()))
-    if len(l) == 1:
-        continue
-    _, *P = l
-    graph[i].extend(P)
+def topological_sort(graph):
+    indegree = defaultdict(int)
+    for k, v in graph.items():
+        for w in v:
+            indegree[w] += 1
+        if k not in indegree:
+            indegree[k] = 0
+    res = []
+    stack = []
+    for k, v in indegree.items():
+        if v == 0:
+            stack.append(k)
+    while stack:
+        v = stack.pop()
+        res.append(v)
+        for w in graph[v]:
+            indegree[w] -= 1
+            if indegree[w] == 0:
+                stack.append(w)
+    return res
 
 def bfs(graph, n):
     depth = defaultdict(lambda: -1)
@@ -25,8 +36,18 @@ def bfs(graph, n):
 
     return depth
 
-depth = bfs(graph, 1)
-depth = sorted(depth.items(), key=lambda x: x[1], reverse=True)
-ans = [k for k, v in depth if v >= 1]
+N = int(input())
 
+graph = defaultdict(list)
+for i in range(1, N + 1):
+    l = list(map(int, input().split()))
+    if len(l) == 1:
+        continue
+    _, *P = l
+    graph[i].extend(P)
+
+depth = bfs(graph, 1)
+sorted_depth = topological_sort(graph)[::-1]
+
+ans = list(filter(lambda x: x in depth and x != 1, sorted_depth))
 print(" ".join(map(str, ans)))
