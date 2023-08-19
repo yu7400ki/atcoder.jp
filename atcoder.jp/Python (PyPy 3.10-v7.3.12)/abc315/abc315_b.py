6756a6f70941c93390bdaadf7897338be2965fba
yu7400ki <yu7400ki@gmail.com>
2023-08-19 21:29:34 +0900

    [WA] abc315/abc315_b Python (PyPy 3.10-v7.3.12) at 2023-08-19 21:29:34
    {
        "id": 44731619,
        "epoch_second": 1692448174,
        "problem_id": "abc315_b",
        "contest_id": "abc315",
        "user_id": "yu7400ki",
        "language": "Python (PyPy 3.10-v7.3.12)",
        "point": 0.0,
        "length": 274,
        "result": "WA",
        "execution_time": 59
    }

---
+++ atcoder.jp/Python (PyPy 3.10-v7.3.12)/abc315/abc315_b.py
@@ -0,0 +1,14 @@
+M = int(input())
+D = list(map(int, input().split()))
+
+def ceil(a, b):
+    return (a + b - 1) // b
+
+d_sum = sum(D)
+d_mid = ceil(d_sum, 2)
+
+for i, d in enumerate(D):
+    d_sum -= d
+    if d_sum <= d_mid:
+        print(i + 1, d - d_mid + d_sum + 1)
+        break
