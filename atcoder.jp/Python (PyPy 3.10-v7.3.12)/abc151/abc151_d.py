4f911c69958641f70ac523e0f6fcf03c719b9793
yu7400ki <yu7400ki@gmail.com>
2023-09-09 02:40:28 +0900

    [AC] abc151/abc151_d Python (PyPy 3.10-v7.3.12) at 2023-09-09 02:40:28
    {
        "id": 45335482,
        "epoch_second": 1694194828,
        "problem_id": "abc151_d",
        "contest_id": "abc151",
        "user_id": "yu7400ki",
        "language": "Python (PyPy 3.10-v7.3.12)",
        "point": 400.0,
        "length": 1119,
        "result": "AC",
        "execution_time": 400
    }

---
+++ atcoder.jp/Python (PyPy 3.10-v7.3.12)/abc151/abc151_d.py
@@ -0,0 +1,35 @@
+H, W = map(int, input().split())
+S = [list(input()) for _ in range(H)]
+INF = 1 << 60
+
+def grid_to_graph(grid, wall="#"):
+    H, W = len(grid), len(grid[0])
+    graph = [[0] * (W * H) for _ in range(W * H)]
+    for i in range(H):
+        for j in range(W):
+            if grid[i][j] == wall:
+                continue
+            parent = i * W + j
+            for x, y in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
+                if 0 <= x < H and 0 <= y < W and grid[x][y] != wall:
+                    adj = x * W + y
+                    graph[parent][adj] = 1
+                    graph[adj][parent] = 1
+    return graph
+
+def warshall_floyd(graph):
+    V = len(graph)
+    dist = [[graph[i][j] if graph[i][j] != 0 else 0 if i == j else INF for j in range(V)] for i in range(V)]
+
+    for k in range(V):
+        for i in range(V):
+            for j in range(V):
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+
+    return dist
+
+graph = grid_to_graph(S)
+dist = warshall_floyd(graph)
+ans = max([max(d, key=lambda x: -1 if x == INF else x) for d in dist])
+
+print(ans)
