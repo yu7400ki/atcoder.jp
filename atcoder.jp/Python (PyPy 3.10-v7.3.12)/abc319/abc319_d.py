3e451d36686e7c8308b872ba3f0aa718db2fda41
yu7400ki <yu7400ki@gmail.com>
2023-09-09 22:10:29 +0900

    [WA] abc319/abc319_d Python (PyPy 3.10-v7.3.12) at 2023-09-09 22:10:29
    {
        "id": 45405155,
        "epoch_second": 1694265029,
        "problem_id": "abc319_d",
        "contest_id": "abc319",
        "user_id": "yu7400ki",
        "language": "Python (PyPy 3.10-v7.3.12)",
        "point": 0.0,
        "length": 433,
        "result": "WA",
        "execution_time": 2213
    }

---
+++ atcoder.jp/Python (PyPy 3.10-v7.3.12)/abc319/abc319_d.py
@@ -0,0 +1,23 @@
+N, M = map(int, input().split())
+L = list(map(int, input().split()))
+
+width = sum(L) // M
+
+while True:
+    row = 0
+    col = 1
+    for l in L:
+        if row + l <= width:
+            if row + l == width:
+                row += l
+            else:
+                row += l + 1
+        else:
+            row = l + 1
+            col += 1
+    if col <= M:
+        break
+    else:
+        width += 1
+
+print(width)
