cc962ad9f0ccc0dee65724a87260a480ad3683f5
yu7400ki <yu7400ki@gmail.com>
2023-09-15 00:37:54 +0900

    [AC] abc318/abc318_c Rust (rustc 1.70.0) at 2023-09-15 00:37:54
    {
        "id": 45530407,
        "epoch_second": 1694705874,
        "problem_id": "abc318_c",
        "contest_id": "abc318",
        "user_id": "yu7400ki",
        "language": "Rust (rustc 1.70.0)",
        "point": 300.0,
        "length": 521,
        "result": "AC",
        "execution_time": 22
    }

---
+++ atcoder.jp/Rust (rustc 1.70.0)/abc318/abc318_c.rs
@@ -0,0 +1,30 @@
+use proconio::{input, fastout};
+
+fn ceil(a: usize, b: usize) -> usize {
+    (a + b - 1) / b
+}
+
+#[fastout]
+fn main() {
+    input! {
+        n: usize,
+        d: usize,
+        p: usize,
+        mut f: [usize; n],
+    }
+
+    f.sort();
+    f.reverse();
+    let mut acc = vec![0; n + 1];
+    for i in 0..n {
+        acc[i + 1] = acc[i] + f[i];
+    }
+
+    let mut ans = usize::MAX;
+    for i in 0..=ceil(n, d) {
+        let l = i * d;
+        ans = ans.min((acc[n] - acc[l.min(n)]) + p * i);
+    }
+
+    println!("{}", ans);
+}
