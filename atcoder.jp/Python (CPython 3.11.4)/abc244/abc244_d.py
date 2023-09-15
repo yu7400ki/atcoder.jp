04999ce3f8e1b3425182a42d58ffa4e802460b1d
yu7400ki <yu7400ki@gmail.com>
2023-09-16 03:20:06 +0900

    [AC] abc244/abc244_d Python (CPython 3.11.4) at 2023-09-16 03:20:06
    {
        "id": 45554637,
        "epoch_second": 1694802006,
        "problem_id": "abc244_d",
        "contest_id": "abc244",
        "user_id": "yu7400ki",
        "language": "Python (CPython 3.11.4)",
        "point": 400.0,
        "length": 298,
        "result": "AC",
        "execution_time": 12
    }

--- atcoder.jp/Python (CPython 3.11.4)/abc244/abc244_d.py
+++ atcoder.jp/Python (CPython 3.11.4)/abc244/abc244_d.py
@@ -6,9 +6,11 @@ T = list(input().split())
 cnt = 0
 
 while S != T:
-    cnt += 1
     i = random.randint(0, 2)
     j = random.randint(0, 2)
+    if i == j:
+        continue
+    cnt += 1
     S[i], S[j] = S[j], S[i]
 
 if cnt % 2 == 0:
