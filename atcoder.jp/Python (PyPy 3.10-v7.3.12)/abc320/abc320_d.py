14ce24e95669ab8226c72ab20077302a1917416f
yu7400ki <yu7400ki@gmail.com>
2023-09-16 21:36:50 +0900

    [TLE] abc320/abc320_d Python (PyPy 3.10-v7.3.12) at 2023-09-16 21:36:50
    {
        "id": 45615133,
        "epoch_second": 1694867810,
        "problem_id": "abc320_d",
        "contest_id": "abc320",
        "user_id": "yu7400ki",
        "language": "Python (PyPy 3.10-v7.3.12)",
        "point": 0.0,
        "length": 959,
        "result": "TLE",
        "execution_time": 2837
    }

---
+++ atcoder.jp/Python (PyPy 3.10-v7.3.12)/abc320/abc320_d.py
@@ -0,0 +1,41 @@
+from collections import defaultdict, deque
+
+N, M = map(int, input().split())
+
+points = [[None] * N for _ in range(N)]
+graph = defaultdict(list)
+ans = [None] * N
+
+for _ in range(M):
+    A, B, X, Y = map(int, input().split())
+    A -= 1
+    B -= 1
+    graph[A].append(B)
+    graph[B].append(A)
+    points[A][B] = (X, Y)
+    points[B][A] = (-X, -Y)
+
+def bfs(graph: defaultdict, n) -> defaultdict:
+    depth = defaultdict(lambda: -1)
+    depth[n] = 0
+    ans[n] = (0, 0)
+    queue = deque()
+    queue.append(n)
+
+    while queue:
+        u = queue.popleft()
+        for v in graph[u]:
+            if depth[v] == -1:
+                queue.append(v)
+                depth[v] = depth[u] + 1
+                ans[v] = (ans[u][0] + points[u][v][0], ans[u][1] + points[u][v][1])
+
+    return depth
+
+bfs(graph, 0)
+
+for i in range(N):
+    if ans[i] is None:
+        print("undecidable")
+    else:
+        print(ans[i][0], ans[i][1])
