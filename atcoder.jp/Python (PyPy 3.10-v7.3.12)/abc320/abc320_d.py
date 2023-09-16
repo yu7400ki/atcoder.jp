783185498e6cd021c0bacafd684a80e217bfb904
yu7400ki <yu7400ki@gmail.com>
2023-09-16 22:45:23 +0900

    [AC] abc320/abc320_d Python (PyPy 3.10-v7.3.12) at 2023-09-16 22:45:23
    {
        "id": 45644673,
        "epoch_second": 1694871923,
        "problem_id": "abc320_d",
        "contest_id": "abc320",
        "user_id": "yu7400ki",
        "language": "Python (PyPy 3.10-v7.3.12)",
        "point": 400.0,
        "length": 767,
        "result": "AC",
        "execution_time": 465
    }

--- atcoder.jp/Python (PyPy 3.10-v7.3.12)/abc320/abc320_d.py
+++ atcoder.jp/Python (PyPy 3.10-v7.3.12)/abc320/abc320_d.py
@@ -1,41 +1,7 @@
 from collections import defaultdict, deque
 
-class UnionFind:
-    def __init__(self):
-        self.parents = defaultdict(lambda: -1)
-        self.rank = defaultdict(int)
-        self.siz = defaultdict(lambda: 1)
-
-    def find(self, x):
-        if self.parents[x] == -1:
-            return x
-        else:
-            self.parents[x] = self.find(self.parents[x])
-            return self.parents[x]
-
-    def union(self, x, y) -> bool:
-        rx = self.find(x)
-        ry = self.find(y)
-        if rx == ry:
-            return False
-        if self.rank[rx] < self.rank[ry]:
-            rx, ry = ry, rx
-        self.parents[ry] = rx
-        if self.rank[rx] == self.rank[ry]:
-            self.rank[rx] += 1
-        self.siz[rx] += self.siz[ry]
-        return True
-
-    def size(self, x) -> int:
-        return self.siz[self.find(x)]
-
-    def groups(self):
-        return {x: self.siz[x] for x in self.parents if self.parents[x] == -1}
-
 N, M = map(int, input().split())
 
-uf = UnionFind()
-points = [[None] * N for _ in range(N)]
 graph = defaultdict(list)
 ans = [None] * N
 
@@ -43,27 +9,21 @@ for _ in range(M):
     A, B, X, Y = map(int, input().split())
     A -= 1
     B -= 1
-    if uf.union(A, B):
-        graph[A].append(B)
-        graph[B].append(A)
-    points[A][B] = (X, Y)
-    points[B][A] = (-X, -Y)
+    graph[A].append((B, (X, Y)))
+    graph[B].append((A, (-X, -Y)))
 
 def bfs(graph: defaultdict, n) -> defaultdict:
-    visited = [False] * N
     ans[n] = (0, 0)
     queue = deque()
     queue.append(n)
 
     while queue:
         u = queue.popleft()
-        if visited[u]:
-            continue
-        visited[u] = True
-        for v in graph[u]:
+        for v, (dx, dy) in graph[u]:
             if ans[v] is None:
                 queue.append(v)
-                ans[v] = (ans[u][0] + points[u][v][0], ans[u][1] + points[u][v][1])
+                x, y = ans[u]
+                ans[v] = (x + dx, y + dy)
 
 bfs(graph, 0)
 
