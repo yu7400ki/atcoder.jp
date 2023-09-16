665d37c608b4e9ad9a4ce49e7d887deec7caeb3d
yu7400ki <yu7400ki@gmail.com>
2023-09-16 22:47:07 +0900

    [TLE] abc320/abc320_d Python (PyPy 3.10-v7.3.12) at 2023-09-16 22:47:07
    {
        "id": 45644898,
        "epoch_second": 1694872027,
        "problem_id": "abc320_d",
        "contest_id": "abc320",
        "user_id": "yu7400ki",
        "language": "Python (PyPy 3.10-v7.3.12)",
        "point": 0.0,
        "length": 871,
        "result": "TLE",
        "execution_time": 2837
    }

--- atcoder.jp/Python (PyPy 3.10-v7.3.12)/abc320/abc320_d.py
+++ atcoder.jp/Python (PyPy 3.10-v7.3.12)/abc320/abc320_d.py
@@ -2,6 +2,7 @@ from collections import defaultdict, deque
 
 N, M = map(int, input().split())
 
+points = [[None] * N for _ in range(N)]
 graph = defaultdict(list)
 ans = [None] * N
 
@@ -9,8 +10,10 @@ for _ in range(M):
     A, B, X, Y = map(int, input().split())
     A -= 1
     B -= 1
-    graph[A].append((B, (X, Y)))
-    graph[B].append((A, (-X, -Y)))
+    graph[A].append(B)
+    graph[B].append(A)
+    points[A][B] = (X, Y)
+    points[B][A] = (-X, -Y)
 
 def bfs(graph: defaultdict, n) -> defaultdict:
     ans[n] = (0, 0)
@@ -19,9 +22,10 @@ def bfs(graph: defaultdict, n) -> defaultdict:
 
     while queue:
         u = queue.popleft()
-        for v, (dx, dy) in graph[u]:
+        for v in graph[u]:
             if ans[v] is None:
                 queue.append(v)
+                dx, dy = points[u][v]
                 x, y = ans[u]
                 ans[v] = (x + dx, y + dy)
 
