a66ca5d0d68dbd3e4347ac997a674f81e7f8e6f1
yu7400ki <yu7400ki@gmail.com>
2023-09-16 21:52:17 +0900

    [TLE] abc320/abc320_d Python (PyPy 3.10-v7.3.12) at 2023-09-16 21:52:17
    {
        "id": 45624474,
        "epoch_second": 1694868737,
        "problem_id": "abc320_d",
        "contest_id": "abc320",
        "user_id": "yu7400ki",
        "language": "Python (PyPy 3.10-v7.3.12)",
        "point": 0.0,
        "length": 1914,
        "result": "TLE",
        "execution_time": 2838
    }

--- atcoder.jp/Python (PyPy 3.10-v7.3.12)/abc320/abc320_d.py
+++ atcoder.jp/Python (PyPy 3.10-v7.3.12)/abc320/abc320_d.py
@@ -1,7 +1,40 @@
 from collections import defaultdict, deque
 
+class UnionFind:
+    def __init__(self):
+        self.parents = defaultdict(lambda: -1)
+        self.rank = defaultdict(int)
+        self.siz = defaultdict(lambda: 1)
+
+    def find(self, x):
+        if self.parents[x] == -1:
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
+
+    def union(self, x, y) -> bool:
+        rx = self.find(x)
+        ry = self.find(y)
+        if rx == ry:
+            return False
+        if self.rank[rx] < self.rank[ry]:
+            rx, ry = ry, rx
+        self.parents[ry] = rx
+        if self.rank[rx] == self.rank[ry]:
+            self.rank[rx] += 1
+        self.siz[rx] += self.siz[ry]
+        return True
+
+    def size(self, x) -> int:
+        return self.siz[self.find(x)]
+
+    def groups(self):
+        return {x: self.siz[x] for x in self.parents if self.parents[x] == -1}
+
 N, M = map(int, input().split())
 
+uf = UnionFind()
 points = [[None] * N for _ in range(N)]
 graph = defaultdict(list)
 ans = [None] * N
@@ -10,14 +43,13 @@ for _ in range(M):
     A, B, X, Y = map(int, input().split())
     A -= 1
     B -= 1
-    graph[A].append(B)
-    graph[B].append(A)
+    if uf.union(A, B):
+        graph[A].append(B)
+        graph[B].append(A)
     points[A][B] = (X, Y)
     points[B][A] = (-X, -Y)
 
 def bfs(graph: defaultdict, n) -> defaultdict:
-    # depth = defaultdict(lambda: -1)
-    # depth[n] = 0
     visited = [False] * N
     ans[n] = (0, 0)
     queue = deque()
