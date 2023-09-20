59a2260b4b3951d0403f013c0f9dfae9699954a8
yu7400ki <yu7400ki@gmail.com>
2023-09-20 12:56:28 +0900

    [AC] abc112/abc112_c Python (CPython 3.11.4) at 2023-09-20 12:56:28
    {
        "id": 45743088,
        "epoch_second": 1695182188,
        "problem_id": "abc112_c",
        "contest_id": "abc112",
        "user_id": "yu7400ki",
        "language": "Python (CPython 3.11.4)",
        "point": 300.0,
        "length": 547,
        "result": "AC",
        "execution_time": 282
    }

--- atcoder.jp/Python (CPython 3.11.4)/abc112/abc112_c.py
+++ atcoder.jp/Python (CPython 3.11.4)/abc112/abc112_c.py
@@ -11,5 +11,10 @@ for cx, cy in product(range(101), repeat=2):
         H = h + abs(x - cx) + abs(y - cy)
         heights.append(H)
     if len(set(heights)) == 1:
-        print(cx, cy, heights[0])
-        break
+        H = heights[0]
+        for x, y, h in xyh:
+            if h != max(H - abs(x - cx) - abs(y - cy), 0):
+                break
+        else:
+            print(cx, cy, H)
+            exit()
