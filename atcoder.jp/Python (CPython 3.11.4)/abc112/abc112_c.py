1579c2e2094bb3de3d186832e0c16adfa68c5ec9
yu7400ki <yu7400ki@gmail.com>
2023-09-20 12:01:49 +0900

    [WA] abc112/abc112_c Python (CPython 3.11.4) at 2023-09-20 12:01:49
    {
        "id": 45742359,
        "epoch_second": 1695178909,
        "problem_id": "abc112_c",
        "contest_id": "abc112",
        "user_id": "yu7400ki",
        "language": "Python (CPython 3.11.4)",
        "point": 0.0,
        "length": 396,
        "result": "WA",
        "execution_time": 263
    }

---
+++ atcoder.jp/Python (CPython 3.11.4)/abc112/abc112_c.py
@@ -0,0 +1,15 @@
+from itertools import product
+
+N = int(input())
+xyh = [tuple(map(int, input().split())) for _ in range(N)]
+
+for cx, cy in product(range(101), repeat=2):
+    heights = []
+    for x, y, h in xyh:
+        if h == 0:
+            continue
+        H = h + abs(x - cx) + abs(y - cy)
+        heights.append(H)
+    if len(set(heights)) == 1:
+        print(cx, cy, heights[0])
+        break
