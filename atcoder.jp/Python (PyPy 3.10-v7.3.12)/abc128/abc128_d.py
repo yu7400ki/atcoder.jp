5c6e657a975a94e133cb2fcdaff7a45d1846f878
yu7400ki <yu7400ki@gmail.com>
2023-09-20 22:26:49 +0900

    [RE] abc128/abc128_d Python (PyPy 3.10-v7.3.12) at 2023-09-20 22:26:49
    {
        "id": 45755174,
        "epoch_second": 1695216409,
        "problem_id": "abc128_d",
        "contest_id": "abc128",
        "user_id": "yu7400ki",
        "language": "Python (PyPy 3.10-v7.3.12)",
        "point": 0.0,
        "length": 521,
        "result": "RE",
        "execution_time": 140
    }

---
+++ atcoder.jp/Python (PyPy 3.10-v7.3.12)/abc128/abc128_d.py
@@ -0,0 +1,22 @@
+N, K = map(int, input().split())
+V = list(map(int, input().split()))
+
+ans = 0
+for l in range(K+1):
+    for r in range(K+1):
+        n = K - l - r
+        if n < 0:
+            break
+        get = []
+        for i in range(l):
+            get.append(V[i])
+        for j in range(r):
+            get.append(V[N - j - 1])
+        get.sort(reverse=True)
+        while get and get[-1] < 0 and n > 0:
+            get.pop()
+            n -= 1
+        tmp = sum(get)
+        ans = max(ans, tmp)
+
+print(ans)
