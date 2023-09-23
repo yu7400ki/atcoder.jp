6606d8ff2f32c6fe9809a42fb44d98a15b80789f
yu7400ki <yu7400ki@gmail.com>
2023-09-23 23:33:10 +0900

    [RE] abc321/abc321_e Python (CPython 3.11.4) at 2023-09-23 23:33:10
    {
        "id": 45890869,
        "epoch_second": 1695479590,
        "problem_id": "abc321_e",
        "contest_id": "abc321",
        "user_id": "yu7400ki",
        "language": "Python (CPython 3.11.4)",
        "point": 0.0,
        "length": 598,
        "result": "RE",
        "execution_time": 1068
    }

--- atcoder.jp/Python (CPython 3.11.4)/abc321/abc321_e.py
+++ atcoder.jp/Python (CPython 3.11.4)/abc321/abc321_e.py
@@ -8,23 +8,20 @@ def search(n, x, k):
     else:
         return min(n, right) - left + 1
 
+
 def solve():
     N, X, K = map(int, input().split())
     ans = search(N, X, K)
-    current = X
-    while current != 1 and K > 0:
+    parent = X
+    while parent != 1 and K > 0:
+        opposite = parent ^ 1
+        parent = parent >> 1
         K -= 1
         if K == 0:
             ans += 1
             break
-        parent = current >> 1
-        if current & 1 == 0:
-            opposite = (parent << 1) + 1
-        else:
-            opposite = parent << 1
         d = K - 1
         ans += search(N, opposite, d)
-        current = parent
     print(ans)
 
 
