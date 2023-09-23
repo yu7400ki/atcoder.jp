294fe95d4e6c0230aa1f44d292cb5b9e15072893
yu7400ki <yu7400ki@gmail.com>
2023-09-23 23:23:36 +0900

    [RE] abc321/abc321_e Python (CPython 3.11.4) at 2023-09-23 23:23:36
    {
        "id": 45890348,
        "epoch_second": 1695479016,
        "problem_id": "abc321_e",
        "contest_id": "abc321",
        "user_id": "yu7400ki",
        "language": "Python (CPython 3.11.4)",
        "point": 0.0,
        "length": 717,
        "result": "RE",
        "execution_time": 1164
    }

--- atcoder.jp/Python (CPython 3.11.4)/abc321/abc321_e.py
+++ atcoder.jp/Python (CPython 3.11.4)/abc321/abc321_e.py
@@ -1,30 +1,30 @@
+def search(n, x, k):
+    if k < 0:
+        return 0
+    left = x << k
+    right = left + (1 << k) - 1
+    if left > n:
+        return 0
+    else:
+        return min(n, right) - left + 1
+
 def solve():
     N, X, K = map(int, input().split())
-    left = X << K
-    right = left + (1 << K) - 1
-    if left > N:
-        ans = 0
-    else:
-        ans = min(N, right) - left + 1
-    parent = X
-    while parent != 1 and K > 0:
+    ans = search(N, X, K)
+    current = X
+    while current != 1 and K > 0:
         K -= 1
         if K == 0:
             ans += 1
             break
-        b = bin(parent)[2:]
-        parent = parent >> 1
+        parent = current >> 1
+        if current & 1 == 0:
+            opposite = (parent << 1) + 1
+        else:
+            opposite = parent << 1
         d = K - 1
-        if b[-1] == "0":
-            root = (parent << 1) + 1
-        else:
-            root = parent << 1
-        left = root << d
-        right = left + (1 << d) - 1
-        if left > N:
-            ans += 0
-        else:
-            ans += min(N, right) - left + 1
+        ans += search(N, opposite, d)
+        current = parent
     print(ans)
 
 
