8add4d1249e756484cba1c62fab0926ec269389b
yu7400ki <yu7400ki@gmail.com>
2023-10-03 01:28:49 +0900

    [AC] abc106/abc106_d Python (CPython 3.11.4) at 2023-10-03 01:28:49
    {
        "id": 46170005,
        "epoch_second": 1696264129,
        "problem_id": "abc106_d",
        "contest_id": "abc106",
        "user_id": "yu7400ki",
        "language": "Python (CPython 3.11.4)",
        "point": 400.0,
        "length": 644,
        "result": "AC",
        "execution_time": 695
    }

--- atcoder.jp/Python (CPython 3.11.4)/abc106/abc106_d.py
+++ atcoder.jp/Python (CPython 3.11.4)/abc106/abc106_d.py
@@ -3,14 +3,23 @@ N, M, Q = map(int, input().split())
 cnt = [[0] * N for _ in range(N)]
 for _ in range(M):
     L, R = map(int, input().split())
-    cnt[L-1][R-1] += 1
+    cnt[L - 1][R - 1] += 1
 
 acc = [[0] * (N + 1) for _ in range(N + 1)]
 for y in range(N):
     for x in range(N):
-        acc[y+1][x+1] = acc[y][x+1] + acc[y+1][x] - acc[y][x] + cnt[y][x]
+        acc[y + 1][x + 1] = acc[y][x + 1] + acc[y + 1][x] - acc[y][x] + cnt[y][x]
 
 for _ in range(Q):
     p, q = map(int, input().split())
-    ans = acc[q][q] - acc[p-1][q] - acc[q][p-1] + acc[p-1][p-1]
+    br_x, br_y = q, q
+    bl_x, bl_y = p, q
+    tr_x, tr_y = q, p
+    tl_x, tl_y = p, p
+    ans = (
+        acc[br_y][br_x]
+        - acc[bl_y][bl_x - 1]
+        - acc[tr_y - 1][tr_x]
+        + acc[tl_y - 1][tl_x - 1]
+    )
     print(ans)
