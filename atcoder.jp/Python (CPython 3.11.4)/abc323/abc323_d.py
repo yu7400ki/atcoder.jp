8c1a68dd06689812f9d1ecb4894e21dd980d2a97
yu7400ki <yu7400ki@gmail.com>
2023-10-07 23:04:50 +0900

    [RE] abc323/abc323_d Python (CPython 3.11.4) at 2023-10-07 23:04:50
    {
        "id": 46353998,
        "epoch_second": 1696687490,
        "problem_id": "abc323_d",
        "contest_id": "abc323",
        "user_id": "yu7400ki",
        "language": "Python (CPython 3.11.4)",
        "point": 0.0,
        "length": 393,
        "result": "RE",
        "execution_time": 152
    }

--- atcoder.jp/Python (CPython 3.11.4)/abc323/abc323_d.py
+++ atcoder.jp/Python (CPython 3.11.4)/abc323/abc323_d.py
@@ -1,11 +1,3 @@
-def count_one(n):
-    c = 0
-    while n > 0:
-        if n & 1:
-            c += 1
-        n >>= 1
-    return c
-
 from collections import defaultdict
 
 N = int(input())
@@ -15,7 +7,7 @@ for _ in range(N):
     slime[s] = c
 
 new_slime = defaultdict(int)
-for s, c in slime.items():
+for s, c in slime:
     i = 0
     while s & 1 == 0:
         s >>= 1
@@ -24,6 +16,6 @@ for s, c in slime.items():
 
 ans = 0
 for c in new_slime.values():
-    ans += count_one(c)
+    ans += c.bit_count()
 
 print(ans)
