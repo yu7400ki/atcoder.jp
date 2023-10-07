05e01b63c1c84ff71883b85d82d3dc29bb2b0a4c
yu7400ki <yu7400ki@gmail.com>
2023-10-07 21:54:19 +0900

    [AC] abc323/abc323_d Python (CPython 3.11.4) at 2023-10-07 21:54:19
    {
        "id": 46332262,
        "epoch_second": 1696683259,
        "problem_id": "abc323_d",
        "contest_id": "abc323",
        "user_id": "yu7400ki",
        "language": "Python (CPython 3.11.4)",
        "point": 425.0,
        "length": 591,
        "result": "AC",
        "execution_time": 402
    }

---
+++ atcoder.jp/Python (CPython 3.11.4)/abc323/abc323_d.py
@@ -0,0 +1,30 @@
+def count_one(n):
+    c = 0
+    while n > 0:
+        if n & 1:
+            c += 1
+        n >>= 1
+    return c
+
+from collections import defaultdict
+
+N = int(input())
+slime = defaultdict(int)
+for _ in range(N):
+    s, c = map(int, input().split())
+    slime[s] = c
+
+sorted_slime = sorted(slime.items(), key=lambda x: x[0], reverse=True)
+new_slime = defaultdict(int)
+for s, c in sorted_slime:
+    i = 0
+    while s & 1 == 0:
+        s >>= 1
+        i += 1
+    new_slime[s] += c * (2 ** i)
+
+ans = 0
+for c in new_slime.values():
+    ans += count_one(c)
+
+print(ans)
