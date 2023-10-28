a099d81717f52f2bcd2a21cf63ce2ee78785aae8
yu7400ki <yu7400ki@gmail.com>
2023-10-28 21:19:15 +0900

    [AC] abc326/abc326_c Python (PyPy 3.10-v7.3.12) at 2023-10-28 21:19:15
    {
        "id": 47013454,
        "epoch_second": 1698495555,
        "problem_id": "abc326_c",
        "contest_id": "abc326",
        "user_id": "yu7400ki",
        "language": "Python (PyPy 3.10-v7.3.12)",
        "point": 300.0,
        "length": 388,
        "result": "AC",
        "execution_time": 180
    }

--- atcoder.jp/Python (PyPy 3.10-v7.3.12)/abc326/abc326_c.py
+++ atcoder.jp/Python (PyPy 3.10-v7.3.12)/abc326/abc326_c.py
@@ -8,19 +8,14 @@ A = deque(A)
 ma = 0
 lst = deque()
 
-lst.append(A.popleft())
-cu = 1
-
 while A:
-    if A[0] - lst[0] >= M:
-        l = lst.popleft()
-        cu -= 1
-    else:
-        cu += 1
-        lst.append(A.popleft())
     if len(lst) == 0:
-        cu += 1
         lst.append(A.popleft())
-    ma = max(ma, cu)
+    else:
+        if A[0] - lst[0] >= M:
+            l = lst.popleft()
+        else:
+            lst.append(A.popleft())
+    ma = max(ma, len(lst))
 
 print(ma)
