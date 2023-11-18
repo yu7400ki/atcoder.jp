3dea9c3157d0d8032644a86fc3e5fc46d90de350
yu7400ki <yu7400ki@gmail.com>
2023-11-18 22:50:17 +0900

    [AC] abc329/abc329_f Python (PyPy 3.10-v7.3.12) at 2023-11-18 22:50:17
    {
        "id": 47731090,
        "epoch_second": 1700315417,
        "problem_id": "abc329_f",
        "contest_id": "abc329",
        "user_id": "yu7400ki",
        "language": "Python (PyPy 3.10-v7.3.12)",
        "point": 500.0,
        "length": 355,
        "result": "AC",
        "execution_time": 664
    }

--- atcoder.jp/Python (PyPy 3.10-v7.3.12)/abc329/abc329_f.py
+++ atcoder.jp/Python (PyPy 3.10-v7.3.12)/abc329/abc329_f.py
@@ -6,6 +6,11 @@ for _ in range(Q):
     a, b = map(int, input().split())
     a -= 1
     b -= 1
-    c[b] |= c[a]
-    c[a] = set()
+    if len(c[a]) < len(c[b]):
+        c[b] |= c[a]
+        c[a] = set()
+    else:
+        c[a] |= c[b]
+        c[b] = c[a]
+        c[a] = set()
     print(len(c[b]))
