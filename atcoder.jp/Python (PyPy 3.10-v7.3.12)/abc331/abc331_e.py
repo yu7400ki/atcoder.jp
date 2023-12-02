86e71bcf108f533b3244180a1a3057500a75a1f7
yu7400ki <yu7400ki@gmail.com>
2023-12-02 22:24:49 +0900

    [TLE] abc331/abc331_e Python (PyPy 3.10-v7.3.12) at 2023-12-02 22:24:49
    {
        "id": 48140191,
        "epoch_second": 1701523489,
        "problem_id": "abc331_e",
        "contest_id": "abc331",
        "user_id": "yu7400ki",
        "language": "Python (PyPy 3.10-v7.3.12)",
        "point": 0.0,
        "length": 561,
        "result": "TLE",
        "execution_time": 3317
    }

---
+++ atcoder.jp/Python (PyPy 3.10-v7.3.12)/abc331/abc331_e.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+N, M, L = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+pair = defaultdict(set)
+for _ in range(L):
+    c, d = map(int, input().split())
+    c -= 1
+    d -= 1
+    pair[c].add(d)
+
+a_sorted = sorted(enumerate(a), key=lambda x: x[1])
+b_sorted = sorted(enumerate(b), key=lambda x: x[1])
+
+ans = -1
+for i, a in a_sorted:
+    for j, b in b_sorted:
+        if j in pair[i]:
+            continue
+        else:
+            ans = max(ans, a + b)
+
+print(ans)
