429c1ce30639d60b766e90731ec2954a5aedc213
yu7400ki <yu7400ki@gmail.com>
2023-12-16 21:14:54 +0900

    [AC] abc333/abc333_b Python (CPython 3.11.4) at 2023-12-16 21:14:54
    {
        "id": 48551793,
        "epoch_second": 1702728894,
        "problem_id": "abc333_b",
        "contest_id": "abc333",
        "user_id": "yu7400ki",
        "language": "Python (CPython 3.11.4)",
        "point": 200.0,
        "length": 226,
        "result": "AC",
        "execution_time": 10
    }

--- atcoder.jp/Python (CPython 3.11.4)/abc333/abc333_b.py
+++ atcoder.jp/Python (CPython 3.11.4)/abc333/abc333_b.py
@@ -1,7 +1,15 @@
 S1, S2 = list(input())
 T1, T2 = list(input())
 
-if abs(ord(S1) - ord(S2)) == abs(ord(T1) - ord(T2)):
+d1 = abs(ord(S1) - ord(S2))
+d2 = abs(ord(T1) - ord(T2))
+
+if d1 > 2:
+    d1 = 5 - d1
+if d2 > 2:
+    d2 = 5 - d2
+
+if d1 == d2:
     print("Yes")
 else:
     print("No")
