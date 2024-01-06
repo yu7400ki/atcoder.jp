bae380a6d2603b089f667ab9f3ac8afaa1ac5177
yu7400ki <yu7400ki@gmail.com>
2024-01-06 22:27:35 +0900

    [TLE] abc335/abc335_c Python (PyPy 3.10-v7.3.12) at 2024-01-06 22:27:35
    {
        "id": 49116512,
        "epoch_second": 1704547655,
        "problem_id": "abc335_c",
        "contest_id": "abc335",
        "user_id": "yu7400ki",
        "language": "Python (PyPy 3.10-v7.3.12)",
        "point": 0.0,
        "length": 469,
        "result": "TLE",
        "execution_time": 2214
    }

--- atcoder.jp/Python (PyPy 3.10-v7.3.12)/abc335/abc335_c.py
+++ atcoder.jp/Python (PyPy 3.10-v7.3.12)/abc335/abc335_c.py
@@ -7,14 +7,11 @@ parts = deque(parts, N)
 
 for _ in range(Q):
     t, u = input().split()
-    match t:
-        case "1":
-            mov = (
-                1 if u == "R" else -1 if u == "L" else 0,
-                1 if u == "U" else -1 if u == "D" else 0,
-            )
-            head = parts[0]
-            parts.appendleft((head[0] + mov[0], head[1] + mov[1]))
-        case "2":
-            u = int(u) - 1
-            print(parts[u][0], parts[u][1])
+    if t == "1":
+        dx = 1 if u == "R" else -1 if u == "L" else 0
+        dy = 1 if u == "U" else -1 if u == "D" else 0
+        head = parts[0]
+        parts.appendleft((head[0] + dx, head[1] + dy))
+    else:
+        u = int(u) - 1
+        print(parts[u][0], parts[u][1])
