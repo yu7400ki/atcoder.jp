b6d8243d7443741a4a91b480dd08a65e31196455
yu7400ki <yu7400ki@gmail.com>
2024-01-20 21:11:57 +0900

    [AC] abc337/abc337_b Python (CPython 3.11.4) at 2024-01-20 21:11:57
    {
        "id": 49451643,
        "epoch_second": 1705752717,
        "problem_id": "abc337_b",
        "contest_id": "abc337",
        "user_id": "yu7400ki",
        "language": "Python (CPython 3.11.4)",
        "point": 200.0,
        "length": 274,
        "result": "AC",
        "execution_time": 9
    }

--- atcoder.jp/Python (CPython 3.11.4)/abc337/abc337_b.py
+++ atcoder.jp/Python (CPython 3.11.4)/abc337/abc337_b.py
@@ -4,8 +4,8 @@ S = [[key,len(list(group))] for key,group in itertools.groupby(list(input()))]
 if len(S) > 3:
     print("No")
 else:
-    for s, t in zip(S, ["A", "B", "C"]):
-        if s[0] != t:
+    for s, t in zip(S, S[1:]):
+        if s[0] > t[0]:
             print("No")
             break
     else:
