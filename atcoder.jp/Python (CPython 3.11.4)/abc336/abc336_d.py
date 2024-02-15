a93d5e3067a8c0103f1aa3c9885b20c8600df084
yu7400ki <yu7400ki@gmail.com>
2024-02-16 03:12:40 +0900

    [WA] abc336/abc336_d Python (CPython 3.11.4) at 2024-02-16 03:12:40
    {
        "id": 50294334,
        "epoch_second": 1708020760,
        "problem_id": "abc336_d",
        "contest_id": "abc336",
        "user_id": "yu7400ki",
        "language": "Python (CPython 3.11.4)",
        "point": 0.0,
        "length": 226,
        "result": "WA",
        "execution_time": 85
    }

--- atcoder.jp/Python (CPython 3.11.4)/abc336/abc336_d.py
+++ atcoder.jp/Python (CPython 3.11.4)/abc336/abc336_d.py
@@ -1,11 +1,12 @@
 N = int(input())
 A = list(map(int, input().split()))
 
-A[0] = A[-1] = 1
 for _ in range(2):
-    for i in range(1, N):
-        if A[i - 1] + 1 < A[i]:
-            A[i] = A[i - 1] + 1
+    level = 0
+    for i in range(N):
+        if A[i] > level:
+            level += 1
+            A[i] = level
     A.reverse()
 
 print(max(A))
