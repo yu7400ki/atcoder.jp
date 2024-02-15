420d510451b3638baf4a281e4c2bd7ee638b7ccb
yu7400ki <yu7400ki@gmail.com>
2024-02-16 01:18:50 +0900

    [AC] abc337/abc337_c Python (CPython 3.11.4) at 2024-02-16 01:18:50
    {
        "id": 50293329,
        "epoch_second": 1708013930,
        "problem_id": "abc337_c",
        "contest_id": "abc337",
        "user_id": "yu7400ki",
        "language": "Python (CPython 3.11.4)",
        "point": 300.0,
        "length": 175,
        "result": "AC",
        "execution_time": 361
    }

--- atcoder.jp/Python (CPython 3.11.4)/abc337/abc337_c.py
+++ atcoder.jp/Python (CPython 3.11.4)/abc337/abc337_c.py
@@ -2,8 +2,8 @@ N = int(input())
 A = list(map(int, input().split()))
 A = {a: i for i, a in enumerate(A)}
 
-i = A[-1]
-print(i + 1)
+a = A[-1] + 1
+print(a)
 for _ in range(N - 1):
-    i = A[i + 1]
-    print(i + 1)
+    a = A[a] + 1
+    print(a)
