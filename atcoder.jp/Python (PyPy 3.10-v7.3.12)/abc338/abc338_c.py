fb23093dfd20574d8ee5234f154a056c219da6c9
yu7400ki <yu7400ki@gmail.com>
2024-02-15 22:41:37 +0900

    [WA] abc338/abc338_c Python (PyPy 3.10-v7.3.12) at 2024-02-15 22:41:37
    {
        "id": 50290677,
        "epoch_second": 1708004497,
        "problem_id": "abc338_c",
        "contest_id": "abc338",
        "user_id": "yu7400ki",
        "language": "Python (PyPy 3.10-v7.3.12)",
        "point": 0.0,
        "length": 383,
        "result": "WA",
        "execution_time": 347
    }

---
+++ atcoder.jp/Python (PyPy 3.10-v7.3.12)/abc338/abc338_c.py
@@ -0,0 +1,15 @@
+N = int(input())
+Q = list(map(int, input().split()))
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+inf = 1 << 60
+ans = 0
+for i in range(10**6):
+    rest = [q - a * i for q, a in zip(Q, A)]
+    if any([r < 0 for r in rest]):
+        break
+    j = min([r // b if b > 0 else inf for r, b in zip(rest, B)])
+    ans = max(ans, i + j)
+
+print(ans)
