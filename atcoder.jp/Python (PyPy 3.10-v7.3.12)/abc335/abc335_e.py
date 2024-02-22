e344463ba529190a470b57f453b9debe2d8c7f3e
yu7400ki <yu7400ki@gmail.com>
2024-02-22 23:28:11 +0900

    [WA] abc335/abc335_e Python (PyPy 3.10-v7.3.12) at 2024-02-22 23:28:11
    {
        "id": 50519387,
        "epoch_second": 1708612091,
        "problem_id": "abc335_e",
        "contest_id": "abc335",
        "user_id": "yu7400ki",
        "language": "Python (PyPy 3.10-v7.3.12)",
        "point": 0.0,
        "length": 680,
        "result": "WA",
        "execution_time": 487
    }

---
+++ atcoder.jp/Python (PyPy 3.10-v7.3.12)/abc335/abc335_e.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+from atcoder.dsu import DSU
+
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+g = defaultdict(list)
+uf = DSU(N)
+for _ in range(M):
+    u, v = map(int, input().split())
+    u -= 1
+    v -= 1
+    if A[u] == A[v]:
+        uf.merge(u, v)
+    else:
+        if A[u] > A[v]:
+            u, v = v, u
+        g[u].append(v)
+
+dp = [-1] * N
+dp[0] = 1
+for i in sorted(range(N), key=lambda x: A[x]):
+    u = uf.leader(i)
+    if dp[u] == -1:
+        continue
+    for j in g[i]:
+        v = uf.leader(j)
+        if u == v:
+            continue
+        dp[v] = max(dp[v], dp[u] + 1)
+
+print(max(dp[N - 1], 0))
