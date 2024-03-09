1887a7d9f01357332067c1575cf9c884df35893a
yu7400ki <yu7400ki@gmail.com>
2024-03-09 21:56:27 +0900

    [AC] abc344/abc344_e Python (PyPy 3.10-v7.3.12) at 2024-03-09 21:56:27
    {
        "id": 51071976,
        "epoch_second": 1709988987,
        "problem_id": "abc344_e",
        "contest_id": "abc344",
        "user_id": "yu7400ki",
        "language": "Python (PyPy 3.10-v7.3.12)",
        "point": 475.0,
        "length": 1308,
        "result": "AC",
        "execution_time": 545
    }

--- atcoder.jp/Python (PyPy 3.10-v7.3.12)/abc344/abc344_e.py
+++ atcoder.jp/Python (PyPy 3.10-v7.3.12)/abc344/abc344_e.py
@@ -7,6 +7,27 @@ class Node:
     left: "Node" = None
     right: "Node" = None
 
+    def insert(self, node: "Node"):
+        node.left = self
+        node.right = self.right
+        if self.right:
+            self.right.left = node
+        self.right = node
+
+    def delete(self):
+        if self.left:
+            self.left.right = self.right
+        if self.right:
+            self.right.left = self.left
+
+    def __str__(self):
+        ret = []
+        node = self
+        while node:
+            ret.append(str(node.value))
+            node = node.right
+        return " ".join(ret)
+
 
 N = int(input())
 A = list(map(int, input().split()))
@@ -28,22 +49,13 @@ for _ in range(Q):
         x, y = query
         node = index_map[x]
         new_node = Node(y)
-        new_node.left = node
-        new_node.right = node.right
-        node.right = new_node
+        node.insert(new_node)
         index_map[y] = new_node
     elif t == 2:
         x = query[0]
         node = index_map[x]
         if node == head:
             head = node.right
-        if node.left:
-            node.left.right = node.right
-        if node.right:
-            node.right.left = node.left
+        node.delete()
 
-
-node = head
-while node:
-    print(node.value, end=" ")
-    node = node.right
+print(head)
