3f62e777175bc72aa134b95e71dc0fff0a4306de
yu7400ki <yu7400ki@gmail.com>
2024-03-31 01:48:16 +0900

    [WA] abc347/abc347_d Python (CPython 3.11.4) at 2024-03-31 01:48:16
    {
        "id": 51889810,
        "epoch_second": 1711817296,
        "problem_id": "abc347_d",
        "contest_id": "abc347",
        "user_id": "yu7400ki",
        "language": "Python (CPython 3.11.4)",
        "point": 0.0,
        "length": 849,
        "result": "WA",
        "execution_time": 12
    }

---
+++ atcoder.jp/Python (CPython 3.11.4)/abc347/abc347_d.py
@@ -0,0 +1,35 @@
+import sys
+
+a, b, C = map(int, input().split())
+c = C.bit_count()
+
+for i in range(c):
+    j = c - i
+    x = a - i
+    y = b - j
+    if x == y and x >= 0 and y >= 0:
+        k = 1
+        ans_a = 0
+        ans_b = 0
+        while i or j or x:
+            if C & k:
+                if i:
+                    i -= 1
+                    ans_a |= k
+                elif j:
+                    j -= 1
+                    ans_b |= k
+            else:
+                if x:
+                    x -= 1
+                    ans_a |= k
+                    ans_b |= k
+            k <<= 1
+        if ans_a < 1 << 60 or ans_b < 1 << 60:
+            print(ans_a, ans_b)
+            assert ans_a ^ ans_b == C
+            assert ans_a.bit_count() == a
+            assert ans_b.bit_count() == b
+            break
+else:
+    print(-1)
